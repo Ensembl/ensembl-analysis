099f2c9b3191b6702acc595bf0224640105f4ab6
Laura Clarke <lec@sanger.ac.uk>
2004-06-29 12:57:43 +0000

    start on a more maintainable version of the runnables and runnabledbs

---
+++ modules/Bio/EnsEMBL/Analysis/Runnable/RepeatMasker.pm
@@ -0,0 +1,181 @@
+# Ensembl module for Bio::EnsEMBL::Analysis::Runnable::RepeatMasker
+#
+# Copyright (c) 2004 Ensembl
+#
+
+=head1 NAME
+
+Bio::EnsEMBL::Analysis::Runnable::RepeatMasker
+
+=head1 SYNOPSIS
+
+  my $repeat_masker = Bio::EnsEMBL::Analysis::Runnable::RepeatMasker->
+  new(
+      -query => 'slice',
+      -program => 'repeatmasker',
+      -options => '-low'
+     );
+  $repeat_masker->run;
+  my @repeats = @{$repeat_masker->output};
+
+=head1 DESCRIPTION
+
+RepeatMasker expects to run the program RepeatMasker
+and produce RepeatFeatures which can be stored in the repeat_feature
+and repeat_consensus tables in the core database
+
+
+=head1 CONTACT
+
+Post questions to the Ensembl development list: ensembl-dev@ebi.ac.uk
+
+=cut
+
+
+package Bio::EnsEMBL::Analysis::Runnable::RepeatMasker;
+
+use strict;
+use warnings;
+
+use Bio::EnsEMBL::Analysis::Runnable;
+use Bio::EnsEMBL::RepeatFeature;
+use Bio::EnsEMBL::RepeatConsensus;
+use Bio::EnsEMBL::Utils::Exception qw(throw warning);
+use vars qw(@ISA);
+
+@ISA = qw(Bio::EnsEMBL::Analysis::Runnable);
+
+
+=head2 run_analysis
+
+  Arg [1]   : Bio::EnsEMBL::Analysis::Runnable::RepeatMasker
+  Function  : constructs a commandline and runs the program passed
+  in, the generic method in Runnable isnt used as RepeatMasker doesnt
+  fit this module
+  Returntype: none
+  Exceptions: throws if run failed because sysetm doesnt
+  return 0 or the output file doesnt exist
+  Example   : 
+
+=cut
+
+
+sub run_analysis{
+  my ($self) = @_;
+  my $cmd = $self->program." ";
+  $cmd .= $self->options." " if($self->options);
+  $cmd .= $self->queryfile;
+  print "Running analysis ".$cmd."\n";
+  system($cmd) == 0 or throw("FAILED to run ".$cmd);
+  foreach my $file(glob $self->queryfile."*"){
+    $self->files_to_delete($file);
+  }
+  if(! -e $self->resultsfile){
+    throw("FAILED to run repeatmasker on ".$self->queryfile." ".
+          $self->resultsfile." has not been produced ".
+          "RepeatMasker:run_analysis");
+  }
+}
+
+
+
+=head2 parse_results
+
+  Arg [1]   : Bio::EnsEMBL::Analysis::Runnable::RepeatMasker
+  Arg [2]   : string, filename
+  Function  : open and parse the results file into repeat
+  features
+  Returntype: none 
+  Exceptions: throws on failure to open or close output file
+  Example   : 
+
+=cut
+
+
+
+sub parse_results{
+  my ($self, $results) =  @_;
+  if(!$results){
+    $results = $self->resultsfile;
+  }
+  open(OUT, "<".$results) or throw("FAILED to open ".$results.
+                                   "RepeatMasker:parse_results");
+  REPEAT:while(<OUT>){
+      if (/no repetitive sequences detected/) {
+        print "RepeatMasker didn't find any repetitive sequences";
+        last REPEAT; #no repeats found no point carrying on
+      }
+      my @columns;
+      if(/\d+/){ #ignoring introductory lines
+        chomp;
+        @columns = split;
+        pop @columns if $columns[-1] eq '*';
+        if (@columns != 15) {
+          throw("Can't parse repeatmasker output unexpected number ".
+                "of columns in the output ".@columns." in ".$_);
+        }
+        my ($score, $query_name, $query_start, $query_end, $strand,
+            $repeat_name, $repeat_class) 
+          =  @columns[0, 4, 5, 6, 8, 9, 10];
+        my $start_column;
+        if($strand eq '+'){ 
+          $start_column = 11;
+          $strand = 1;
+        }
+        if($strand eq 'C'){
+          $start_column = 13;
+          $strand = -1;
+        }#the location of the start and end inside the repeat 
+        #is different depending on the strand
+        my $repeat_start = $columns[$start_column];
+        my $repeat_end = $columns[12];
+        if($repeat_end < $repeat_start){
+          my $temp_end = $repeat_start;
+          $repeat_start = $repeat_end;
+          $repeat_end = $temp_end;
+        }
+        my $rc = $self->_get_consensus($repeat_name, $repeat_class);
+        my $rf = Bio::EnsEMBL::RepeatFeature->new;
+        $rf->score            ($score);
+        $rf->start            ($query_start);
+        $rf->end              ($query_end);
+        $rf->strand           ($strand);
+        $rf->hstart           ($repeat_start);
+        $rf->hend             ($repeat_end);
+        $rf->repeat_consensus ($rc);
+        $rf->slice($self->query);
+        $self->output([$rf]);
+      }
+    }
+  close(OUT) or throw("FAILED to close ".$results.
+                      "RepeatMasker:parse_results");
+}
+
+
+=head2 _get_consensus
+
+  Arg [1]   : Bio::EnsEMBL::Analysis::Runnable::RepeatMasker
+  Arg [2]   : string, name of repeat
+  Arg [3]   : string, name of class
+  Function  : create a new Bio::EnsEMBL::RepeatConsensus object
+  Returntype: Bio::EnsEMBL::RepeatConsensus
+  Exceptions: 
+  Example   : 
+
+=cut
+
+
+sub _get_consensus {
+    my ($self, $name, $class) = @_;
+    my $cons;
+
+    unless ($cons = $self->{'_consensi'}{$name}) {
+      $cons = new Bio::EnsEMBL::RepeatConsensus;
+      $cons->name($name);
+      $cons->repeat_class($class);
+      $self->{'_consensi'}{$name} = $cons;
+    }
+
+    return $cons;
+
+}
---
+++ modules/Bio/EnsEMBL/Analysis/RunnableDB.pm
@@ -0,0 +1,504 @@
+# Ensembl module for Bio::EnsEMBL::Analysis::RunnableDB
+#
+# Copyright (c) 2004 Ensembl
+#
+
+=head1 NAME
+
+Bio::EnsEMBL::Analysis::RunnableDB
+
+=head1 SYNOPSIS
+
+  my $repeat_masker = Bio::EnsEMBL::Analysis::RunnableDB::RepeatMasker->
+  new(
+      -input_id => 'contig::AL805961.22.1.166258:1:166258:1',
+      -db => $db,
+      -analysis => $analysis,
+     );
+  $repeat_masker->fetch_input;
+  $repeat_masker->run;
+  $repeat_masker->write_output;
+
+=head1 DESCRIPTION
+
+This module acts as a base class for our RunnableDBs who act as an 
+interface between the core database and our Runnables both fetching
+input data and writing data back to the databases
+
+The module provides some base functionality some of which must be
+used
+
+The constructor fits the model the pipeline which runs most of
+our analyses expects. If a child runnabledb expects more arguments
+to the constructor than this one it wont be directly runnable by the
+pipeline
+
+Most of the other methods provided are containers of some description
+but there are some methods with specific functionality
+
+parameters_hash is there to parse a string from the parameters varible
+in the analysis object. This string should have the format
+key => value, key => value where the key would be the Runnables
+constructor argument and the value the variable. This is to allow
+some flexibility in the arguments expected by and the way we run
+Runnables.
+
+fetch_sequence fetched a sequence using the fetch_by_name method of 
+the slice adaptor from the given database. The name, database and an
+array of logic_names to determine masking can be given. If no name
+or database is provided the method defaults to input_id and db
+
+validate, this is a method which does some basic validation of the
+feature before storage. This checks if slice and analysis object are
+defined and if start, end and strand are defined then that the start
+is smaller than the end and both the start and end are > 0
+
+All runnableDBs need to implement 3 methods to run within the pipeline
+
+fetch_input, this always must be implemented by individual child
+RunnableDBs as the input required for different analyses can vary so
+widely that it is impossible to write a generic method
+
+run, there is a run method implemented. To use this child runnabledbs
+need to have added the runnables they want run to the runnable method
+which holds an array of runnables which are each called and the output 
+stored in this method
+
+write_output, there is also a generic implementation of this. To use this
+method the child runnabledb must implement a get_adaptor method which
+returns the appropriate adaptor to be used in storage. 
+
+=head1 CONTACT
+
+Post questions to the Ensembl development list: ensembl-dev@ebi.ac.uk
+
+=cut
+package Bio::EnsEMBL::Analysis::RunnableDB;
+
+
+use strict;
+use warnings;
+
+use Bio::EnsEMBL::Utils::Exception qw(verbose throw warning);
+use Bio::EnsEMBL::Utils::Argument qw( rearrange );
+use Bio::EnsEMBL::Root;
+
+use vars qw (@ISA);
+
+@ISA = qw(Bio::EnsEMBL::Root);
+
+
+=head2 new
+
+  Arg [1]   : Bio::EnsEMBL::Analysis::RunnableDB
+  Arg [2]   : Bio::EnsEMBL::Pipeline::DBSQL::DBAdaptor
+  Arg [3]   : Bio::EnsEMBL::Analysis
+  Function  : create a Bio::EnsEMBL::Analysis::RunnableDB object
+  Returntype: Bio::EnsEMBL::Analysis::RunnableDB
+  Exceptions: throws if not passed either a dbadaptor, input id or
+  an analysis object
+  Example   : $rdb = $perl_path->new( -analysis => $self->analysis,
+                                      -input_id => $self->input_id,
+                                      -db => $self->adaptor->db );
+
+=cut
+
+
+
+sub new{
+  my ($class,@args) = @_;
+  my $self = $class->SUPER::new(@args);
+  &verbose('WARNING');
+  my ($db, $input_id, $analysis) = rearrange(['DB', 'INPUT_ID', 
+                                              'ANALYSIS'], @args);
+  if(!$db || !$analysis || !$input_id){
+    throw("Can't create a RunnableDB without a dbadaptor ".$db." an ".
+          "analysis object ".$analysis." or an input_id ".$input_id);
+  }
+  $self->db($db);
+  $self->analysis($analysis);
+  $self->input_id($input_id);
+  return $self;
+}
+
+
+
+=head2 db
+
+  Arg [1]   : Bio::EnsEMBL::Analysis::RunnableDB
+  Arg [2]   : Bio::EnsEMBL::Pipeline::DBAdaptor
+  Function  : container for dbadaptor
+  Returntype: Bio::EnsEMBL::Pipeline::DBSQL::DBAdaptor
+  Exceptions: throws if not passed a Bio::EnsEMBL::DBSQL::DBConnection
+  object
+  Example   : 
+
+=cut
+
+
+sub db{
+  my $self = shift;
+  my $db = shift;
+  if($db){
+    throw("Must pass RunnableDB:db a Bio::EnsEMBL::DBSQL::DBConnection ".
+          "not a ".$db) 
+      unless($db->isa('Bio::EnsEMBL::Pipeline::DBSQL::DBAdaptor'));
+    $self->{'db'} = $db;
+  }
+  return $self->{'db'};
+}
+
+
+
+=head2 analysis
+
+  Arg [1]   : Bio::EnsEMBL::Analysis::RunnableDB
+  Arg [2]   : Bio::EnsEMBL::Analysis
+  Function  : container for analysis object
+  Returntype: Bio::EnsEMBL::Analysis
+  Exceptions: throws passed incorrect object type
+  Example   : 
+
+=cut
+
+
+
+sub analysis{
+  my $self = shift;
+  my $analysis = shift;
+  if($analysis){
+    throw("Must pass RunnableDB:analysis a Bio::EnsEMBL::Analysis".
+          "not a ".$analysis) unless($analysis->isa
+                                     ('Bio::EnsEMBL::Analysis'));
+    $self->{'analysis'} = $analysis;
+  }
+  return $self->{'analysis'};
+}
+
+
+
+=head2 query
+
+  Arg [1]   : Bio::EnsEMBL::Analysis::RunnableDB
+  Arg [2]   : Bio::EnsEMBL::Slice
+  Function  : container for slice object
+  Returntype: Bio::EnsEMBL::Slice
+  Exceptions: throws if passed the incorrect object type
+  Example   : 
+
+=cut
+
+
+sub query{
+  my $self = shift;
+  my $slice = shift;
+  if($slice){
+    throw("Must pass RunnableDB:query a Bio::EnsEMBL::Slice".
+          "not a ".$slice) unless($slice->isa
+                                     ('Bio::EnsEMBL::Slice'));
+    $self->{'slice'} = $slice;
+  }
+  return $self->{'slice'};
+}
+
+
+=head2 runnable
+
+  Arg [1]   : Bio::EnsEMBL::Analysis::RunnableDB
+  Arg [2]   : Bio::EnsEMBL::Analysis::Runnable
+  Function  : container for an array of runnables
+  Returntype: arrayref
+  Exceptions: throws if passed the wrong object type
+  Example   : 
+
+=cut
+
+
+
+sub runnable{
+  my ($self, $runnable) = @_;
+  if(!$self->{'runnable'}){
+    $self->{'runnable'} = [];
+  }
+  if($runnable){
+    throw("Must pass RunnableDB:runnable a ".
+          "Bio::EnsEMBL::Analysis::Runnable not a ".$runnable) 
+      unless($runnable->isa('Bio::EnsEMBL::Analysis::Runnable'));
+    push(@{$self->{'runnable'}}, $runnable);
+  }
+  return $self->{'runnable'};
+}
+
+
+=head2 containers
+
+  Arg [1]   : Bio::EnsEMBL::Analysis::RunnableDB
+  Arg [2]   : string/int
+  Function  : container for specified variable
+  Returntype: string/int
+  Exceptions: none
+  Example   : 
+
+=cut
+
+
+sub input_id{
+  my $self = shift;
+  $self->{'input_id'} = shift if(@_);
+  return $self->{'input_id'};
+}
+
+
+sub input_is_void{
+  my $self = shift;
+  $self->{'input_is_void'} = shift if(@_);
+  return $self->{'input_is_void'};
+}
+
+
+sub failing_job_status{
+  my $self = shift;
+  $self->{'failing_status'} = shift if(@_);
+  return $self->{'failing_status'};
+}
+
+=head2 output
+
+  Arg [1]   : Bio::EnsEMBL::Analysis::RunnableDB
+  Arg [2]   : arrayref of output
+  Function  : push array passed into onto output array
+  Returntype: arrayref
+  Exceptions: throws if not passed correct type
+  Example   : $self->output(\@output);
+
+=cut
+
+
+sub output{
+  my ($self, $output) = @_;
+  if(!$self->{'output'}){
+    $self->{'output'} = [];
+  }
+  if($output){
+    if(ref($output) ne 'ARRAY'){
+      throw('Must pass RunnableDB:output an array ref not a '.$output);
+    }
+    push(@{$self->{'output'}}, @$output);
+  }
+  return $self->{'output'};
+}
+
+#utility methods
+
+=head2 fetch_sequence
+
+  Arg [1]   : Bio::EnsEMBL::Analysis::RunnableDB
+  Arg [2]   : string, name
+  Arg [3]   : Bio::EnsEMBL::DBAdaptor
+  Arg [4]   : arrayref of logic_name if sequence is to be masked
+  Function  : gets sequence from specifed database
+  Returntype: Bio::EnsEMBL::Slice
+  Exceptions: none
+  Example   : 
+
+=cut
+
+
+sub fetch_sequence{
+  my ($self, $name, $db, $repeat_masking) = @_;
+  if(!$db){
+    $db = $self->db;
+  }
+  if(!$name){
+    $name = $self->input_id;
+  }
+  my $sa = $db->get_SliceAdaptor;
+  my $slice = $sa->fetch_by_name($name);
+  $repeat_masking = [] unless($repeat_masking);
+  if(@$repeat_masking){
+    my $sequence = $slice->get_repeatmasked_seq($repeat_masking);
+    $slice = $sequence
+  }
+  return $slice;
+}
+
+
+=head2 parameters_hash
+
+  Arg [1]   : Bio::EnsEMBL::Analysis::RunnableDB
+  Arg [2]   : string, parameters string
+  Function  : parses the parameters string into a hash
+  for the Runnables constructor. If neither of the delimiters
+  are found in the string the string is given the key of options
+  Returntype: hashref
+  Exceptions: 
+  Example   : 
+
+=cut
+
+
+sub parameters_hash{
+  my ($self, $string) = @_;
+  if(!$string){
+    $string = $self->analysis->parameters;
+  }
+  my %parameters_hash;
+  if(!$string){
+    return;
+  }
+  if($string != /,/ && $string != /=>/){
+    $parameters_hash{'options'} = $string;
+  }else{
+     my @pairs = split (/,/, $string);
+     foreach my $pair(@pairs){
+       my ($key, $value) = split (/=>/, $pair); 
+       if ($key && $value) {
+         $key   =~ s/^\s+//g;
+         $key   =~ s/\s+$//g;
+         $value =~ s/^\s+//g;
+         $value =~ s/\s+$//g;
+         $parameters_hash{$key} = $value;
+       } else {
+         $parameters_hash{$key} = "__NONE__";
+       }
+     }
+  }
+  return \%parameters_hash;
+}
+
+
+
+
+=head2 run
+
+  Arg [1]   : Bio::EnsEMBL::Analysis::RunnableDB
+  Function  : cycles through all the runnables, calls run and pushes
+  their output into the RunnableDBs output array
+  Returntype: array ref
+  Exceptions: none
+  Example   : 
+
+=cut
+
+
+
+sub run{
+  my ($self) = @_;
+  foreach my $runnable(@{$self->runnable}){
+    $runnable->run;
+    my $output =  $runnable->output;
+    push(@{$self->{'output'}}, @{$runnable->output});
+  }
+  return $self->{'output'};
+}
+
+
+
+=head2 write_output
+
+  Arg [1]   : Bio::EnsEMBL::Analysis::RunnableDB
+  Function  : set analysis and slice on each feature
+  Returntype: 1
+  Exceptions: none
+  Example   : 
+
+=cut
+
+
+sub write_output{
+  my ($self) = @_;
+  my $adaptor = $self->get_adaptor;
+  foreach my $feature(@{$self->output}){
+    $feature->analysis($self->analysis);
+    $feature->slice($self->query) if(!$feature->slice);
+    $self->validate($feature);
+    eval{
+      $adaptor->store($feature);
+    };
+    if($@){
+      throw("RunnableDB:store failed, failed to write ".$feature." to ".
+            "the database $@");
+    }
+  }
+  return 1;
+}
+
+
+
+=head2 validate
+
+  Arg [1]   : Bio::EnsEMBL::Analysis::RunnableDB
+  Arg [2]   : Bio::EnsEMBL::Feature
+  Function  : validates feature
+  Returntype: Bio::EnsEMBL::Feature
+  Exceptions: throws if no slice or analysis is defined
+  if the start, end or strand arent defined, if start or end are
+  less than one or if start is greater than end
+  Example   : 
+
+=cut
+
+
+sub validate{
+  my ($self, $feature) = @_;
+  my @error_messages;
+  if(!$feature){
+    throw("Can't validate a feature without a feature ".
+          "RunnableDB::validate");
+  }else{
+    if(!$feature->slice){
+      my $string = "No slice defined";
+      push(@error_messages, $string);
+    }
+    if(!$feature->analysis){
+      my $string = "No analysis defined";
+      push(@error_messages, $string);
+    }
+    if(!$feature->start){
+      my $string = "No start defined";
+      push(@error_messages, $string); 
+    }
+    if(!$feature->end){
+      my $string = "No end defined";
+      push(@error_messages, $string); 
+    }
+    if(!defined $feature->strand){
+      my $string = "No strand defined";
+      push(@error_messages, $string); 
+    }
+    if($feature->start > $feature->end){
+      my $string = "Start is greater than end ".$feature->start." ".
+        $feature->end;
+      push(@error_messages, $string); 
+    }
+    if($feature->start < 1 || $feature->end < 1){
+      my $string = "Feature has start or end coordinates less than 1 ".
+        $feature->start." ".$feature->end;
+      push(@error_messages, $string); 
+    }
+  }
+  if(@error_messages > 0){
+    print STDERR join("\n", @error_messages);
+    throw("Invalid feature RunnableDB:validate");
+  }
+}
+
+
+
+=head2 fetch_input
+
+  Arg [1]   : Bio::EnsEMBL::Analysis::RunnableDB
+  Function  : throw as it means child hasnt implement an essential method
+  Returntype: none
+  Exceptions: see function
+  Example   : 
+
+=cut
+
+
+
+sub fetch_input{
+  my ($self) = @_;
+  throw("Must implement fetch input in ".$self." RunnableDB will ".
+        "not provide this");
+}
