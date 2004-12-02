48f510e49094dd06379bb57d848fe96de1addba0
Simon White <sw4@sanger.ac.uk>
2004-12-02 11:30:45 +0000

    added documentation

--- modules/Bio/EnsEMBL/Analysis/Runnable/Spliced_elsewhere.pm
+++ modules/Bio/EnsEMBL/Analysis/Runnable/Spliced_elsewhere.pm
@@ -1,3 +1,37 @@
+# You may distribute this module under the same terms as perl itself
+#
+# POD documentation - main docs before the code
+
+=pod 
+
+=head1 NAME
+
+Bio::EnsEMBL::Analysis::Runnable::Spliced_elsewhere;
+
+=head1 SYNOPSIS
+
+  my $runnable = Bio::EnsEMBL::Analysis::Runnable::Spliced_elsewhere ->new
+  (
+   '-trans' => \@transcript_array_ref,
+   '-analysis' => $analysis_object,
+  );
+    $runnable->run;
+    $output = $runnable->output
+
+=head1 DESCRIPTION
+
+Runnable for Bio::EnsEMBL::Analysis::RunnableDB::Spliced_elsewhere
+Does the blast analysis and returns the results to the runnable_db for
+parsing.
+Uses Bio::EnsEMBL::Analysis::Config::Pseudogene for config
+
+=head1 CONTACT
+
+Simon White
+
+sw4@sanger.ac.uk
+
+=cut
 
 package Bio::EnsEMBL::Analysis::Runnable::Spliced_elsewhere;
 
@@ -13,37 +47,62 @@ use vars qw(@ISA);
 @ISA = qw(Bio::EnsEMBL::Analysis::Runnable);
 
 
+=head2 new
+
+  Args       : various
+  Description: Runnable constructor
+  Returntype : Bio::EnsEMBL::Analysis::Runnable::Spliced_elsewhere;
+  Caller     : general
+
+=cut
+
 sub new {
   my ($class,@args) = @_;
   my $self = $class->SUPER::new(@args);
 
-  $self->{'_genes'} = [];	#array of genes to test;  
+  $self->{'_trans'} = [];	#array of transcripts to test;  
 
-  my($genes) = $self->_rearrange([qw(
-				     GENES
+  my($trans) = $self->_rearrange([qw(
+				     TRANS
 				    )], @args);
-  if ($genes) {
-    $self->genes($genes);
+  if ($trans) {
+    $self->trans($trans);
   }
+  # Path to blast database
   $self->db("$PS_MULTI_EXON_DB"."multi_exon_blastdb.fasta");
   return $self;
 }
 
+=head2 run
 
+Arg [none] :
+  Description: calls run_blast for each transcript in turn
+  Returntype : none
+  Exceptions : none
+  Caller     : general
+
+=cut
 
 sub run {
   my ($self)=@_;
-  my @genes = @{$self->genes};
-  foreach my $gene (@genes){
-    foreach my $trans(@{$gene->get_all_Transcripts}){
-      if ($trans->translateable_seq){
-	$self->run_blast($trans);
-      }
+  my @trans = @{$self->trans};
+  foreach my $trans(@trans){
+    if ($trans->translateable_seq){
+      $self->run_blast($trans);
     }
   }
   return 1;
 }
 
+=head2 run_blast
+
+Arg [none] :
+  Description: runs the blast analysis, uses BPliteWapper as parser
+  Returntype : none
+  Exceptions : none
+  Caller     : general
+
+=cut
 
 sub  run_blast{
   my ($self,$trans)=@_;
@@ -66,40 +125,45 @@ sub  run_blast{
 
   $blast->run();
   my @array =($trans, $blast->output);
-  # maybe sort output based on evalue and chuck out dodgy hits
-  # or put them into the parameters for the blast?
-
   push @{$self->output},\@array;
   return 1;
 }
 
-
-
 ########################################################
 # Containers
 
-=head2 genes
+=head2 trans
 
-Arg [1]    : array ref
-  Description: get/set gene set to run over
-  Returntype : array ref to Bio::EnsEMBL::Gene objects
-  Exceptions : throws if not a Bio::EnsEMBL::Gene
+  Arg [1]    : array ref
+  Description: get/set transcript set to run over
+  Returntype : array ref to Bio::EnsEMBL::Transcript objects
+  Exceptions : throws if not a Bio::EnsEMBL::Transcript
   Caller     : general
 
 =cut
 
-sub genes {
-  my ($self, $genes) = @_;
-  if ($genes) {
-    foreach my $gene (@{$genes}) {
-      unless  ($gene->isa("Bio::EnsEMBL::Gene")){
-	$self->throw("Input isn't a Bio::EnsEMBL::Gene, it is a $gene\n$@");
+sub trans {
+  my ($self, $trans) = @_;
+  if ($trans) {
+    foreach my $tran (@{$trans}) {
+      unless  ($tran->isa("Bio::EnsEMBL::Transcript")){
+	$self->throw("Input isn't a Bio::EnsEMBL::Transcript, it is a $tran\n$@");
       }
     }
-    $self->{'_genes'} = $genes;
+    $self->{'_trans'} = $trans;
   }
-  return $self->{'_genes'};
+  return $self->{'_trans'};
 }
+
+=head2 db
+
+  Arg [1]    : scalar
+  Description: get/set path to blast database
+  Returntype : scalar
+  Exceptions : none
+  Caller     : general
+
+=cut
 
 sub db {
   my ($self, $db) = @_;
