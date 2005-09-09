6bd2cc3c12543e342666c49c2944041147550436
James Gilbert <jgrg@sanger.ac.uk>
2005-09-09 15:21:16 +0000

    *** empty log message ***

--- modules/Bio/EnsEMBL/Analysis/RunnableDB/Finished.pm
+++ modules/Bio/EnsEMBL/Analysis/RunnableDB/Finished.pm
@@ -16,53 +16,76 @@ sub new {
 
 sub write_output {
 	my ($self) = @_;
-	foreach my $runnable ( @{ $self->runnable } ) {
-		my $db_version = $runnable->get_db_version
-		  if $runnable->can('get_db_version');
-		$self->db_version_searched($db_version);    # make sure we set this here
+    
+    # We expect an array of arrays from output()
+    foreach my $output ($self->output) {
+        ### code db_version_searched method may be duplicated in several modules
+        ### How does it get written into the input_id_analysis table?
+	    foreach my $runnable ( @{ $self->runnable } ) {
+		    my $db_version = $runnable->get_db_version
+		      if $runnable->can('get_db_version');
+		    $self->db_version_searched($db_version);    # make sure we set this here
+	    }
 
-		if ( my @output = @{ $runnable->output } ) {
-			my $dbobj      = $self->db;
-			my $seqfetcher =
-			  Bio::EnsEMBL::Pipeline::SeqFetcher::Finished_Pfetch->new;
-			my %ids = map { $_->hseqname, 1 } @output;
-			my @ids_keys = keys(%ids);
-			$seqfetcher->write_descriptions( $dbobj, \@ids_keys );
-		}
+        next unless @$output;   # No feature output
+        
+        # The type of adaptor used to store the data depends
+        # upon the type of the first element of @$output
+        my $adaptor = $self->get_adaptor($output);
+        
+	    # Remove AlignFeatures already in db from each output
+        if (ref($output->[0]) =~ /AlignFeature$/) {
+            $self->remove_stored_AlignFeatures($adaptor, $output);
+            
+            $self->write_descriptions($output);
+        }
 
-	}
-	### Remove features already in db from each runnable
-	if(my $adaptor = $self->get_adaptor){ 
-	# if no adaptor is provided it means that there is no output feature
-		
-		## create a hashtable of key='feature signature' and value=1
-		my $db_features =
-		  $adaptor->fetch_all_by_Slice( $self->query,
-			$self->analysis->logic_name );
-		print STDERR "Found ", scalar(@$db_features), " features already in db\n";
-		my %db_feat = map { $self->get_feature_key($_), 1 } @$db_features;
-		## remove duplicated features
-		my $tmp_array = [];
-		my $ff        = $self->feature_factory;
-		my $output = $self->output;
-		for ( my $i = 0 ; $i < scalar(@$output) ; $i++ ) {
-			my $f = $output->[$i];
-			$f->analysis( $self->analysis );
-			$f->slice( $self->query ) if ( !$f->slice );
-			$ff->validate($f);
-			my $f_key = $self->get_feature_key($f);
-			if ( !$db_feat{ $f_key } ) {
-				push( @$tmp_array, $f );
-			}
-			else {
-				print "Feature ", $f_key , " already in table\n";
-			}
-		}
-		$self->{'output'} = $tmp_array;
-	}
+        # Store features in the database
+        my $analysis = $self->analysis;
+        my $slice    = $self->query;
+        my $ff       = $self->feature_factory;
+        foreach my $feature (@$output) {
+            $feature->analysis($analysis);
+            $feature->slice($slice) if (!$feature->slice);
+            $ff->validate($feature);
+            eval { $adaptor->store($feature); };
+            if ($@) {
+                throw("RunnableDB:store failed, failed to write " . $feature
+                      . " to the database: $@");
+            }
+        }
+    }
+}
 
-	$self->SUPER::write_output();
-	return;
+sub remove_stored_AlignFeatures {
+    my ($self, $adaptor, $output) = @_;
+
+    ## create a hashtable of key='feature signature' and value=1
+    my $db_features =
+      $adaptor->fetch_all_by_Slice($self->query, $self->analysis->logic_name);
+    print STDERR "Found ", scalar(@$db_features), " features already in db\n";
+    my %db_feat = map { $self->get_feature_key($_), 1 } @$db_features;
+    ## remove duplicated features
+    for (my $i = 0 ; $i < @$output ;) {
+        my $feature = $output->[$i];
+        my $f_key = $self->get_feature_key($feature);
+        if ($db_feat{$f_key}) {
+            print "Feature ", $f_key, " already in table\n";
+            splice(@$output, $i, 1);
+        }
+        else {
+            $i++;
+        }
+    }
+}
+
+sub write_descriptions {
+    my( $self, $output ) = @_;
+    
+    my $dbobj = $self->db;
+    my $seqfetcher = Bio::EnsEMBL::Pipeline::SeqFetcher::Finished_Pfetch->new;
+    my %ids = map { $_->hseqname, 1 } @$output;
+    $seqfetcher->write_descriptions( $dbobj, [keys %ids] );
 }
 
 sub get_feature_key {
@@ -78,23 +101,21 @@ sub get_feature_key {
 }
 
 sub get_adaptor {
-	my ($self) = @_;
-	my $feat;
-	if(!($feat = $self->output->[0])){ return 0; }
+	my ($self, $output) = @_;
+
+	my $feat = $output->[0];
+	
 	if ( $feat->isa('Bio::EnsEMBL::DnaPepAlignFeature') ) {
 		return $self->db->get_ProteinAlignFeatureAdaptor;
 	}
 	elsif ( $feat->isa('Bio::EnsEMBL::DnaDnaAlignFeature') ) {
 		return $self->db->get_DnaAlignFeatureAdaptor;
 	}
-	elsif ( $feat->isa('Bio::EnsEMBL::SimpleFeatures') ) {
-		return $self->db->get_SimpleFeatureAdaptor;
-	}
+    elsif ( $feat->isa('Bio::EnsEMBL::SimpleFeature') ) {
+        return $self->db->get_SimpleFeatureAdaptor;
+    }
 	else {
-		throw(
-'	feature must be either a Bio::EnsEMBL::DnaPepAlignFeature or a Bio::EnsEMBL::DnaDnaAlignFeature 
-	or a Bio::EnsEMBL::SimpleFeatures.'
-		);
+		throw('unexpected feature type: '. ref($feat));
 	}
 }
 
