34e10605bfa46ac7e9845d30c66a8073117b53a7
Mustapha Larbaoui <ml6@sanger.ac.uk>
2005-09-09 13:59:48 +0000

    a typo

--- modules/Bio/EnsEMBL/Analysis/RunnableDB/Finished/DepthFilter.pm
+++ modules/Bio/EnsEMBL/Analysis/RunnableDB/Finished/DepthFilter.pm
@@ -53,7 +53,7 @@ sub run {
 	my ( $filtered_features, $saturated_zones) =
         $self->depth_filter($orig_features, $slice, $max_coverage, $percentid_cutoff); 
 
-	$self->output($filtered_features, $saturated_zones);
+	$self->multi_output($filtered_features, $saturated_zones);
 
     # TODO:  and store the @$saturated_zones as simple features
 }
