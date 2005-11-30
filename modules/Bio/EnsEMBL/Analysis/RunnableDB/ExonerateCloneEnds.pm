4d88f8a745541a7d084c66ebe90ff1f826d51c36
Julio Fernandez Banet <jb16@sanger.ac.uk>
2005-11-30 16:52:53 +0000

    Update in the Map clone sequence module

---
+++ modules/Bio/EnsEMBL/Analysis/RunnableDB/ExonerateCloneEnds.pm
@@ -0,0 +1,482 @@
+
+=pod
+
+=head1 NAME
+
+Bio::EnsEMBL::Analysis::RunnableDB::Exonerate2Genes;
+
+
+
+
+=head1 SYNOPSIS
+
+my $clone = 
+  Bio::EnsEMBL::Analysis::RunnableDB::ExonerateCloneEnds->new(
+    -db         => $refdb,
+    -analysis   => $analysis_obj,
+    -database   => $EST_GENOMIC,
+    -query_seqs => \@sequences,
+  );
+
+$clone->fetch_input();
+$clone->run();
+$clone->write_output(); #writes to DB
+
+=head1 DESCRIPTION
+
+This object maps clone sequences to a genome,
+and writing the results as Features. 
+
+=head1 CONTACT
+
+Post general queries to B<ensembl-dev@ebi.ac.uk>
+
+=head1 APPENDIX
+
+=cut
+
+package Bio::EnsEMBL::Analysis::RunnableDB::ExonerateCloneEnds;
+
+use strict;
+use Bio::EnsEMBL::Utils::Exception qw(throw warning);
+use Bio::EnsEMBL::Analysis::RunnableDB;
+use Bio::EnsEMBL::Analysis::Runnable::ExonerateCloneEnds;
+use Bio::EnsEMBL::Analysis::Config::ExonerateCloneEnds;
+
+
+use vars qw(@ISA);
+
+@ISA = qw (Bio::EnsEMBL::Analysis::RunnableDB);
+
+############################################################
+sub new {
+  my ( $class, @args ) = @_;
+  my $self = $class->SUPER::new(@args);
+  
+  #Because we dont know whether the sort of adapter (compara, hive, core)
+  #we'll be passed, it's better to just remake the core dbadaptor and
+  #keep it on ourselves as the dna db - this has to be the intent of the
+  #dataadaptor input through the rulemanager / beekeeper / whatever
+
+  $self->read_and_check_config($CLONE_CONFIG);
+  return $self;
+}
+
+sub fetch_input {
+  my ($self) = @_;
+
+  my $logic = $self->analysis->logic_name;
+
+  if ($self->input_id =~ /\w+:\w+:(\w+):(\w+):(\w+):\w+:(\w+)/){ 
+   
+    my $target_id= $1;
+    my $target_start = $2;
+    my $target_end = $3;
+    my $clone_id = $4;
+
+    my $slice_adaptor = $self->DNADB->get_SliceAdaptor();
+
+    my $target_seq = $slice_adaptor->fetch_by_region('toplevel',$target_id, $target_start, $target_end);
+
+    my $seqfetcher = Bio::EnsEMBL::Pipeline::SeqFetcher::OBDAIndexSeqFetcher->new(
+                               -db => [('/ecs2/scratch2/jb16/sheep/sheep_clones_idx')],
+                               -format    => 'fasta', );
+
+    my $query_seq = $seqfetcher->get_Seq_by_acc($clone_id);
+
+    ##########################################
+    # set up the target (genome)
+    ##########################################
+
+    my $target = $target_seq;
+
+    ##########################################
+    # set up the query (dna clone seq)
+    ##########################################
+
+    my $query = $query_seq;
+ 
+    ##########################################
+    # setup the runnable
+    ##########################################
+
+    my %parameters = %{ $self->parameters_hash };
+  
+    if ( 
+      not exists( $parameters{-options} )
+      and defined $self->OPTIONS
+    ){
+      $parameters{-options} = '';
+    }
+    
+    $parameters{-options} = $self->OPTIONS;
+
+    print STDERR "PROGRAM FILE: ".$self->analysis->program_file."\n";
+  
+    my $runnable = Bio::EnsEMBL::Analysis::Runnable::ExonerateCloneEnds->new(
+      -program            => $self->analysis->program_file,
+      -analysis           => $self->analysis,
+      -target_file        => $target,
+      -query_type         => $self->QUERYTYPE,
+      -query_file         => $query,
+      %parameters,
+    );
+  
+    $self->runnable($runnable);
+  
+  }else{
+  
+    ##########################################
+    # set up the target (genome)
+    ##########################################
+
+    my $target = $self->GENOMICSEQS;
+
+    if ( -e $target ){
+      if(-d $target ) {
+        warn ("Target $target is a directory of files\n");
+      }elsif (-s $target){
+        warn ("Target $target is a whole-genome file\n");
+      }else{
+        throw("'$target' isn't a file or a directory?");
+      }
+    } else {
+      throw("'$target' could not be found");
+    }
+
+    ##########################################
+    # set up the query (dna clone seq)
+    ##########################################
+
+    my ($query_file, $chunk_number, $chunk_total);
+
+    my $query = $self->QUERYSEQS;
+
+    if ( -e $query and -s $query ) {
+
+      # query seqs is a single file; input id will correspond to a chunk number
+      $query_file = $query;
+      my $iid_regexp = $self->IIDREGEXP;
+
+      if (not defined $iid_regexp){
+        throw("You must define IIDREGEXP in config to enable inference of chunk number and total from your single fasta file" )
+      }
+
+      ( $chunk_number, $chunk_total ) = $self->input_id =~ /$iid_regexp/;
+      if(!$chunk_number || !$chunk_total){
+        throw "I can't make sense of your input id  using the IIDREGEXP in the config!\n";
+      }
+
+      #store this for reference later
+      $self->query_file($query_file);
+
+    } else {
+
+      throw("'$query'  must refer to a single fasta file with all probe sequences referenced by clone_probe_id\n");
+
+    }
+
+    ##########################################
+    # setup the runnable
+    ##########################################
+
+    my %parameters = %{ $self->parameters_hash };
+    
+    if ( 
+      not exists( $parameters{-options} )
+      and defined $self->OPTIONS
+    ){
+      $parameters{-options} = '';
+    }
+    
+    $parameters{-options} = $self->OPTIONS;
+    
+    print $self->OPTIONS,"\n";
+    print STDERR "PROGRAM FILE: ".$self->analysis->program_file."\n";
+
+    my $runnable = Bio::EnsEMBL::Analysis::Runnable::ExonerateCloneEnds->new(
+      -program            => $self->analysis->program_file,
+      -analysis           => $self->analysis,
+      -target_file        => $target,
+      -query_type         => $self->QUERYTYPE,
+      -query_file         => $query_file,
+      -query_chunk_number => $chunk_number,
+      -query_chunk_total  => $chunk_total,
+      %parameters,
+    );
+  
+    $self->runnable($runnable);
+
+  }
+}
+
+############################################################
+
+sub run {
+  my ($self) = @_;
+  my @clone_features;
+
+  throw("Can't run - no runnable objects") unless ( $self->runnable );
+
+  my $runnable = @{ $self->runnable }[0];
+  
+  $runnable->run;
+
+  @clone_features = @{$runnable->output};
+
+  #
+  #Replace the 'dummy' clone array and probe objects in the
+  #CloneFeature objects with the 'real' instances found in
+  #the populate... method
+  $self->output(\@clone_features);
+  $self->clone_features(\@clone_features);
+  $self->clean_clone_features(@{$self->clone_features});
+ 
+}
+
+############################################################
+
+sub write_output {
+  my ( $self, @output ) = @_;
+
+  my $outdb = $self->create_output_db;
+  my $clone_feature_adaptor = $outdb->get_DnaAlignFeatureAdaptor;
+
+  #Add analysis, slices to DnaAlign_features, and make
+  #sure they're pointing at the persistent array instances
+  #instead of the fake arrays  they were created with
+  $self->clean_clone_features(@{$self->clone_features});
+  
+  foreach my $clone_feature (@{$self->clone_features}){
+  
+    eval{ $clone_feature_adaptor->store($clone_feature) };
+    if ($@) {
+      $self->throw("Unable to store clone feature!\n $@");
+    }
+  }
+
+}
+
+############################################################
+
+sub clean_clone_features {
+  my ( $self, @clone_features ) = @_;
+
+  my $db = $self->create_output_db;
+  my $slice_adaptor = $db->get_SliceAdaptor;
+
+  my %genome_slices;
+
+  foreach my $clone_feature (@clone_features) {
+
+    $clone_feature->analysis( $self->analysis );
+
+    # get the slice based on the seqname stamped on in the runnable
+    my $slice_id = $clone_feature->seqname;
+
+    if ( not exists $genome_slices{$slice_id} ) {
+      # assumes genome seqs were named in the Ensembl API Slice naming
+      # convention, i.e. coord_syst:version:seq_reg_id:start:end:strand
+      $genome_slices{$slice_id} = $slice_adaptor->fetch_by_name($slice_id);
+    }
+    my $slice = $genome_slices{$slice_id};
+
+    $clone_feature->slice($slice); 
+ 
+  }
+return @clone_features;
+}
+
+sub query_file {
+  my ( $self, $value ) = @_;
+
+  if ( defined $value ) {
+    $self->{'_query_file'} = $value;
+  }
+
+  if ( exists( $self->{'_query_file'} ) ) {
+    return $self->{'_query_file'};
+  } else {
+    return undef;
+  }
+}
+
+sub create_output_db {
+  my ($self) = @_;
+
+  my $outdb;
+  my $dnadb;
+
+  if ( $self->OUTDB && $self->DNADB) {
+    $dnadb =  new Bio::EnsEMBL::DBSQL::DBAdaptor(%{ $self->OUTDB }); 
+
+    $outdb = new Bio::EnsEMBL::DBSQL::DBAdaptor(%{ $self->OUTDB }, 
+                                                  -dnadb => $dnadb 
+						);
+      
+  } elsif( $self->OUTDB) {
+    $outdb = new Bio::EnsEMBL::DBSQL::DBAdaptor(%{ $self->OUTDB }); 
+  } else {
+    $outdb = $self->db;
+  }
+
+  return $outdb;
+}
+
+
+#############################################################
+
+sub clone_features {
+  my ( $self, $value ) = @_;
+
+  if ( defined $value ) {
+    $self->{'_clone_features'} = $value;
+  }
+
+  if ( exists( $self->{'_clone_features'} ) ) {
+    return $self->{'_clone_features'};
+  } else {
+    return undef;
+  }
+}
+
+#############################################################
+# Declare and set up config variables
+#############################################################
+
+sub read_and_check_config {
+  my $self = shift;
+
+  $self->SUPER::read_and_check_config($CLONE_CONFIG);
+
+  ##########
+  # CHECKS
+  ##########
+  my $logic = $self->analysis->logic_name;
+
+  # check that compulsory options have values
+  foreach my $config_var (
+    qw(
+      QUERYSEQS
+      QUERYTYPE
+      GENOMICSEQS
+    )
+  ){
+    if ( not defined $self->$config_var ){
+      throw("You must define $config_var in config for logic '$logic'");
+    }
+  }
+
+  # output db does not have to be defined, but if it is, it should be a hash
+  if ($self->OUTDB && ref( $self->OUTDB ) ne "HASH") {
+    throw("OUTDB in config for '$logic' must be a hash ref of db connection pars.");
+  }
+  
+  if ( $self->DNADB and ref( $self->DNADB ) ne "HASH" ) {
+    throw("DNADB in config for '$logic' must be a hash ref of db connection pars.");
+  }
+}
+
+sub QUERYSEQS {
+  my ( $self, $value ) = @_;
+
+  if ( defined $value ) {
+    $self->{'_CONFIG_QUERYSEQS'} = $value;
+  }
+
+  if ( exists( $self->{'_CONFIG_QUERYSEQS'} ) ) {
+    return $self->{'_CONFIG_QUERYSEQS'};
+  } else {
+    return undef;
+  }
+}
+
+sub QUERYTYPE {
+  my ( $self, $value ) = @_;
+
+  if ( defined $value ) {
+    $self->{'_CONFIG_QUERYTYPE'} = $value;
+  }
+
+  if ( exists( $self->{'_CONFIG_QUERYTYPE'} ) ) {
+    return $self->{'_CONFIG_QUERYTYPE'};
+  } else {
+    return undef;
+  }
+}
+
+sub GENOMICSEQS {
+  my ( $self, $value ) = @_;
+
+  if ( defined $value ) {
+    $self->{'_CONFIG_GENOMICSEQS'} = $value;
+  }
+
+  if ( exists( $self->{'_CONFIG_GENOMICSEQS'} ) ) {
+    return $self->{'_CONFIG_GENOMICSEQS'};
+  } else {
+    return undef;
+  }
+}
+
+sub IIDREGEXP {
+  my ( $self, $value ) = @_;
+
+  if ( defined $value ) {
+    $self->{'_CONFIG_IIDREGEXP'} = $value;
+  }
+
+  if ( exists( $self->{'_CONFIG_IIDREGEXP'} ) ) {
+    return $self->{'_CONFIG_IIDREGEXP'};
+  } else {
+    return undef;
+  }
+}
+
+sub OUTDB {
+  my ( $self, $value ) = @_;
+
+  if ( defined $value ) {
+    $self->{'_CONFIG_OUTDB'} = $value;
+  }
+
+  if ( exists( $self->{'_CONFIG_OUTDB'} ) ) {
+    return $self->{'_CONFIG_OUTDB'};
+  } else {
+    return undef;
+  }
+}
+
+sub DNADB {
+  my ( $self, $value ) = @_;
+
+  if ( defined $value ) {
+    $self->{'_CONFIG_DNADB'} = $value;
+  }
+
+  if ( exists( $self->{'_CONFIG_DNADB'} ) ) {
+    return $self->{'_CONFIG_DNADB'};
+  } else {
+    return undef;
+  }
+}
+
+sub OPTIONS {
+  my ( $self, $value ) = @_;
+
+  if ( defined $value ) {
+    $self->{'_CONFIG_OPTIONS'} = $value;
+  }
+
+  if ( exists( $self->{'_CONFIG_OPTIONS'} ) ) {
+    return $self->{'_CONFIG_OPTIONS'};
+  } else {
+    return undef;
+  }
+}
+
+###############################################
+###     end of config
+###############################################
+
+1;
--- modules/Bio/EnsEMBL/Analysis/RunnableDB/MapCloneEnds.pm
+++ modules/Bio/EnsEMBL/Analysis/RunnableDB/MapCloneEnds.pm
@@ -25,192 +25,242 @@ package Bio::EnsEMBL::Analysis::RunnableDB::MapCloneEnds;
 use strict;
 use Bio::EnsEMBL::Utils::Exception qw(throw warning);
 use Bio::EnsEMBL::Analysis::RunnableDB;
-#use Bio::EnsEMBL::Analysis::RunnableDB::ExonerateClones;
-#use Bio::EnsEMBL::Analysis::RunnableDB::ExonerateRefinedCloneEnds;
-use Bio::EnsEMBL::Analysis::Config::MapCloneEnds;
+use Bio::EnsEMBL::Analysis::RunnableDB::ExonerateCloneEnds;
 
 use vars qw(@ISA);
 
 @ISA = qw (Bio::EnsEMBL::Analysis::RunnableDB);
 
+##########################################################################
 
+sub new {
+  my ( $class, @args ) = @_;
+  my $self = $class->SUPER::new(@args);
+
+  return $self;
+}
+
+##########################################################################
+
+sub fetch_input {
+ 
+# It does nothing so no really code implementation here.
+  my ($self) = @_;
+
+}
+
+##########################################################################
+
+sub run {
+  my ($self) = @_;
 
 # Run ExonerateClones to get a first idea on the aligned/location of the clone Ends 
- my $exonerate = Bio::EnsEMBL::Analysis::RunnableDB::ExonerateClones->new(
+  my $exonerate = Bio::EnsEMBL::Analysis::RunnableDB::ExonerateCloneEnds->new(
+    -DB          => $self->db,
+    -INPUT_ID    => $self->input_id,
+    -ANALYSIS    => $self->fetch_analysis("EXONERATE_CLONE_ENDS"),
   );
 
-$exonerate->fetch_input();
+  $exonerate->fetch_input();
 
-## Run exonerat and get the output.
-@clone_alignments = @{$exonerate->run()};
+  $exonerate->run();
 
+  ## Run exonerat and get the output.
+  my $clone_alignments = $exonerate->output();
 
-my  @ordered_alignments = sort{$a->hseqname() <=> $b->hseqname()} @clone_alignments;
+  my @selected_alignments = @{$self->filter_alignments($clone_alignments)};
 
-
-### Filter the results of the previous step and prepare data to send to the refine step.
-
-
-my %clone_cluster = ();
-
-# Group alignments by clone name
-foreach my $clone_alignment(@ordered_alignments){
-
-  my $clone_name = $clone_alignment->hseqname();
-
- if(!$clone_cluster{$clone_name}){
-    $clone_cluster{$clone_name} = [];
-  }
- push (@{$clone_cluster{$clone_name}}, $clone_alignment);
-}
-
-my @selected_alignments = [];
-
-# Cluster alignments obtained for each clone
-foreach my $clone_key(keys %clone_cluster){  
-  my @alignments = @{$clone_cluster{$clone_key}};
-
-  foreach my $alignment(@alignments){
- 
-  my %chr_cluster = ();
-  my $hit_name= $alignment->hseq_name;
-  my $chr_name= $alignment->seq_name;
+  my @refine_output;
+  foreach my $selected_alignment(@selected_alignments){
   
-# print $seqname,"\n";
+    my $clone_id=$selected_alignment->hseqname;
+    my $chr_id = $selected_alignment->seqname;
+    my $start = ($selected_alignment->start)-1000;
+    my $end = ($selected_alignment->end)+1000;
 
-   
-# Generate a unique key that will be found only when only clone aligns more than
-# once against the same chromosomal sequence
-  my $cluster_key = $chr_name."-".$hit_name;
+    my @chr_name = split (/:/, $chr_id);
+  
+    my $input_id = $chr_name[0].":".$chr_name[1].":".$chr_name[2].":".$start.":".$end.":".$chr_name[5].":".$clone_id;
 
-  # check if there is any sequence aligned from the same clone against the chromosome
-  # In case there isn't, start the count of alignments for that clone.
-  if(!$chr_cluster{$cluster_key}){
-    $chr_cluster{$cluster_key} = [];
-  }
-    push (@{$chr_cluster{$cluster_key}}, $alignment);
-
-  }
-
-  my $selected_alignment = "";
-  my $cluster_size = 0;
-  my $biggest_score = 0;
-
-# For each clone check how many clusters were obtained.
-# Check the clusters and selected the best alignment cluster
-# to be used as template for the more exhaustive exonerate alignment.
-
-  foreach my $chr_cluster_key (keys %chr_cluster){
-    # If the cluster contains more than one sequence, order the sequences and check if they are
-    # separated by less than 1000 bases. If the separation is bigger don't count them as part of the cluster
-    if (scalar(@{$chr_cluster{$chr_cluster_key}})>1){
-      # counter to get the real number of alignment within the cluster.
-      my $size_counter = 0;
-
-      # get the first aligment of the cluster to be used in the exonerate step
-      my $first_selected_alignment;
-
-      # order the alignments in the clster to be able to check the real distance among them
-      my  @ordered_aligns = sort{$a->hstart() <=> $b->hstart()} @{$chr_cluster{$chr_cluster_key}};
-     
-
-      my $previous_end = 0;
-      my $previous_alignment;
-
-      my $end_of_cluster = 0;
-     
-
-      # Use this value to avoid the comparison of the first alignment against null, that in case the start
-      # point of the alignment is lower than the cutoff which lead to the clustering of the first alignment
-      # with a NULL alignment
-      my $first_alignment = 0;
+    my $refine = Bio::EnsEMBL::Analysis::RunnableDB::ExonerateCloneEnds->new(
+      -DB          => $self->db,
+      -INPUT_ID    => $input_id,
+      -ANALYSIS    => $self->fecth_analysis("REFINE_CLONE_ENDS"),
+    );
     
-      
-      foreach my $ordered_align(@ordered_aligns){
-        
-        # if the two condiditions are acomplished check if the alignments form a cluster
-        if ($first_alignment!=0 && $end_of_cluster == 0){
-        
-          # if the distance is sorter that the threshold the alignments cluster
-          if((($ordered_align->start)-($previous_end))<1000 && (($ordered_align->start)-($previous_end))>0){
-            
-            if(!$cluster_alignments{$ordered_align->hseqname}){
-              $cluster_alignments{$ordered_align->hseqname} = [];
-              push (@{$cluster_alignments{$ordered_align->hseqname}}, $previous_alignment);
-              $size_counter++;  
-              $first_selected_alignment = $previous_alignment;
-            }
-            push (@{$cluster_alignments{$ordered_align->hseqname}}, $ordered_align);
-            $size_counter++;
-         
-          }elsif($cluster_alignments{$ordered_align->hseqname}){
-            $end_of_cluster = 1;
-            
-	  }
-          $previous_alignment = $ordered_align;
-          $previous_end = $ordered_align->end;
-        }
-
-        # Change this value after the first alignment is checked.
-        if ($first_alignment == 0){
-          $first_alignment = 1;
-          $previous_alignment = $ordered_align;
-          $previous_end = $ordered_align->end;
-        }
-      }
-      if ($size_counter > $cluster_size){
-        $cluster_size= $size_counter;
-        $selected_alignment = $
-
-      } 
-    }
-    elsif($cluster_size==0 && {$chr_cluster{$chr_cluster_key}}[0]->score()> $biggest_score){
-     $biggest_score = {$chr_cluster{$chr_cluster_key}}[0]->score();
-     $selected_alignment = $chr_cluster{$chr_cluster_key}[0];
-    }
+    $self->refine_object($refine);
+    $refine ->fetch_input();
+    $refine ->run();
+    push (@refine_output, $refine->output());
   }
-  push (@selected_alignments, $selected_alignment); 
+  return @refine_output;
 }
+
+##########################################################################
+
+sub write_output {
+
+  my ( $self, @output ) = @_;
  
-#### Prepare to run ExonerateRefineCloneEnds.pm
-
-use strict;
-use Bio::EnsEMBL::DBSQL::DBAdaptor;
-use Bio::EnsEMBL::Utils::Slice qw(split_Slices);
-
-
-my $db = Bio::EnsEMBL::DBSQL::DBAdaptor->new(
-        -user   => 'ensro',
-        -dbname => 'homo_sapiens_core_36_35i',
-        -host   =>  'ecs2',
-        -port   =>  '3364',
-        -driver => 'mysql');
-
-my  $slice_adaptor = $db->get_SliceAdaptor();
+  foreach my $runnable_output(@output){
+    $self->refine->write_output($runnable_output);
+  }
+}
 
 
-####
-# foreach $selected_alignment(@selectted_alignemnts){
-#  
-# my $clone_id=$selected_alignment->hseqname;
-# my $chr_id = $selected_alignment->seqname;
-# my $start = ($selected_alignment->start)-1000;
-# my $end = ($selected_alignment->end)+1000;
+############################################################################################
+
+sub fetch_analysis{
+  my ($self, $logic_name) = @_;
+ 
+  my  $db = $self->db;
+
+  my $analysis_adaptor= $db->get_AnalysisAdaptor;
+
+  my $analysis = $analysis_adaptor->fetch_by_logic_name($logic_name);
+  
+  return $analysis;
+}
+
+sub refine_object{
+  my ($self, $refine_object) = @_;
+  
+  if(!$self->{'refine'}){
+    $self->{'refine'}='';
+  }
+  
+  $self->{'refine'}= $refine_object;
+  return $self->{'refine'};
+
+}
+
+sub filter_alignments{
+  my ($self, $clone_alignments) = @_;
+ 
+  my  @ordered_alignments = sort{$a->hseqname() <=> $b->hseqname()} @{$clone_alignments};
 
 
-#It has to be run for one sequence at a time so it might go inside a foreach loop??
+  ### Filter the results of the previous step and prepare data to send to the refine step.
 
-my $slices = $slice_adaptor->fetch_by_region('chromosome',$chr, $start, $end);
-#my $slices = $slice_adaptor->fetch_by_region('supercontig',$chr, $start, $end);
+  my %clone_cluster = ();
 
-my $logic = $self->analysis->logic_name;
+  # Group alignments by clone name
+  foreach my $clone_alignment(@ordered_alignments){
+   
+    my $clone_name = $clone_alignment->hseqname();
 
- my $refine = Bio::EnsEMBL::Analysis::RunnableDB::ExonerateRefinedCloneEnds->new(
+    print $clone_name,"\n";
 
-  );
+    if(!$clone_cluster{$clone_name}){
+      $clone_cluster{$clone_name} = [];
+    }
+    push (@{$clone_cluster{$clone_name}}, $clone_alignment);
+  }
 
-$refine ->fetch_input();
-$refine ->run();
-$refine ->write_output();
+  my @selected_alignments = [];
 
-#}
+  # Cluster alignments obtained for each clone
+  foreach my $clone_key(keys %clone_cluster){  
+    my @alignments = @{$clone_cluster{$clone_key}};
+    my %chr_cluster = ();
+  
+    foreach my $alignment(@alignments){
+   
+      my $hit_name= $alignment->hseqname;
+      my $chr_name= $alignment->seqname;
+  
+      # print $seqname,"\n";
+   
+      # Generate a unique key that will be found only when only clone aligns more than
+      # once against the same chromosomal sequence
+      my $cluster_key = $chr_name."-".$hit_name;
+
+      # check if there is any sequence aligned from the same clone against the chromosome
+      # In case there isn't, start the count of alignments for that clone.
+      if(!$chr_cluster{$cluster_key}){
+        $chr_cluster{$cluster_key} = [];
+      }
+      push (@{$chr_cluster{$cluster_key}}, $alignment);
+    }
+
+    my $selected_alignment = "";
+    my $cluster_size = 0;
+    my $biggest_score = 0;
+
+  # For each clone check how many clusters were obtained.
+  # Check the clusters and selected the best alignment cluster
+  # to be used as template for the more exhaustive exonerate alignment.
+
+    foreach my $chr_cluster_key (keys %chr_cluster){
+      # If the cluster contains more than one sequence, order the sequences and check if they are
+      # separated by less than 1000 bases. If the separation is bigger don't count them as part of the cluster
+      if (scalar(@{$chr_cluster{$chr_cluster_key}})>1){
+        # counter to get the real number of alignment within the cluster.
+        my $size_counter = 0;
+
+        # get the first aligment of the cluster to be used in the exonerate step
+        my $first_selected_alignment;
+
+        # order the alignments in the clster to be able to check the real distance among them
+        my  @ordered_aligns = sort{$a->hstart() <=> $b->hstart()} @{$chr_cluster{$chr_cluster_key}};    
+
+        my $previous_end = 0;
+        my $previous_alignment;
+
+        my $end_of_cluster = 0;
+     
+        my %cluster_alignments = ();
+
+        # Use this value to avoid the comparison of the first alignment against null, that in case the start
+        # point of the alignment is lower than the cutoff which lead to the clustering of the first alignment
+        # with a NULL alignment
+        my $first_alignment = 0;
+      
+        foreach my $ordered_align(@ordered_aligns){
+        
+          # if the two condiditions are acomplished check if the alignments form a cluster
+          if ($first_alignment!=0 && $end_of_cluster == 0){
+        
+            # if the distance is sorter that the threshold the alignments cluster
+            if((($ordered_align->start)-($previous_end))<1000 && (($ordered_align->start)-($previous_end))>0){
+            
+              if(!$cluster_alignments{$ordered_align->hseqname}){
+                $cluster_alignments{$ordered_align->hseqname} = [];
+                push (@{$cluster_alignments{$ordered_align->hseqname}}, $previous_alignment);
+                $size_counter++;  
+                $first_selected_alignment = $previous_alignment;
+              }
+              push (@{$cluster_alignments{$ordered_align->hseqname}}, $ordered_align);
+              $size_counter++;
+         
+            }elsif($cluster_alignments{$ordered_align->hseqname}){
+              $end_of_cluster = 1;
+            
+	    }
+            $previous_alignment = $ordered_align;
+            $previous_end = $ordered_align->end;
+          }
+
+          # Change this value after the first alignment is checked.
+          if ($first_alignment == 0){
+            $first_alignment = 1;
+            $previous_alignment = $ordered_align;
+            $previous_end = $ordered_align->end;
+          }
+        }
+        if ($size_counter > $cluster_size){
+          $cluster_size= $size_counter;
+          $selected_alignment = $first_selected_alignment;
+
+        } 
+      }
+      elsif($cluster_size==0 && ${$chr_cluster{$chr_cluster_key}}[0]->score()> $biggest_score){
+        $biggest_score = ${$chr_cluster{$chr_cluster_key}}[0]->score();
+        $selected_alignment = $chr_cluster{$chr_cluster_key}[0];
+      }
+    }
+    push (@selected_alignments, $selected_alignment); 
+  }
+  return \@selected_alignments;
+}
+
