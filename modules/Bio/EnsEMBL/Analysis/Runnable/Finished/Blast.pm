cc55ed076b09d7aab785a4007b63a5a835c75a8c
Mustapha Larbaoui <ml6@sanger.ac.uk>
2006-01-16 10:40:38 +0000

    Code formatting & replace fetch_databases to databases

--- modules/Bio/EnsEMBL/Analysis/Runnable/Finished/Blast.pm
+++ modules/Bio/EnsEMBL/Analysis/Runnable/Finished/Blast.pm
@@ -8,90 +8,103 @@ use Bio::EnsEMBL::Analysis::Config::Blast;
 use Bio::EnsEMBL::Utils::Exception qw(throw warning);
 use base ("Bio::EnsEMBL::Analysis::Runnable::Blast");
 
-
 $ENV{BLASTDB} = '/data/blastdb/Ensembl';
 
 BEGIN {
-    print "\nUSING " . __PACKAGE__ . "\n\n";
+	print "\nUSING " . __PACKAGE__ . "\n\n";
 }
-
 
 sub get_analysis {
 
-    my ($self) = @_;
+	my ($self) = @_;
 
-    my ($ana);
-    unless ( $ana = $self->{'_analysis'} ) {
-        my ($source) = $self->program =~ m{([^/]+)$}
-          or throw( "Can't parse last element from path: '" . $self->program . "'" );
-        $ana = $self->{'_analysis'} = Bio::EnsEMBL::Analysis->new(
-            -db              => $self->database,
-            -db_version      => 1,                 # ARUUGA!!!
-            -program         => $source,
-            -program_version => 1,
-            -gff_source      => $source,
-            -gff_feature     => 'similarity',
-            -logic_name      => 'blast',
-        );
-    }
-    return $ana;
+	my ($ana);
+	unless ( $ana = $self->{'_analysis'} ) {
+		my ($source) = $self->program =~ m{([^/]+)$}
+		  or throw(
+			"Can't parse last element from path: '" . $self->program . "'" );
+		$ana = $self->{'_analysis'} = Bio::EnsEMBL::Analysis->new(
+			-db              => $self->database,
+			-db_version      => 1,                 # ARUUGA!!!
+			-program         => $source,
+			-program_version => 1,
+			-gff_source      => $source,
+			-gff_feature     => 'similarity',
+			-logic_name      => 'blast',
+		);
+	}
+	return $ana;
 }
 
+sub parse_results {
 
-sub parse_results{
-
-  my ($self) = @_;
-  my $results = $self->results_files;
-  my $bplites = $self->parser->parse_files($results);
-  my $threshold_type  = $self->parser->threshold_type;
-  my $threshold = $self->parser->threshold;
-  my $discard_overlaps = $self->parser->discard_overlaps;
-  my $coverage = $self->parser->coverage;
-  my $hits    = $self->parser->get_best_hits($bplites,$threshold_type,$threshold);
-  my $query_length = $self->query->length or throw("Couldn't get query length");
-  my $output = $self->parser->_apply_coverage_filter( $query_length, $hits,$threshold_type,$threshold,$coverage,$discard_overlaps );
-  $self->output($output);
-  return $output;
+	my ($self)           = @_;
+	my $results          = $self->results_files;
+	my $bplites          = $self->parser->parse_files($results);
+	my $threshold_type   = $self->parser->threshold_type;
+	my $threshold        = $self->parser->threshold;
+	my $discard_overlaps = $self->parser->discard_overlaps;
+	my $coverage         = $self->parser->coverage;
+	my $hits             =
+	  $self->parser->get_best_hits( $bplites, $threshold_type, $threshold );
+	my $query_length = $self->query->length
+	  or throw("Couldn't get query length");
+	my $output =
+	  $self->parser->_apply_coverage_filter( $query_length, $hits,
+		$threshold_type, $threshold, $coverage, $discard_overlaps );
+	$self->output($output);
+	return $output;
 }
 
+sub databases {
+	my ( $self, @vals ) = @_;
 
-sub fetch_databases {
-
-    my ($self) = @_;
-    my @databases;
-    my $db_names = $self->database; 
-    $db_names =~ s/\s//g;
-
-    foreach my $dbname(split(",", $db_names)){ # allows the use of a comma separated list in $self->database
-	# prepend the environment variable $BLASTDB if
-	# database name is not an absoloute path
-	unless ($dbname =~ m!^/!) {
-	    $dbname = $ENV{BLASTDB} . "/" . $dbname;
+	if ( not exists $self->{databases} ) {
+		$self->{databases} = [];
 	}
-	# If the expanded database name exists put this in
-	# the database array.
-	#
-	# If it doesn't exist then see if $database-1,$database-2 exist
-	# and put them in the database array
-	if (-f $dbname) {
-	    push(@databases,$dbname);
-	    $self->get_db_version( $dbname );
-	} else {
-	    my $count = 1;
-            my $db_filename;
-	    while (-f ( $db_filename = "${dbname}-${count}" )) {
-		push(@databases, $db_filename);
-		$self->get_db_version( $db_filename );
-		$count++;
-	    }
-	    $! = undef; # to stop pollution as it will be "No such file or directory" after while loop above.
-	}
-    }
-    if (scalar(@databases) == 0) {
-	throw("No databases exist for " . $db_names);
-    }
 
-    return \@databases;
+	foreach my $val (@vals) {
+		my $db_names = $val;
+		my @databases;
+		$db_names =~ s/\s//g;
+
+		foreach my $dbname ( split( ",", $db_names ) )
+		{    # allows the use of a comma separated list in $self->database
+			    # prepend the environment variable $BLASTDB if
+			    # database name is not an absoloute path
+			unless ( $dbname =~ m!^/! ) {
+				$dbname = $ENV{BLASTDB} . "/" . $dbname;
+			}
+
+			# If the expanded database name exists put this in
+			# the database array.
+			#
+			# If it doesn't exist then see if $database-1,$database-2 exist
+			# and put them in the database array
+			if ( -f $dbname ) {
+				push( @databases, $dbname );
+				$self->get_db_version($dbname);
+			}
+			else {
+				my $count = 1;
+				my $db_filename;
+				while ( -f ( $db_filename = "${dbname}-${count}" ) ) {
+					push( @databases, $db_filename );
+					$self->get_db_version($db_filename);
+					$count++;
+				}
+				$! = undef
+				  ; # to stop pollution as it will be "No such file or directory" after while loop above.
+			}
+		}
+		if ( scalar(@databases) == 0 ) {
+			throw( "No databases exist for " . $db_names );
+		} else {
+			push @{$self->{databases}}, @databases;
+		}
+	}
+
+	return $self->{databases};
 
 }
 
@@ -111,47 +124,59 @@ sub fetch_databases {
 
 =cut
 
-sub get_db_version{
-    my ($self, $db) = @_;
-    my $debug_this = 1; # this just shows debug info.
-    my $force_dbi  = 0; # this will force a dbi call SLOW!!!!!!
-    unless($self->{'_db_version_searched'}){
-        if($db){
-            $BlastableVersion::debug = $debug_this;            
-            warning "BlastableVersion is cvs revision $BlastableVersion::revision \n" if $debug_this;
-            
-            my $ver = eval { 
-                my $blast_ver = BlastableVersion->new();
-                $blast_ver->force_dbi($force_dbi); # if set will be SLOW.
-                $blast_ver->get_version($db);
-                $blast_ver;
-            };
-            throw("I failed to get a BlastableVersion for $db") if $@;
-            
-            my $dbv = $ver->version();
-            my $sgv = $ver->sanger_version();
-            my $name = $ver->name();
-            my $date = $ver->date();
-            unless ($dbv){
-                throw(
-                    "I know nothing about $db I tried to find out:\n" .
-                    " - name <" . $name . ">\n" .
-                    " - date <" . $date . ">\n" .
-                    " - version <" . $dbv . ">\n" .
-                    " - sanger_version <" . $sgv . ">\n");
-            }
-            $self->{'_db_version_searched'} = $dbv;
-        }else{
-            throw("You've asked about what I searched, but I don't know." . 
-                         " It's not set. I need to be called with a database filename first");
-            # The code probably got here because of a problem with the MLDBM
-            # cache file on the machine this was running on.  
-            # the cache file is stored @ /var/tmp/blast_versions
-            # try <rm -f /var/tmp/blast_versions>
-        }
-    }
-    return $self->{'_db_version_searched'};
+sub get_db_version {
+	my ( $self, $db ) = @_;
+	my $debug_this = 1;    # this just shows debug info.
+	my $force_dbi  = 0;    # this will force a dbi call SLOW!!!!!!
+	unless ( $self->{'_db_version_searched'} ) {
+		if ($db) {
+			$BlastableVersion::debug = $debug_this;
+			warning
+			  "BlastableVersion is cvs revision ".$BlastableVersion::revision." \n"
+			  if $debug_this;
+
+			my $ver = eval {
+				my $blast_ver = BlastableVersion->new();
+				$blast_ver->force_dbi($force_dbi);    # if set will be SLOW.
+				$blast_ver->get_version($db);
+				$blast_ver;
+			};
+			throw("I failed to get a BlastableVersion for $db") if $@;
+
+			my $dbv  = $ver->version();
+			my $sgv  = $ver->sanger_version();
+			my $name = $ver->name();
+			my $date = $ver->date();
+			unless ($dbv) {
+				throw(  "I know nothing about $db I tried to find out:\n"
+					  . " - name <". $name . ">\n"
+					  . " - date <". $date . ">\n"
+					  . " - version <". $dbv . ">\n"
+					  . " - sanger_version <". $sgv. ">\n" );
+			}
+			$self->{'_db_version_searched'} = $dbv;
+		}
+		else {
+			throw(  "You've asked about what I searched, but I don't know."
+				  . " It's not set. I need to be called with a database filename first"
+			);
+
+			# The code probably got here because of a problem with the MLDBM
+			# cache file on the machine this was running on.
+			# the cache file is stored @ /var/tmp/blast_versions
+			# try <rm -f /var/tmp/blast_versions>
+		}
+	}
+	return $self->{'_db_version_searched'};
 }
+
+sub DESTROY 
+{
+	my ( $self ) = @_;
+	# just do the cleanup
+	$self->delete_files;	
+}
+
 1;
 
 __END__
