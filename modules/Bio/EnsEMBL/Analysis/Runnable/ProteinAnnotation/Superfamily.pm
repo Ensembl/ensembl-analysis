c7113c0fe72f5fc76e947c8e41910e9a66f747fa
Kevin Howe <klh@ebi.ac.uk>
2006-03-10 10:32:28 +0000

    Extended to use Superfamily->SCOP mappings (if given)

--- modules/Bio/EnsEMBL/Analysis/Runnable/ProteinAnnotation/Superfamily.pm
+++ modules/Bio/EnsEMBL/Analysis/Runnable/ProteinAnnotation/Superfamily.pm
@@ -29,6 +29,9 @@ package Bio::EnsEMBL::Analysis::Runnable::ProteinAnnotation::Superfamily;
 use vars qw(@ISA);
 use strict;
 
+use Bio::EnsEMBL::Utils::Exception qw(verbose throw warning info );
+use Bio::EnsEMBL::Utils::Argument qw( rearrange );
+
 use Bio::EnsEMBL::Analysis::Runnable::ProteinAnnotation::Hmmpfam;
 use Bio::EnsEMBL::Analysis::Tools::ProteinAnnotationFilter;
 
@@ -36,14 +39,61 @@ use Bio::EnsEMBL::Analysis::Tools::ProteinAnnotationFilter;
 
 
 #################################
+
+sub new {
+  my ($class, @args) = @_;
+
+  my $self = $class->SUPER::new(@args);
+
+  my ($scop_map_file,
+      $evalue) = rearrange(['SCOPMAP',
+                            'EVALUE',
+                            ], @args);
+    
+  $self->scop_map($scop_map_file)
+      if defined $scop_map_file;
+
+  if (defined $evalue) {
+    $self->evalue_cutoff($evalue);
+    my $opts = defined($self->options) ? $self->options : "";
+    $self->options("$opts -E " . $evalue);
+  } 
+
+  return $self;
+}
+
+
 sub parse_results {
   my ($self) = @_;
 
   $self->SUPER::parse_results;
+
+  # although the search will have been done with the
+  # required evalur cutoff, we have to filter again here
+  # on the basis of per-domain evalue (as opposed to 
+  # whole sequence evalue)
+
+  my @pass_eval;
+  foreach my $hit (@{$self->output}) {
+    if (not defined $self->evalue_cutoff or 
+        $hit->p_value <= $self->evalue_cutoff) {
+      push @pass_eval, $hit;
+    }
+  }
   
   my $filter = Bio::EnsEMBL::Analysis::Tools::ProteinAnnotationFilter->new();  
-  my $out = $filter->filter_results($self->output);
+  my $out = $filter->filter_results(\@pass_eval);
   #my $out = $self->output;
+
+  # finally, change the names to scop ids if we have the mappings
+  if ($self->scop_map) {
+    foreach my $hit (@$out) {
+      if (exists($self->scop_map->{$hit->hseqname})) {
+        my $scopname = $self->scop_map->{$hit->hseqname};
+        $hit->hseqname( $scopname );
+      }
+    }
+  }
 
   $self->output($out);
 }
@@ -64,5 +114,38 @@ sub output {
 
   return $self->{output};
 }
+
+sub scop_map {
+  my ($self, $file) = @_;
+
+  my %scop_map;
+
+  if (defined $file) {
+    open SCOPMAP, $file or throw("Could not open SCOP mapfile '$file'");
+    while(<SCOPMAP>) {
+      /^(\S+)\s+(\S+)/ and $scop_map{$1} = $2;
+    }
+    close(SCOPMAP);
+    
+    $self->{_scop_map} = \%scop_map;
+  }
+
+  return $self->{_scop_map};
+}
+
+sub evalue_cutoff {
+  my ($self, $val) = @_;
+
+  if (defined $val) {
+    $self->{_evalue_cutoff} = $val;
+  }
+
+  if (not exists $self->{_evalue_cutoff}) {
+    return undef;
+  } else {
+    return $self->{_evalue_cutoff};
+  }
+}
+
 
 1;
