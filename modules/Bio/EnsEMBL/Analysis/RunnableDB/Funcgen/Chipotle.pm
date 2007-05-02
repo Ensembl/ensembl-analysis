4e32b293945c4f90b6ae94eef52a07f050729b3a
Stefan Graef <graef@sanger.ac.uk>
2007-05-02 16:31:08 +0000

    initial efg chipotle analysis import

---
+++ modules/Bio/EnsEMBL/Analysis/RunnableDB/Funcgen/Chipotle.pm
@@ -0,0 +1,239 @@
+# Ensembl module for Bio::EnsEMBL::Analysis::RunnableDB::Chipotle
+#
+# Copyright (c) 2007 Ensembl
+#
+
+=head1 NAME
+
+Bio::EnsEMBL::Analysis::RunnableDB::Funcgen::Chipotle
+
+=head1 SYNOPSIS
+
+  my $chipotle = Bio::EnsEMBL::Analysis::RunnableDB::Funcgen::Chipotle->new
+     (
+        -input_id => 'chromosome::20:1:100000:1',
+        
+     );
+  $chipotle->fetch_input;
+  $chipotle->run;
+  $chipotle->write_output;
+
+=head1 DESCRIPTION
+
+This module provides an interface between the ensembl database and
+the Runnable Dust which wraps the program tcdust
+
+This module can fetch appropriate input from the database
+pass it to the runnable then write the results back to the database
+in the predicted_feature table with respect to the resut_set, data_set 
+and feature_set table
+
+=head1 AUTHOR
+
+This module was created by Stefan Graf. It is part of the 
+Ensembl project: http://www.ensembl.org/
+
+=head1 CONTACT
+
+Post questions to the Ensembl development list: ensembl-dev@ebi.ac.uk
+
+=cut
+
+package Bio::EnsEMBL::Analysis::RunnableDB::Funcgen::Chipotle;
+
+use strict;
+use warnings;
+use Data::Dumper;
+
+use Bio::EnsEMBL::Analysis::RunnableDB;
+use Bio::EnsEMBL::Analysis::RunnableDB::Funcgen;
+use Bio::EnsEMBL::Analysis::Runnable::Funcgen::Chipotle;
+
+use Bio::EnsEMBL::Analysis::Config::Funcgen::Chipotle;
+
+use Bio::EnsEMBL::Utils::Exception qw(throw warning);
+use vars qw(@ISA); 
+
+@ISA = qw(Bio::EnsEMBL::Analysis::RunnableDB::Funcgen);
+
+
+
+=head2 new
+
+  Arg [1]     : 
+  Arg [2]     : 
+  Description : Instantiates new Chipotle runnabledb
+  Returntype  : Bio::EnsEMBL::Analysis::RunnableDB::Funcgen::Chipotle object
+  Exceptions  : 
+  Example     : 
+
+=cut
+
+sub new {
+  my ($class,@args) = @_;
+  my $self = $class->SUPER::new(@args);
+  
+  $self->read_and_check_config($CHIPOTLE_CONFIG);
+
+  # Chipotle analysis defaults
+  $self->analysis->description('Sliding window approach bases on Gaussian '.
+                               'background model method for significance '.
+                               'estimation.');
+  $self->analysis->display_label('ChIPOTle');
+
+  #print Dumper $self;
+
+  return $self;
+}
+
+################################################################################
+# Declare and set up config variables
+################################################################################
+
+sub read_and_check_config {
+  my $self = shift;
+
+  $self->SUPER::read_and_check_config($CHIPOTLE_CONFIG);
+
+  ##########
+  # CHECKS
+  ##########
+
+  my $logic = $self->analysis->logic_name;
+
+  # check that compulsory options have values
+  foreach my $config_var (
+    qw(
+       PROGRAM
+       OPTIONS
+       LOGIC_NAME
+       EFG_EXPERIMENT
+       EFG_ANALYSIS
+       EFG_FT_NAME
+       EFG_FT_CLASS
+       EFG_CT_NAME
+    )
+  ){
+    if ( not defined $self->$config_var ){
+      throw("You must define $config_var in config.");
+    }
+  }
+}
+
+sub PROGRAM {
+  my ( $self, $value ) = @_;
+
+  if ( defined $value ) {
+    $self->{'_CONFIG_PROGRAM'} = $value;
+  }
+
+  if ( exists( $self->{'_CONFIG_PROGRAM'} ) ) {
+    return $self->{'_CONFIG_PROGRAM'};
+  } else {
+    return undef;
+  }
+}
+
+sub OPTIONS {
+  my ( $self, $value ) = @_;
+
+  if ( defined $value ) {
+    $self->{'_CONFIG_OPTIONS'} = $value;
+  }
+
+  if ( exists( $self->{'_CONFIG_OPTIONS'} ) ) {
+    return $self->{'_CONFIG_OPTIONS'};
+  } else {
+    return undef;
+  }
+}
+
+sub LOGIC_NAME {
+  my ( $self, $value ) = @_;
+
+  if ( defined $value ) {
+    $self->{'_CONFIG_LOGIC_NAME'} = $value;
+  }
+
+  if ( exists( $self->{'_CONFIG_LOGIC_NAME'} ) ) {
+    return $self->{'_CONFIG_LOGIC_NAME'};
+  } else {
+    return undef;
+  }
+}
+
+sub EFG_EXPERIMENT {
+  my ( $self, $value ) = @_;
+
+  if ( defined $value ) {
+    $self->{'_CONFIG_EFG_EXPERIMENT'} = $value;
+  }
+
+  if ( exists( $self->{'_CONFIG_EFG_EXPERIMENT'} ) ) {
+    return $self->{'_CONFIG_EFG_EXPERIMENT'};
+  } else {
+    return undef;
+  }
+}
+
+sub EFG_ANALYSIS {
+  my ( $self, $value ) = @_;
+
+  if ( defined $value ) {
+    $self->{'_CONFIG_EFG_ANALYSIS'} = $value;
+  }
+
+  if ( exists( $self->{'_CONFIG_EFG_ANALYSIS'} ) ) {
+    return $self->{'_CONFIG_EFG_ANALYSIS'};
+  } else {
+    return undef;
+  }
+}
+
+sub EFG_FT_NAME {
+  my ( $self, $value ) = @_;
+
+  if ( defined $value ) {
+    $self->{'_CONFIG_EFG_FT_NAME'} = $value;
+  }
+
+  if ( exists( $self->{'_CONFIG_EFG_FT_NAME'} ) ) {
+    return $self->{'_CONFIG_EFG_FT_NAME'};
+  } else {
+    return undef;
+  }
+}
+
+sub EFG_FT_CLASS {
+  my ( $self, $value ) = @_;
+
+  if ( defined $value ) {
+    $self->{'_CONFIG_EFG_FT_CLASS'} = $value;
+  }
+
+  if ( exists( $self->{'_CONFIG_EFG_FT_CLASS'} ) ) {
+    return $self->{'_CONFIG_EFG_FT_CLASS'};
+  } else {
+    return undef;
+  }
+}
+
+sub EFG_CT_NAME {
+  my ( $self, $value ) = @_;
+
+  if ( defined $value ) {
+    $self->{'_CONFIG_EFG_CT_NAME'} = $value;
+  }
+
+  if ( exists( $self->{'_CONFIG_EFG_CT_NAME'} ) ) {
+    return $self->{'_CONFIG_EFG_CT_NAME'};
+  } else {
+    return undef;
+  }
+}
+
+#############################################################
+###     end of config
+#############################################################
+
+1;
