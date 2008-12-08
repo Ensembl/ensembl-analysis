c3d238d5941444710702c2be2dd84eb45013a33c
Julio Fernandez Banet <jb16@sanger.ac.uk>
2008-12-08 14:15:58 +0000

    Fixed a bug that was assigning the wrong marker synonym - marker_id link when two markers have the same name.

--- scripts/markers/load_markers.pl
+++ scripts/markers/load_markers.pl
@@ -72,10 +72,10 @@ my $write = 1;
             'source:s'      => \$source,
             'help!'         => \$help,
             'write!'        => \$write,
-	   ) or ($help = 1);
+            ) or ($help = 1);
 
 if ($help) {
-    exec('perldoc', $0);
+  exec('perldoc', $0);
 }
 
 if(!$host || !$name || !$user){
@@ -90,17 +90,17 @@ my $db = new Bio::EnsEMBL::DBSQL::DBAdaptor(
 					    '-dbname' => $name,
 					    '-pass'   => $pass,
 					    '-port'   => $port,
-					   );
+                                            );
 
 my $query          = "insert into marker(left_primer, right_primer, min_primer_dist, ".
-                     "max_primer_dist, priority, type) values (?,?,?,?,1,'est')";
+    "max_primer_dist, priority, type) values (?,?,?,?,1,'est')";
 my $sth            = $db->dbc->prepare($query);
 my $syn_sql        = "insert into marker_synonym (marker_id, source, name) values(?, ?, ?)";
 my $syn_sth        = $db->dbc->prepare($syn_sql);
 my $disp_query     = "update marker set display_marker_synonym_id=? where marker_id=?";
 my $disp_sth       = $db->dbc->prepare($disp_query);
 my $display_id_sql = "select marker_synonym_id from marker_synonym ".
-                     "where name = ? and source = ?";
+    "where name = ? and source = ? and marker_id = ?";
 my $disp_id_sth    = $db->dbc->prepare($display_id_sql);
 
 open FP, $file or throw("Failed opening".$file);
@@ -114,23 +114,28 @@ my @row = $lenght_sth->fetchrow_array();
 $fieldlength = $row[1];
 $fieldlength =~ /\w+\(([\d]+)\)/;
 $fieldlength = $1 or warn "\nCould not verify lenght of name field in table marker_synonym".
-                          "\nplease check manually!\n\n";
+    "\nplease check manually!\n\n";
+
 if($fieldlength){
   while (<FP>) {
+    my @line = split /\t/, $_;
+    if(scalar(@line) != 8){
+      print "Somethig wrong: ",join (" ",@line),"\n"; 
+    }
     my ($display_id, $lprim, $rprim, $dist, $name,
-	$junk, $acc, $species) = split /\t/, $_;
+        $junk, $acc, $species) = @line;
     if($name){
       my @names = split /\;/, $name;
       foreach my $name(@names){
-	if(length($name) > $maxlen){
-	  $maxlen = length($name);
-	}
+        if(length($name) > $maxlen){
+          $maxlen = length($name);
+        }
       }
     }
   }
   if($maxlen > $fieldlength){
     print STDERR "\nERROR: Length of name field in table marker_synonym has length ".
-          $fieldlength.", longest entry in file has length ".$maxlen."\n".
+        $fieldlength.", longest entry in file has length ".$maxlen."\n".
 	  "Suggested correction:\n".
 	  "mysql -u$user -p$pass -h$host -P$port -D$name -e".
 	  "'alter table marker_synonym modify name varchar(".($maxlen+1).");'\n\n";
@@ -141,20 +146,24 @@ if($fieldlength){
 
 seek(FP, 0, 0);
 
-MARKER:
-
-while (<FP>) {
+MARKER: while (<FP>) {
   my ($display_id, $lprim, $rprim, $dist, $name, 
       $junk, $acc, $species) = split /\t/, $_;
   my %names;
+  my @dists = split /-/, $dist;
   #insert marker
-  $sth->execute($lprim, $rprim, $dist, $dist) if($write);
+  if (scalar(@dists)>1){
+    $sth->execute($lprim, $rprim, $dists[0], $dists[1]) if($write);
+  }else{
+    $sth->execute($lprim, $rprim, $dist, $dist) if($write);
+  }
   my $mid = $sth->{mysql_insertid};
   my $dis_source = $source."_NUM";
   #insert marker_synonym
   $syn_sth->execute($mid, $dis_source, $display_id) if($write);
   my @names = split /\;/, $name if($name);
   my $display_name;
+  my $check_source;
   foreach my $id(@names){
     if($id && $id ne '-'){
       $display_name = $id if(!$display_name);
@@ -162,7 +171,7 @@ while (<FP>) {
       $names{$id} = 1;
     }
   }
-  my $check_source = $source;
+  $check_source = $source;
   my @accs = split  /\;/, $acc if($acc);
   foreach my $id(@accs){
     if($id && $id ne '-'){
@@ -178,19 +187,20 @@ while (<FP>) {
     $display_name = $display_id;
     $check_source =  $source."_NUM";
   }
-  $disp_id_sth->execute($display_name, $check_source) if($write);
+  #$disp_id_sth->execute($display_name, $check_source) if($write);
+  $disp_id_sth->execute($display_name, $check_source, $mid) if($write);
   my ($dis_syn_id) = $disp_id_sth->fetchrow;
   if(!$dis_syn_id && $write){
     print "SQL = ".$display_id_sql." ".$display_name." ".$source." didnt ".
-          "get an id\n";
+        "get an id\n";
     exit;
   }
   if($dis_syn_id == $mid && $write){
-    print "Dis syn id and mid seem be both be $dis_syn_id:$mid\n";
+    print "Display synonym id and marker id are both the same:  $dis_syn_id:$mid\n";
     exit;
   }
   $disp_sth->execute($dis_syn_id, $mid) if($write);
-
+  
 }
 
 close(FP) or throw("Failed to close ".$file);
