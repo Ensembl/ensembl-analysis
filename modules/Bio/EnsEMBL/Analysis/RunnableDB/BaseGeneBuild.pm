94184eb8c1332d7020f391645d712f8189c79c9b
Mustapha Larbaoui <ml6@sanger.ac.uk>
2009-07-21 11:07:42 +0000

    remove the dependency to compara

--- modules/Bio/EnsEMBL/Analysis/RunnableDB/BaseGeneBuild.pm
+++ modules/Bio/EnsEMBL/Analysis/RunnableDB/BaseGeneBuild.pm
@@ -1,5 +1,5 @@
 #This is to be a baseclass for the genebuild code to have various
-#object like methods which genebuild modules want. GeneBuild code 
+#object like methods which genebuild modules want. GeneBuild code
 #should ideally inherit from this
 
 
@@ -7,9 +7,8 @@ package Bio::EnsEMBL::Analysis::RunnableDB::BaseGeneBuild;
 
 use strict;
 use Bio::EnsEMBL::Utils::Exception qw(throw warning verbose);
-use Bio::EnsEMBL::Analysis::Tools::Logger qw(logger_info); 
+use Bio::EnsEMBL::Analysis::Tools::Logger qw(logger_info);
 use Bio::EnsEMBL::Pipeline::DBSQL::DBAdaptor;
-use Bio::EnsEMBL::Compara::DBSQL::DBAdaptor;
 use Bio::EnsEMBL::DBSQL::DBAdaptor;
 use Bio::EnsEMBL::Analysis::Config::Databases qw(DATABASES DNA_DBNAME);
 use Bio::EnsEMBL::Analysis::RunnableDB;
@@ -39,11 +38,11 @@ sub database_hash{
   Arg [2]   : return a pipeline db adaptor flag
   Function  : Returns a Bio::EnsEMBL::DBSQL::DBAdaptor for a given hash key.
               or a Bio::EnsEMBL::Pipeline::DBSQL::DBAdaptor if requested
-              Requires proper configuration of 
-              Bio::EnsEMBL::Analysis::Config::Databases 
- 
+              Requires proper configuration of
+              Bio::EnsEMBL::Analysis::Config::Databases
+
   Returntype: Bio::EnsEMBL:DBSQL::DBAdaptor or Bio::EnsEMBL::Pipeline::DBSQL::DBAdaptor
-  Exceptions: throw if key can't be found in Databases.pm 
+  Exceptions: throw if key can't be found in Databases.pm
 
 =cut
 
@@ -53,11 +52,11 @@ sub get_dbadaptor{
   my $db;
   if(!$hash->{$name}){
     if (exists $DATABASES->{$name}) {
-      my $constructor_args = $DATABASES->{$name}; 
+      my $constructor_args = $DATABASES->{$name};
 
-      foreach my $arg ( qw ( -user -port -host -dbname) ) {  
-        unless ( $$constructor_args{$arg}){ 
-          throw ("Database-connection-details not properly configured : Argument : $arg missing in Databases.pm\n") ; 
+      foreach my $arg ( qw ( -user -port -host -dbname) ) {
+        unless ( $$constructor_args{$arg}){
+          throw ("Database-connection-details not properly configured : Argument : $arg missing in Databases.pm\n") ;
         }
       }
       if ( $use_pipeline_adaptor ) {
@@ -66,6 +65,7 @@ sub get_dbadaptor{
 							    %$constructor_args,
 	  						   );
          } elsif ( $use_pipeline_adaptor == 2 || $use_pipeline_adaptor eq "compara" ) {
+         	require Bio::EnsEMBL::Compara::DBSQL::DBAdaptor;
             $db = Bio::EnsEMBL::Compara::DBSQL::DBAdaptor->new(
                                                             %$constructor_args,
                                                               );
@@ -76,13 +76,13 @@ sub get_dbadaptor{
 						 );
       }
       if($name ne $DNA_DBNAME ){
-        if (length($DNA_DBNAME) ne 0 ){  
+        if (length($DNA_DBNAME) ne 0 ){
           if ( $not_use_dna_database ) {
-# if two different species are considered, the not_use_dna_database is set to 1 to avoid adding the second species to the first one 
-          } else { 
-           my $dnadb = $self->get_dbadaptor($DNA_DBNAME); 
-           $db->dnadb($dnadb); 
-         } 
+# if two different species are considered, the not_use_dna_database is set to 1 to avoid adding the second species to the first one
+          } else {
+           my $dnadb = $self->get_dbadaptor($DNA_DBNAME);
+           $db->dnadb($dnadb);
+         }
         }else{
           warning("You haven't defined a DNA_DBNAME in Config/Databases.pm");
         }
