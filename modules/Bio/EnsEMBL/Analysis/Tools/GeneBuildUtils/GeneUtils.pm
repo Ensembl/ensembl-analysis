f6f88bd539051487e60876bfca9b4e04ccba2a12
Jan-hinnerk Vogel <jhv@sanger.ac.uk>
2009-10-19 15:57:24 +0000

    uses add_ORF_to_transcript() from TranslationUtils

--- modules/Bio/EnsEMBL/Analysis/Tools/GeneBuildUtils/GeneUtils.pm
+++ modules/Bio/EnsEMBL/Analysis/Tools/GeneBuildUtils/GeneUtils.pm
@@ -62,7 +62,7 @@ use vars qw (@ISA  @EXPORT);
 
 use Bio::EnsEMBL::Utils::Exception qw(verbose throw warning stack_trace_dump);
 use Bio::EnsEMBL::Analysis::Tools::GeneBuildUtils::TranscriptUtils qw(print_Transcript clone_Transcript get_evidence_ids attach_Slice_to_Transcript fully_load_Transcript empty_Transcript attach_Analysis_to_Transcript attach_Analysis_to_Transcript_no_support print_Transcript_and_Exons);
-use Bio::EnsEMBL::Analysis::Tools::GeneBuildUtils::TranslationUtils qw(run_translate); 
+use Bio::EnsEMBL::Analysis::Tools::GeneBuildUtils::TranslationUtils qw(run_translate add_ORF_to_transcript ); 
 use Bio::EnsEMBL::Analysis::Tools::GeneBuildUtils qw (id coord_string empty_Object);
 use Bio::EnsEMBL::Analysis::Tools::GeneBuildUtils::ExonUtils;
 use Bio::EnsEMBL::Gene;
@@ -535,101 +535,13 @@ sub compute_6frame_translations{
     my @met_predictions = @{run_translate ($transcript, 1)};
     my @nomet_predictions = @{run_translate ($transcript)};
 
-    my @ne_exons = @{$transcript->get_all_Exons} ;
-
-
-
     ORF: for my $orf ( @met_predictions, @nomet_predictions ) {
       # create new transcript for every ORF 
-      my $nt = new Bio::EnsEMBL::Transcript( -EXONS => \@ne_exons );
-
-    #Here we take the best prediction with a methionine unless 
-    #there aren't any of the best prediction without a 
-    #methoinine is more than twice the length
-
-    my $orf_start = $orf->[1];
-    my $orf_end = $orf->[2];
-    my $translation = Bio::EnsEMBL::Translation->new();
-
-    my ($translation_start, $translation_end, $translation_start_Exon, $translation_end_Exon);
-     my $exon_count = 0;
-    my $pos = 1;
-    foreach my $exon(@{$nt->get_all_Exons}){
-      $exon_count++;
-      if ( $orf_start >= $pos && $orf_start <= $pos
-           + $exon->length - 1 ){
-        $translation_start = $orf_start - $pos+1;
-        $translation_start_Exon = $exon;
-      }
-      if($orf_end >= $pos && $orf_end <= $pos
-         + $exon->length - 1){
-        $translation_end = $orf_end - $pos + 1;
-        $translation_end_Exon = $exon;
-      }
-      $pos += $exon->length;
-    }
-    if(!$translation_start || !$translation_end || !$translation_start_Exon || !$translation_end_Exon){
-      warning("problems making the translation ".  "for ".id($nt));
-      push @new_transcripts, $nt ;
-    }else{
-      $translation->start($translation_start);
-      $translation->end($translation_end);
-      $translation->start_Exon($translation_start_Exon);
-      $translation->end_Exon($translation_end_Exon);
-      $nt->translation($translation);
-    }
-
-    my $found_start = 0;
-    my $found_end = 0;
-  my $last_end_phase;
-    my $first_exon = 1;
-    # print "Setting phases on transcript after adding translation\n";
-    foreach my $exon(@{$nt->get_all_Exons}){
-      $exon->phase(-1);
-      $exon->end_phase(-1);
-
-      # print "  Have exon " . $exon->start . " " . $exon->end . "\n";
-      if($translation->start_Exon == $exon){
-        if($translation->start == 1 && $first_exon){
-          $exon->phase(0);
-          # print "   setting start phase on it to 0 (tstart = 1 and is start_Exon)\n";
-        }
-        $found_start = 1; 
-      }elsif($found_start and not $found_end){
-        $exon->phase($last_end_phase);
-        # print "   setting start phase on it to last_end_phase ($last_end_phase)\n";
-      }
-      my $end_phase;
-      if($exon == $translation->start_Exon){
-        $end_phase = ($exon->end - ($exon->start + 
-                                    $translation->start
-                                    - 1) +1 ) %3;
-        # print "   start_Exon end phase calculated ($end_phase)\n";
-      }else{
-        $end_phase = (($exon->length + $exon->phase) %3);
-        # print "   end phase calculated ($end_phase)\n";
-      }
-      if(($exon == $translation->end_Exon && $exon->length == $translation->end)){
-        # print "   setting end phase to $end_phase (end exon condition)\n";
-        $exon->end_phase($end_phase);
-      }
-
-      $found_end = 1 if($exon == $translation->end_Exon);
-
-      if (($found_start and !$found_end)) {
-        # print "   setting end phase to $end_phase (found_start and not found_end condition)\n";
-        $exon->end_phase($end_phase);
-      }
-
-      $last_end_phase = $exon->end_phase;
-      $first_exon = 0;
+      my $nt = new Bio::EnsEMBL::Transcript( -EXONS => $transcript->get_all_Exons) ; 
+      $nt = add_ORF_to_transcript($orf, $nt) ;  
+      push @new_transcripts, $nt ;  
     } 
-    push @new_transcripts, $nt ;  
-    # make one gene with mult transcripts with 6 open-reading 
-    #my $new_gene = Bio::EnsEMBL::Gene->new(); 
-    #$new_gene->add_Transcript($nt) ; 
-   } # ORF  
- } # next transcript 
+ }
     
  my $new_gene = Bio::EnsEMBL::Gene->new();
  $new_gene->biotype("");
