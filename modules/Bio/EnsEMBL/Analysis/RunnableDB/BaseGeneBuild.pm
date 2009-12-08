481f10f8ac7ffa174a8026224deaed3b8060a541
Amonida Zadissa <amonida@ebi.ac.uk>
2009-12-08 14:10:19 +0000

    Small changes in the warnings that are given when REFEREE_DB and DNA_DB are the same plus some formatting.

--- modules/Bio/EnsEMBL/Analysis/RunnableDB/BaseGeneBuild.pm
+++ modules/Bio/EnsEMBL/Analysis/RunnableDB/BaseGeneBuild.pm
@@ -49,99 +49,130 @@ sub database_hash{
 
 =cut
 
-sub get_dbadaptor{
-  my ($self, $name, $non_standard_db_adaptor , $not_use_dna_database) = @_; 
+sub get_dbadaptor {
+  my ( $self, $name, $non_standard_db_adaptor, $not_use_dna_database ) = @_;
 
-  my ($db, $try_to_attach_dna_db) ;  
+  my ( $db, $try_to_attach_dna_db );
+  my $hash = $self->database_hash;
 
-  my $hash = $self->database_hash; 
-
-  if(!$hash->{$name}){
-    if (exists $DATABASES->{$name}) {
+  if ( !$hash->{$name} ) {
+    if ( exists $DATABASES->{$name} ) {
       my $constructor_args = $DATABASES->{$name};
 
-      # check if we got all arguments 
-      foreach my $arg ( qw ( -user -port -host -dbname) ) {
-        unless ( $$constructor_args{$arg}){
-          throw ("Database-connection-details not properly configured : Argument : $arg missing in Databases.pm for $name \n") ;
+      # check if we got all arguments
+      foreach my $arg (qw ( -user -port -host -dbname)) {
+        unless ( $$constructor_args{$arg} ) {
+          throw(   "Database-connection-details not properly configured : "
+                 . "Argument : $arg missing in Databases.pm for $name \n" );
         }
-      } 
-      if ( defined $non_standard_db_adaptor) { 
-      if ( $non_standard_db_adaptor =~m/1/ || $non_standard_db_adaptor eq "pipeline") { 
-         require Bio::EnsEMBL::Pipeline::DBSQL::DBAdaptor; 
-         $db = Bio::EnsEMBL::Pipeline::DBSQL::DBAdaptor->new( %$constructor_args,); 
-
-      } elsif ( $non_standard_db_adaptor =~m/compara/ ) {
-         require Bio::EnsEMBL::Compara::DBSQL::DBAdaptor; 
-         unless ( $$constructor_args{'-species'}){  
-             throw("need species !\n"); 
-          } 
-         $db = Bio::EnsEMBL::Compara::DBSQL::DBAdaptor->new( %$constructor_args,); 
-
-      } elsif ( $non_standard_db_adaptor =~m/functgenomics/i || $non_standard_db_adaptor =~m/funcgen/i) {
-         require Bio::EnsEMBL::Funcgen::DBSQL::DBAdaptor; 
-         # funcgen adaptor needs species   
-         if ( $$constructor_args{'-species'}) { 
-            $db = Bio::EnsEMBL::Funcgen::DBSQL::DBAdaptor->new( %$constructor_args,); 
-         }else {  
-           throw("if you require a Bio::EnsEMBL::Funcgen::DBSQL::DBAdaptor you need to provide a -speices flag \n" ) ; 
-         } 
       }
-      }  else {
-         $db = Bio::EnsEMBL::DBSQL::DBAdaptor->new( %$constructor_args); 
-         # it's a core db so try to attach dna_db 
-         $try_to_attach_dna_db = 1 ; 
-     } 
+      if ( defined $non_standard_db_adaptor ) {
+        if (    $non_standard_db_adaptor =~ m/1/
+             || $non_standard_db_adaptor eq "pipeline" )
+        {
+          require Bio::EnsEMBL::Pipeline::DBSQL::DBAdaptor;
+          $db =
+            Bio::EnsEMBL::Pipeline::DBSQL::DBAdaptor->new( %$constructor_args );
 
-     #  this bit is attaching a dna_db . 
-       
-     if($name ne $DNA_DBNAME  && length($DNA_DBNAME) ne 0 && $try_to_attach_dna_db ){
-        if ( $not_use_dna_database ) {
-          # if two different species are considered, the not_use_dna_database is set to 1 to avoid adding the second species to the first one
-        } else { 
-         # there's a little danger if we have multiple diffent species in our "Databases.pm" file. 
-         # We need to avoid that the wrong dna db is attached, ie a mouse core with a human dna db. 
+        } elsif ( $non_standard_db_adaptor =~ m/compara/ ) {
+          require Bio::EnsEMBL::Compara::DBSQL::DBAdaptor;
+          unless ( $$constructor_args{'-species'} ) {
+            throw("need species !\n");
+          }
+          $db =
+            Bio::EnsEMBL::Compara::DBSQL::DBAdaptor->new( %$constructor_args );
 
-         my $dnadb = $self->get_dbadaptor($DNA_DBNAME);  
+        } elsif (    $non_standard_db_adaptor =~ m/functgenomics/i
+                  || $non_standard_db_adaptor =~ m/funcgen/i )
+        {
+          require Bio::EnsEMBL::Funcgen::DBSQL::DBAdaptor;
+          # funcgen adaptor needs species
+          if ( $$constructor_args{'-species'} ) {
+            $db = Bio::EnsEMBL::Funcgen::DBSQL::DBAdaptor->new(
+                                                        %$constructor_args, );
+          } else {
+            throw( "if you require a Bio::EnsEMBL::Funcgen::DBSQL::DBAdaptor "
+                   . "you need to provide a -speices flag.\n" );
+          }
+        }
+      } else {
+        $db = Bio::EnsEMBL::DBSQL::DBAdaptor->new(%$constructor_args);
+        # it's a core db so try to attach dna_db
+        $try_to_attach_dna_db = 1;
+      }
 
-         # try to get default asm+ species name for OTHER db - does not work for comapra database 
-         my $core_db_asm = $db->get_MetaContainer->get_default_assembly();    
-         my $core_db_species = $db->get_MetaContainer->get_Species->binomial();  
+      # this bit is attaching a dna_db .
+      if (    $name ne $DNA_DBNAME
+           && defined($DNA_DBNAME)
+           && $try_to_attach_dna_db )
+      {
+        if ($not_use_dna_database) {
+          print "\nNot attaching a DNA_DB to $name\n";
+          # if two different species are considered, the
+          # not_use_dna_database is set to 1 to avoid adding the second species to
+          # the first one
 
-        # get the same for dna-db 
-         my $dna_db_asm = $dnadb->get_MetaContainer->get_default_assembly();      
-         my $dna_db_species = $dnadb->get_MetaContainer()->get_Species->binomial(); 
-   
-         my $dna_db_and_core_db_are_compatible = 1 ; 
-   
-         unless ( $core_db_asm eq $dna_db_asm ) { 
-           warning ("you try to add  a DNA_DB with assembly $dna_db_asm to a core/cdna/otherfeatures DB with assembly $core_db_asm ...\n\t".
-                      "that's incompatbile. I will not add dna_database " . $dnadb->dbname . " to core " . $db->dbname . "\n"); 
-           $dna_db_and_core_db_are_compatible = 0 ; 
-         }    
-   
-         unless ( $core_db_species eq $dna_db_species ) { 
-            warning ("you try to add  a DNA_DB with species $dna_db_species to a core database with speices : $core_db_species - this does not work\n\t".
-                     "that's incompatbile. try to not use any DNA_DATABASE name in Analysis/Config/Databases.pm\n"); 
-            $dna_db_and_core_db_are_compatible = 0 ; 
-         }  
-         if ( $dna_db_and_core_db_are_compatible ) {  
-              $db->dnadb($dnadb);    
-              print "adding dna_db " . $dnadb->dbname . " to " . $db->dbname . "\n" ;  
-         } 
-       } 
-    }else{
+        } else {
+          # there's a little danger if we have multiple diffent
+          # species in our "Databases.pm" file. We need to avoid that the wrong
+          # dna db is attached, ie a mouse core with a human dna db.
+
+          print "\nAttaching DNA_DB to $name...\n";
+          my $dnadb = $self->get_dbadaptor($DNA_DBNAME);
+
+          # try to get default asm+ species name for OTHER db - does not work
+          # for comapra database
+          my $core_db_asm = $db->get_MetaContainer->get_default_assembly();
+          my $core_db_species =
+            $db->get_MetaContainer->get_Species->binomial();
+
+          # get the same for dna-db
+          my $dna_db_asm = $dnadb->get_MetaContainer->get_default_assembly();
+          my $dna_db_species =
+            $dnadb->get_MetaContainer()->get_Species->binomial();
+
+          my $dna_db_and_core_db_are_compatible = 1;
+
+          unless ( $core_db_asm eq $dna_db_asm ) {
+            warning( "you try to add  a DNA_DB with assembly $dna_db_asm to "
+              . "a core/cdna/otherfeatures DB with assembly $core_db_asm ...\n\t"
+              . "that's incompatbile. I will not add dna_database "
+              . $dnadb->dbname . " to core " . $db->dbname . "\n" );
+
+            $dna_db_and_core_db_are_compatible = 0;
+          }
+
+          unless ( $core_db_species eq $dna_db_species ) {
+            warning( "you try to add a DNA_DB with species $dna_db_species to "
+                . "a core database with speices: $core_db_species - this does not work\n"
+                . "try to not use any DNA_DATABASE name in Analysis/Config/Databases.pm\n"
+            );
+            $dna_db_and_core_db_are_compatible = 0;
+          }
+          if ($dna_db_and_core_db_are_compatible) {
+            $db->dnadb($dnadb);
+            print "\nAttaching DNA_DB "
+              . $dnadb->dbname . " to "
+              . $db->dbname . "\n";
+          }
+        } ## end else [ if ($not_use_dna_database)
+      } else {
+        if ( $name eq $DNA_DBNAME ) {
+          print "\nDNA_DB and REFEREE_DB are the same"
+              . " no need to attach the DNA_DB...\n";
+        } else {
           warning("You haven't defined a DNA_DBNAME in Config/Databases.pm ");
+        }
+      }
+    } else {
+      throw("No entry in Config/Databases.pm hash for $name");
     }
-   } else { 
-    throw("No entry in Config/Databases.pm hash for $name");
-   }
-  $self->database_hash($name, $db); 
-  } else{
+    $self->database_hash( $name, $db );
+  } else {
     $db = $hash->{$name};
   }
   return $db;
-}
+} ## end sub get_dbadaptor
 
 
 
