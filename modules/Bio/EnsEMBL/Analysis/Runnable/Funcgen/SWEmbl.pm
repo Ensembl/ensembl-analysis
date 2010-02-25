8d91aae274c7949709cad76fb092379a09073ad1
Nathan Johnson <njohnson@ebi.ac.uk>
2010-02-25 09:43:23 +0000

    Various tweaks including is_gzipped, sam support, better output, tweaking of output fields due to regex parsing, better otuput file naming

--- modules/Bio/EnsEMBL/Analysis/Runnable/Funcgen/SWEmbl.pm
+++ modules/Bio/EnsEMBL/Analysis/Runnable/Funcgen/SWEmbl.pm
@@ -51,6 +51,7 @@ use Bio::EnsEMBL::Analysis::Runnable::Funcgen;
 
 use Bio::EnsEMBL::Utils::Exception qw(throw warning);
 use Bio::EnsEMBL::Utils::Argument qw( rearrange );
+use Bio::EnsEMBL::Funcgen::Utils::EFGUtils qw (get_file_format is_gzipped);
 
 use vars qw(@ISA);
 @ISA = qw(Bio::EnsEMBL::Analysis::Runnable::Funcgen);
@@ -68,14 +69,13 @@ use vars qw(@ISA);
 
 sub run {
 
-    print "Bio::EnsEMBL::Analysis::Runnable::Funcgen::SWEmbl::run\n";
     my ($self, $dir) = @_;
 
     $self->run_analysis;
 
 
     print "Parsing results ... ";
-    $self->parse_results();
+    $self->parse_results();#This is using the generic Runnable::Funcgen::parse_results!
     print "done!\n";
 
 }
@@ -102,21 +102,65 @@ sub run_analysis {
 
     throw($program." is not executable") if(! ($program && -x $program));
 
-    my @fields = (0..2,5,9);
+    my @fields = (1,2,5,9);
+	#removed seq_region_name 0 as this is now matched by the regex 
+	#in Runnable::Funcgen dependant no the input format
+	#Could infact just extend the regex to do the output field assignment instead of the split
+
     $self->output_fields(\@fields);
+	
+	#Override default random file naming as we want to be able to look at the output easily
+	#This will not make the directory if it does not exist!!
+	#(my $resultsfile = $self->infile());
 
-    (my $resultsfile = $self->infile());
-    $self->resultsfile($resultsfile);
-    warn("RESULTS FILE: ".$resultsfile);
+	#Need to checkdir exists anyway and also set to logic_name, not module!
+	#This needs moving to Runnable::Funcgen?
+	#And handling feature_set name where appropriate?
+	
+	
+	my $results_dir = $self->workdir.'/'.$self->analysis->logic_name;
+	my $suffix = &get_file_format($self->query);
+	my $results_file = $self->query;
+	$results_file =~ s/.*\///;	
+	$suffix .= '.gz' if &is_gzipped($self->query, 1);
+	$results_file =~ s/${suffix}$/dat/;
+	$results_file = $results_dir.'/'.$results_file;
+	
+
+	if(! -d $results_dir){
+	  system("mkdir -p $results_dir") && throw("Could not create results dir:\t$results_dir");
+	}
+	elsif(-e $results_file){
+	  #This will stop us overwriting
+	  throw("Results file already exists, please move away before running:\t$results_file");
+	}
+
+	
+
+
+
+    $self->resultsfile($results_file);
+    print "RESULTS FILE: ".$results_file."\n";
     
-    my $command = $self->program . ' -B -z -i ' . $self->query . ' ' . 
-        $self->options . ' -o ' . $resultsfile;
+	my %fswitches = (
+					 bed   => '-B',
+					 sam   => '-S',
+					 maq   => '-M',
+					 eland => '-E',
+					 #bam   => '-?',
+					);
+	my $format_switch = $fswitches{&get_file_format($self->query)};
+	my $zip_switch = (&is_gzipped($self->query, 1)) ? '-z' : '';
+		
+	throw("Could not identify valid SWEmbl input format") if(! $format_switch);
+	
+    my $command = $self->program . " $format_switch $zip_switch -i " . $self->query . ' ' . 
+        $self->options . ' -o ' . $results_file;
     
-    warn("Running analysis " . $command);
-
-    eval { system($command) };
-    throw("FAILED to run $command: ", $@) if ($@);
-
+    print "Running analysis:\t$command\n";
+    system($command) && throw("FAILED to run $command: ", $@);
+	print "Finished analysis:\t$command\n";
+	
 }
 
 
