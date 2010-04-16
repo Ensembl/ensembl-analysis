e37074f0f75c803c7cc62c3e42fcbe45521152de
Amonida Zadissa <amonida@ebi.ac.uk>
2010-04-16 10:32:22 +0000

    * Added an option for DNA database. * Added proper POD * Some reformatting of code

--- scripts/genebuild/copy_genes.pl
+++ scripts/genebuild/copy_genes.pl
@@ -1,20 +1,37 @@
-#!//usr/local/ensembl/bin/perl-w
+=pod
 
-#this script takes database options and a file of gene ids and copies them between two 
-#databases. It can if asked split multi transcript genes into single genes
-#
-#an example commandline might look like
-#
-#perl copy_genes.pl -in_config_name COALESCER_DB -out_config_name UTR_DB gene_ids_to_copy
-#
-# or
-#
-#perl copy_genes.pl -dbhost host -dbuser ensro -dbname est_db -outhost host1 -outuser ensadmin -outpass ensembl -outdbname utr_db gene_ids_to_copy
-#
-#When using the in and out_config_name options it reads the equivalent database from the
-#Bio::EnsEMBL::Analysis::Config::GeneBuild::Databases file
+NAME
+
+  copy_genes.pl
+
+DESCRIPTION
+
+ This script takes database options and a file of gene ids and copies
+ them between two databases. It can if asked split multi transcript
+ genes into single genes.
+
+ When using the in and out_config_name options
+ it reads the equivalent database from the
+ Bio::EnsEMBL::Analysis::Config::GeneBuild::Databases file.
+
+EXAMPLE
+
+ perl copy_genes.pl -in_config_name COALESCER_DB -out_config_name UTR_DB \
+   -file gene_ids_to_copy
+
+ or
+
+ perl copy_genes.pl -dbhost host -dbuser ensro -dbname est_db -outhost host1 \
+   -outuser ensadmin -outpass ensembl -outdbname utr_db -file gene_ids_to_copy
+
+
+=cut
+
+#!/usr/local/ensembl/bin/perl
 
 use strict;
+use warnings;
+use Carp;
 use Bio::EnsEMBL::DBSQL::DBAdaptor;
 use Bio::EnsEMBL::Analysis::Tools::GeneBuildUtils;
 use Bio::EnsEMBL::Analysis::Tools::GeneBuildUtils::GeneUtils;
@@ -45,40 +62,58 @@ my $out_config_name;
 my $split = 0;
 my $infile;
 
-&GetOptions(
-            'dbhost:s'        => \$host,
-            'dbuser:s'        => \$user,
-            'dbpass:s'        => \$pass,
-            'dbname:s'      => \$dbname,
-            'dbport:n'        => \$port,
-            'in_config_name:s' => \$in_config_name,
+GetOptions( 'dbhost:s'          => \$host,
+            'dbuser:s'          => \$user,
+            'dbpass:s'          => \$pass,
+            'dbname:s'          => \$dbname,
+            'dbport:n'          => \$port,
+            'in_config_name:s'  => \$in_config_name,
             'out_config_name:s' => \$out_config_name,
-            'outhost:s'     => \$outhost,
-            'outuser:s'     => \$outuser,
-            'outpass:s'     => \$outpass,
-            'outdbname:s'   => \$outdbname,
-            'outport:n'     => \$outport,
-            'split!' => \$split,
-            'file:s' => \$infile,
-           );
-
+            'outhost:s'         => \$outhost,
+            'outuser:s'         => \$outuser,
+            'outpass:s'         => \$outpass,
+            'outdbname:s'       => \$outdbname,
+            'outport:n'         => \$outport,
+            'dnahost:s'         => \$dnahost,
+            'dnauser:s'         => \$dnauser,
+            'dnadbname:s'       => \$dnadbname,
+            'dnaport:n'         => \$dnaport,
+            'split!'            => \$split,
+            'file:s'            => \$infile );
 
 #print "Connecting to ".$dbname." at ".$host." ".$user."\n";
 my $db;
-if($in_config_name){
+if ($in_config_name) {
   $db = get_db_adaptor_by_string($in_config_name);
-}else{
-  $db = new Bio::EnsEMBL::DBSQL::DBAdaptor(
-                                           -host   => $host,
-                                           -user   => $user,
-                                           -pass   => $pass,
-                                           -port   => $port,
-                                           -dbname => $dbname
-                                          );
+} else {
+  $db =
+    new Bio::EnsEMBL::DBSQL::DBAdaptor( -host   => $host,
+                                        -user   => $user,
+                                        -pass   => $pass,
+                                        -port   => $port,
+                                        -dbname => $dbname );
 }
 
+my $dnadb;
+if ( !$dnadbname ) {
+  my $dna_query = q(SELECT count(1) FROM dna);
+  my $dna_sth = $db->dbc->prepare($dna_query);
+  $dna_sth->execute();
+  my $dna_count = $dna_sth->fetchrow;
 
+  if ( !$dna_count ) {
+    croak( "\nYour database does not contain DNA. "
+         . "Please provide a database with genomic sequences.\n");
+  }
+} else {
+  $dnadb =
+    new Bio::EnsEMBL::DBSQL::DBAdaptor( -host   => $dnahost,
+                                        -user   => $dnauser,
+                                        -port   => $dnaport,
+                                        -dbname => $dnadbname );
 
+  $db->dnadb($dnadb);
+}
 
 
 my $ga = $db->get_GeneAdaptor;
@@ -91,41 +126,43 @@ open(INFILE, "<$infile") or die ("Can't read $infile $! \n");
 while(<INFILE>){
   #print "$_";
   chomp;
-  my $gene_id= $_;
-  my $gene = $ga->fetch_by_dbID($gene_id);
+  my $gene_id = $_;
+  my $gene    = $ga->fetch_by_dbID($gene_id);
   empty_Gene($gene);
-  push(@copy_genes, $gene);
+  push( @copy_genes, $gene );
 }
 close(INFILE);
 
 my $outdb;
-if($out_config_name){
+if ($out_config_name) {
   $outdb = get_db_adaptor_by_string($out_config_name);
-}else{
-  $outdb = new Bio::EnsEMBL::DBSQL::DBAdaptor(
-                                              -host   => $outhost,
-                                              -user   => $outuser,
-                                              -pass   => $outpass,
-                                              -port   => $outport,
-                                              -dbname => $outdbname
-                                             );
+} else {
+  $outdb =
+    new Bio::EnsEMBL::DBSQL::DBAdaptor( -host   => $outhost,
+                                        -user   => $outuser,
+                                        -pass   => $outpass,
+                                        -port   => $outport,
+                                        -dbname => $outdbname );
 }
 
-if($split){
-  foreach my $gene(@copy_genes){
-    push(@genes, @{convert_to_genes($gene->get_all_Transcripts, $gene->analysis, $gene->biotype)});
+if ($split) {
+  foreach my $gene (@copy_genes) {
+    push( @genes,
+          @{convert_to_genes( $gene->get_all_Transcripts, $gene->analysis,
+                              $gene->biotype) }
+        );
   }
-}else{
+} else {
   @genes = @copy_genes;
 }
 print STDERR "Fetched ".scalar(@genes)." genes\n";
 
 my $outga = $outdb->get_GeneAdaptor;
 
-foreach my $gene(@genes){
-  eval{
+foreach my $gene (@genes) {
+  eval {
     fully_load_Gene($gene);
     empty_Gene($gene);
     $outga->store($gene);
-  }
+  };
 }
