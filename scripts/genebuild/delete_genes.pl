cd380cbc1dd0a2371c7ca08edaa811669e75a2c3
Amonida Zadissa <amonida@ebi.ac.uk>
2010-04-16 10:18:06 +0000

    Added option for input file and tidy-up of Doc.

--- scripts/genebuild/delete_genes.pl
+++ scripts/genebuild/delete_genes.pl
@@ -4,27 +4,31 @@
 
   delete_genes.pl
 
-=head1 SYNOPSIS
- 
-  delete_genes.pl
-  deletes genes from given database whose ids are passed in through STDIN
-
 =head1 DESCRIPTION
 
+  Given a list of gene_ids, deletes the genes from the specified
+  database. If config_dbname is provided reads the database details
+  from the Bio::EnsEMBL::Analysis::Config::GeneBuild::Databases
+  configuration.
 
 =head1 OPTIONS
 
-    -dbhost      host name for database (gets put as host= in locator)
-    -dbport      For RDBs, what port to connect to (port= in locator)
-    -dbname    For RDBs, what name to connect to (dbname= in locator)
-    -dbuser    For RDBs, what username to connect as (dbuser= in locator)
-    -dbpass    For RDBs, what password to use (dbpass= in locator)
-    -help      summary of options
+  -dbhost   host name for database (gets put as host= in locator)
+  -dbport   For RDBs, what port to connect to (port= in locator)
+  -dbname   For RDBs, what name to connect to (dbname= in locator)
+  -dbuser   For RDBs, what username to connect as (dbuser= in locator)
+  -dbpass   For RDBs, what password to use (dbpass= in locator)
+  -idfile   File with internal gene ids to be deleted
+  -help     Summary of options
 
+=head1 EXAMPLES
 
-=head2 EXAMPLES
+  perl delete_genes.pl -dbhost ecs2b -dbuser ensadmin -dbpass **** \
+    -dbname rat_Jun03_mk2 -idfile genes_to_delete
 
-./delete_genes.pl -dbhost ecs2b -dbuser ensadmin -dbpass **** -dbname rat_Jun03_mk2 genes_to_delete
+  or
+
+  perl delete_genes.pl -config_dbname GENEBUILD_DB -idfile genes_to_delete
 
 =cut
 
@@ -39,43 +43,43 @@ my $port;
 my $dbname;
 my $user;
 my $pass;
+my $idfile;
 my $config_dbname;
 
-&GetOptions( 
-            'dbhost:s'      => \$host,
-            'dbport:n'      => \$port,
-            'dbname:s'    => \$dbname,
-            'dbuser:s'    => \$user,
-            'dbpass:s'      => \$pass,
-            'config_dbname:s' => \$config_dbname,
-           );
+
+GetOptions( 'dbhost:s'        => \$host,
+            'dbport:n'        => \$port,
+            'dbname:s'        => \$dbname,
+            'dbuser:s'        => \$user,
+            'dbpass:s'        => \$pass,
+            'idfile:s'        => \$idfile,
+            'config_dbname:s' => \$config_dbname, );
 
 
 my $db;
 
-if($config_dbname){
+if ($config_dbname) {
   $db = get_db_adaptor_by_string($config_dbname);
-}elsif($dbname && $host){
-  $db = new Bio::EnsEMBL::DBSQL::DBAdaptor(
-                                            -host   => $host,
-                                            -user   => $user,
-                                            -port   => $port,
-                                            -dbname => $dbname,
-                                            -pass => $pass,
-                                           );
-}else{
-  throw("Need to pass either -dbhost $host and -dbname $dbname or ".
-        "-config_dbname $config_dbname for the script to work");
+} elsif ( $dbname && $host ) {
+  $db =
+    new Bio::EnsEMBL::DBSQL::DBAdaptor( -host   => $host,
+                                        -user   => $user,
+                                        -port   => $port,
+                                        -dbname => $dbname,
+                                        -pass   => $pass, );
+} else {
+  throw(   "Need to pass either -dbhost $host and -dbname $dbname or "
+         . "-config_dbname $config_dbname for the script to work" );
 }
 
 my $gene_adaptor = $db->get_GeneAdaptor;
 
-while(<>){
+open(INFILE, "<$idfile") or die ("Can't read $idfile $! \n");
+
+while (<INFILE>) {
   chomp;
-  my $gene_id= $_;
-  
-  #my $sth = $db->prepare("delete from gene where gene_id = $gene_id");
-  #$sth->execute;
+  my $gene_id = $_;
+
   eval{
     my $gene = $gene_adaptor->fetch_by_dbID($gene_id);
 
@@ -89,4 +93,4 @@ while(<>){
     print "Couldn't remove gene $gene_id ($@)\n";
   }
 }
-
+close(INFILE);
