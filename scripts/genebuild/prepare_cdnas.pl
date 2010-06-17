90cf9f85083b620e983cc3c841dfd7a17040221a
Amy Tang <at6@sanger.ac.uk>
2010-06-17 16:10:09 +0000

    Now including kill_list check as mandatory, so problematic cDNAs (e.g. chimeric or containing retained introns) will not be used in cdna2genome.
    Improved reporting of which cDNA is discarded during preparation and why.
    
    Also updated POD.
    
    Script has been tested using mouse cDNAs.

--- scripts/genebuild/prepare_cdnas.pl
+++ scripts/genebuild/prepare_cdnas.pl
@@ -17,27 +17,32 @@ use warnings;
   and also prints out an annotation file for use
   by exonerate's cdna2genome 
 
-  The script reads in each cDNA and clips it using 
+  The script reads in each cDNA, checks the cDNA against
+  the Ensembl kill_list DB if required, and clips it using 
   Bio::EnsEMBL::Analysis::Tools::PolyAClipping. It then 
   looks for the cDNA in Mole and finds its coordinates. 
+
   If the coordinates are not found or can't be parsed, 
   a message is sent to STDERR and the cDNA is not 
-  written to the outfile. If the cDNA is found, we check 
-  that it has not been clipped into the CDS and then 
-  write the cDNA's fasta to outfile (/path/to/cdna.clip)
-  and its annotation to annotation_file 
-  (/path/to/cdna.annot). This annotation file contains 
-  additional information for the use to know what has 
-  happened during clipping. In order for this annotation 
-  file to be used by exonerate's cdna2genome model, the 
-  annotation file with have to be edited to contain only 
-  the first 4 columns of each line
+  written to the outfile. (Parsing problem often caused by
+  the cDNA not having exact start/end position data in the 
+  EMBL-CDS DB.) If the cDNA is found, we check that it has
+  not been clipped into the CDS and then write the cDNA's 
+  fasta to outfile (/path/to/cdna.clip) and its annotation 
+  to annotation_file (/path/to/cdna.annot). 
+
+  The annotation file contains additional information for 
+  the user to know what has happened during clipping. In order
+  for this annotation file to be used by exonerate's 
+  cdna2genome model, the annotation file will have to be 
+  edited to contain only the first 4 columns of each line
   eg. less /path/to/cdna.annot | awk '{print $1"\t"$2"\t"$3"\t"$4}' 
 
 =head1 OPTIONS
 
   -dbnames
-   database names for Mole databases eg. embl_91,refseq_23
+   database names for Mole databases separated by commas without
+   any whitespaces, eg. embl_91,refseq_23
   
   -infile
    path to the original downloaded cDNA fasta file
@@ -45,13 +50,24 @@ use warnings;
   -outfile
    path to the clipped cDNA fasta file to be written
 
+  -min_length
+   minimum length of cDNA after its polyA tail has been clipped.
+   set to be 0 by default.
+
   -annotation
    path to the annotation file
+
+=head1 EXAMPLE
+
+  perl prepare_cdnas.pl -infile all_DL_cDNAs.fa -outfile clipped_annotated_cDNAs.fa\
+  -annotation cDNAs.annotate -min_length 60 -dbnames embl_91,refseq_23
+
 
 =cut
 
 use Bio::EnsEMBL::Analysis::Tools::PolyAClipping;
 use Bio::SeqIO;
+use Bio::EnsEMBL::KillList::KillList;
 use Getopt::Long;
 use Bio::EnsEMBL::Utils::Exception qw(throw warning);
 use Bio::EnsEMBL::ExternalData::Mole::DBXref;
@@ -113,6 +129,12 @@ foreach my $dbname (@dbnames) {
   push @dbs, $db;
 }
 
+# get kill_list object and a killed_objects hash
+
+my $kill_list_object = Bio::EnsEMBL::KillList::KillList
+     ->new(-TYPE => "cDNA");
+my %kill_list = %{ $kill_list_object->get_kill_list() };
+
 # open the downloaded cdna file
 my $seqin  = new Bio::SeqIO( -file   => "<$infile",
                              -format => "Fasta",
@@ -121,19 +143,41 @@ my $seqin  = new Bio::SeqIO( -file   => "<$infile",
 my $seqout = new Bio::SeqIO( -file   => ">$outfile",
                              -format => "Fasta"
                            );
- 
+
+# Various counter to keep track of cDNAs lost/discarded in the process:
+
+my $total_cDNAs = 0;
+my $XM_count = 0;
+my $killed_count = 0;
+my $parse_problem = 0;
+my $not_in_mole = 0;
+my $too_short_after_clip = 0;
+my $AT_only_count = 0;
+my $cdna_written = 0;
+
+# Now work on cDNAs one by one
+
 SEQFETCH:
 while ( my $cdna = $seqin->next_seq ) {
-  if ($cdna->display_id =~ /^gi\|\d+\|ref\|(XM_.+)\|/ ) { # if cDNA is XM_ RefSeq (predicted), we don't even bother clipping.
-    print "XM RefSeq mRNA, skipped: $1.\n";
-    next;
+  $total_cDNAs ++;
+  if ($cdna->display_id =~ /^gi\|\d+\|ref\|(XM_.+)\|/ ) { 
+    # if cDNA is XM_ RefSeq (predicted), we don't even bother clipping.
+    print STDERR "XM RefSeq mRNA, skipped: $1.\n";
+    $XM_count ++;
+    next SEQFETCH;
   }
+  
   my ($clipped, $clip_end, $num_bases_removed) = clip_if_necessary($cdna, $buffer, $window_size);
-  if (defined $clipped) {    # $clipped would have been returned undefined if the entire sequence seems to be polyA/T tail/head
+  if (defined $clipped) {  # $clipped would have been returned undef if the entire cDNA seq seems to be polyA/T tail/head
     my $id = $clipped->id;
-    #  next unless in_mole;
+    # next SEQFETCH unless in_mole;
     # check whether in Mole
-
+    if ($kill_list{$id}) {
+      print STDERR "$id is in kill list, discarded.\n";
+      $killed_count ++;
+      next SEQFETCH;
+    }
+   
     my ($found, $substr) = in_mole($id, $clip_end, $num_bases_removed, $clipped->length);
     if ($substr) {
       # the clipping cut off too much. We must get it back.
@@ -145,15 +189,35 @@ while ( my $cdna = $seqin->next_seq ) {
     }
     if ($found) {
       if (length($clipped->seq) >= $min_length) {
+        $cdna_written ++;
         $seqout->write_seq($clipped);
       } else {
       print STDERR "Clipped sequence for $id is shorter than $min_length bp and is excluded from output.\n";
+      $too_short_after_clip ++;
       }
-    } else {
-      warning ("Not in Mole $id.");
     }
+  } else {
+    $AT_only_count ++;
   }
 }
+
+my $expected_output_nr = $total_cDNAs-$XM_count-$killed_count-$too_short_after_clip-$AT_only_count-$parse_problem-$not_in_mole;
+if ($cdna_written != $expected_output_nr) {
+  print "BEWARE, NUMBERS DON'T ADD UP!  EXPECTED TO HAVE $expected_output_nr ENTRIES IN OUTPUT FILE, ".
+        "BUT YOU ONLY HAVE $cdna_written.\n";
+}
+ 
+print "\n";
+print "Number of cDNAs processed: $total_cDNAs.\n";
+print "XM RefSeq mRNA discarded: $XM_count.\n";
+print "cDNAs present in kill_list DB: $killed_count.\n";
+print "cDNAs too short after polyA clipped: $too_short_after_clip.\n";
+print "Entire cDNA sequence seems to be of polyA or polyT: $AT_only_count.\n";
+print "Can't parse the head/tail info in EMBL file: $parse_problem.\n";
+print "Can't find the accession in mole DB: $not_in_mole.\n";
+print "cDNAs written to output: $cdna_written.\n";
+
+
 =head2 in_mole 
 
   Arg [1]   : String - accession of cDNA, with version 
@@ -192,7 +256,7 @@ sub in_mole {
       if ( defined $clip_end ) {
         if ( $clip_end eq "tail" ) {
           if ( $length < $end ) {
-            warning("Clipped off too many bases from the tail: $id");
+            print STDERR "Clipped off too many bases from the tail: $id\n";
             $do_substr = $end;
             $string .= "\t| $clip_end | $end | substr_tail";
           } else {
@@ -200,7 +264,7 @@ sub in_mole {
           }
         } elsif ( $clip_end eq "head" ) {
           if ( $start - $num_bases_removed < 0 ) {
-            warning("Clipped off too many bases from the head: $id");
+            print STDERR "Clipped off too many bases from the head: $id\n";
             $do_substr = $start - 1;
             $string    = "$id\t$strand\t1\t$cdslength";
             $string   .= "\t| $clip_end | oldstart $start oldend $end | substr_head";
@@ -217,9 +281,11 @@ sub in_mole {
       $write_cdna = 1;
     } else {
       print STDERR "Parse_problem $id in db ".$in_db->dbc->dbname." \n";
+      $parse_problem ++;
     } 
   } else {
-    print STDERR "Not_in_mole $id\n";
+    print STDERR "Not_in_Mole $id.\n";
+    $not_in_mole ++;
   }
   return ($write_cdna, $do_substr); 
 }  
