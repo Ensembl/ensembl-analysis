356599446bd73bda3a8e7d428cba59cb91f1514a
Michael Gray <mg13@sanger.ac.uk>
2011-09-29 10:34:22 +0000

    Replacement for BlastableVersion.pm

---
+++ modules/Bio/EnsEMBL/Analysis/Tools/BlastDBTracking/Entry.pm
@@ -0,0 +1,13 @@
+package Bio::EnsEMBL::Analysis::Tools::BlastDBTracking::Entry;
+
+use namespace::autoclean;
+use Moose;
+
+has filename       => ( is => 'ro', isa => 'Str', required => 1 );
+has version        => ( is => 'ro', isa => 'Str', required => 1 );
+has sanger_version => ( is => 'ro', isa => 'Int',               );
+has installation   => ( is => 'ro', isa => 'Int',               );
+has count          => ( is => 'ro', isa => 'Int',               );
+has checksum       => ( is => 'ro', isa => 'Str',               );
+
+1;
