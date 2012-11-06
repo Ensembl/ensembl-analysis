5edde6b33e2dfa7d78e836484519b7115f754917
Gary Williams <gw3@ebi.ac.uk>
2012-11-06 12:25:45 +0000

    updated to SignalP v4 - you should update your SignalP binary and analysis to this version

--- modules/Bio/EnsEMBL/Analysis/Runnable/ProteinAnnotation/Signalp.pm
+++ modules/Bio/EnsEMBL/Analysis/Runnable/ProteinAnnotation/Signalp.pm
@@ -31,39 +31,40 @@ use Bio::SeqIO;
 @ISA = qw(Bio::EnsEMBL::Analysis::Runnable::ProteinAnnotation);
 
 
-# we over-ride write_seq_file because Signalp works best
-# if only the first 50 a.a. of the protein is supplied 
-
-sub write_seq_file{
-  my ($self, $seq, $filename) = @_;
-  if(!$seq){
-    $seq = $self->query;
-  }
-  if(!$filename){
-    $filename = $self->queryfile;
-  }
-  my $seqout = Bio::SeqIO->new(
-                               -file => ">".$filename,
-                               -format => 'Fasta',
-                              );
-  eval{
-    my $init_seq = substr($seq->seq, 0, 50);
-    my $newseqobj = Bio::Seq->new(-id => $seq->display_id,
-                                  -seq => $init_seq);
-    $seqout->write_seq($newseqobj);
-  };
-  if($@){
-    throw("FAILED to write $seq to $filename Runnable:write_seq_file");
-  }
-  return $filename;
-}
+# We now leave the length of sequence to analyse at the default 70
+## we over-ride write_seq_file because Signalp works best
+## if only the first 50 a.a. of the protein is supplied 
+#
+#sub write_seq_file{
+#  my ($self, $seq, $filename) = @_;
+#  if(!$seq){
+#    $seq = $self->query;
+#  }
+#  if(!$filename){
+#    $filename = $self->queryfile;
+#  }
+#  my $seqout = Bio::SeqIO->new(
+#                               -file => ">".$filename,
+#                               -format => 'Fasta',
+#                              );
+#  eval{
+#    my $init_seq = substr($seq->seq, 0, 50);
+#    my $newseqobj = Bio::Seq->new(-id => $seq->display_id,
+#                                  -seq => $init_seq);
+#    $seqout->write_seq($newseqobj);
+#  };
+#  if($@){
+#    throw("FAILED to write $seq to $filename Runnable:write_seq_file");
+#  }
+#  return $filename;
+#}
 
 
 sub run_analysis {
   my ($self) = @_;
-  
+ 
   throw("Error running ".$self->program." on ".$self->queryfile) 
-      unless ((system($self->program . " -t euk -trunc 200 "
+      unless ((system($self->program . " -t euk -f short "
                       .$self->queryfile 
                       . " > ".$self->resultsfile)) == 0); 
 }
@@ -80,6 +81,8 @@ sub parse_results {
     # it's a filename
     if (-z $resfile) {  
       # no hits
+     print STDERR "BLERG\n"; 
+
       return;
     }else {
       open ($fh, "<$resfile") or throw ("Error opening $resfile");
@@ -88,43 +91,25 @@ sub parse_results {
     # it'a a filehandle
     $fh = $resfile;
   }
-  
+ 
   # parse
-  my (@fps, $id, $fact1, $fact2, $end);
+  my (@fps, $id, $end, $yesno);
   while (<$fh>) {
+    print STDERR "$_" if $ENV{DEBUG};
     chomp;
-    if (/^\>(\S+)/) {
-      $id = $1;
+    next if (/^#/);
+    ($id, $end, $yesno) = (/^(\S+)\s+[\d\.]+\s+\d+\s+[\d\.]+\s+(\d+)\s+[\d\.]+\s+\d+\s+[\d\.]+\s+[\d+\.]+\s+(\S)/);
+    if (!defined $id) {
+      # throw an exception
+      $self->throw ("parsing problem in ".$self->program);
     }
-    elsif (/max\.\s+Y\s+(\S+)\s+\S+\s+\S+\s+(\S+)/) {
-      $fact1 = $2;
+    if ($yesno eq 'Y') {
+      my $fp = $self->create_protein_feature(1, $end-1, 0, $id, 0, 0,
+					     'Sigp', $self->analysis,
+					     0, 0);
+      push @fps, $fp;
     }
-    elsif (/mean\s+S\s+(\S+)\s+\S+\s+\S+\s+(\S+)/) {
-      $fact2 = $2;
-      
-      if ($fact1 eq "YES" && $fact2 eq "YES") {
-	# signalp 2 output, expecting to parse the next line
-	my $line = <$fh>;
-        if ($line =~ /Most likely cleavage site between pos\.\s+(\d+)/) {
-	    $end = $1;
-        }
-        else {
-          # otherwise signalp3 output, expecting to parse the line after
-	  my $line = <$fh>;
-	  if ($line =~ /Most likely cleavage site between pos\.\s+(\d+)/) {
-	      $end = $1;
-	  }
-	  else {
-	      # otherwise throw an exception
-	      $self->throw ("parsing problem in ".$self->program);
-	  }
-        }
-        my $fp = $self->create_protein_feature(1, $end, 0, $id, 0, 0,
-                                               'Sigp', $self->analysis,
-                                               0, 0);
-        push @fps, $fp;
-      }
-    }
+    
   }
   close($fh);
 
