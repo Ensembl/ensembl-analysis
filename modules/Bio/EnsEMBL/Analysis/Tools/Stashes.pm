577aa4a0fde79e4db429d3a214d5e00bdbce7d37
Andreas Kusalananda Kähäri <ak4@sanger.ac.uk>
2013-01-08 14:20:50 +0000

    Sort out fallout after adding strict and warning pragmas, and do formatting.

--- modules/Bio/EnsEMBL/Analysis/Tools/Stashes.pm
+++ modules/Bio/EnsEMBL/Analysis/Tools/Stashes.pm
@@ -1,40 +1,41 @@
 # $Source: /tmp/ENSCOPY-ENSEMBL-ANALYSIS/modules/Bio/EnsEMBL/Analysis/Tools/Stashes.pm,v $
-# $Revision: 1.4 $
-package Bio::EnsEMBL::Analysis::Tools::Stashes; 
+# $Revision: 1.5 $
+package Bio::EnsEMBL::Analysis::Tools::Stashes;
 
-use warnings ;
-use strict ;
+use warnings;
+use strict;
 use Bio::EnsEMBL::Utils::Exception qw(verbose throw warning);
-use Exporter ; 
-use vars qw (@ISA @EXPORT); 
+use Exporter;
+use vars qw (@ISA @EXPORT %stash $alias @alias %alias);
 
-@ISA = qw(Exporter);
+@ISA    = qw(Exporter);
 @EXPORT = qw( package_stash );
 
 sub package_stash {
-    my ($packageName) = @_;  
+  my ($packageName) = @_;
 
-    my %result ;  
+  my %result;
 
-    local (*alias);  
-    *stash = *{"${packageName}::"};   
+  local (*alias);
+  *stash = *{"${packageName}::"};
 
-    while (($varName, $globValue) = each %stash) {   
-        # only return the config hash 
-        next if $varName =~m/BEGIN/; 
-        next if $varName =~m/import/; 
+  while ( my ( $varName, $globValue ) = each %stash ) {
+    # only return the config hash
+    next if $varName =~ m/BEGIN/;
+    next if $varName =~ m/import/;
 
-        *alias = $globValue;  
-        $result{$varName}=$alias  if (defined($alias)) ; 
-        $result{$varName}=\@alias if (*alias{ARRAY}) ; 
-        $result{$varName}=\%alias if (*alias{HASH}) ;  
-     }     
+    *alias = $globValue;
+    $result{$varName} = $alias  if ( defined($alias) );
+    $result{$varName} = \@alias if ( *alias{ARRAY} );
+    $result{$varName} = \%alias if ( *alias{HASH} );
+  }
 
-     if (scalar(keys %result >1) ) {    
-       throw("Have more than one item exported from $packageName - you'll run into trouble\n")
-     }  
-     my $hash_name = (keys %result)[0];   
-     return [$result{$hash_name},$hash_name] ; 
-}
+  if ( scalar( keys %result > 1 ) ) {
+    throw( "Have more than one item exported from " .
+           "$packageName - you'll run into trouble\n" );
+  }
+  my $hash_name = ( keys %result )[0];
+  return [ $result{$hash_name}, $hash_name ];
+} ## end sub package_stash
 
 1;
