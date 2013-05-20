b672cfabb4d512082a65d118b729abbd6a57a802
Daniel Barrell <db8@sanger.ac.uk>
2013-05-20 11:06:22 +0000

    picard lsf stderr and stdout files should have similar #format

--- scripts/RNASeq/setup_rnaseq_pipeline.pl
+++ scripts/RNASeq/setup_rnaseq_pipeline.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/env perl
 # 
 # $Source: /tmp/ENSCOPY-ENSEMBL-ANALYSIS/scripts/RNASeq/setup_rnaseq_pipeline.pl,v $
-# $Revision: 1.45 $
+# $Revision: 1.46 $
 #
 
 use warnings ;
@@ -1109,7 +1109,7 @@ sub assign_categories {
 sub generate_picard_cmd {
   my ( $files_ref, $out_dir, $merge_dir ) = @_; 
   my $cmd = "bsub -qnormal -M2000000 -R'select[mem>2000] rusage[mem=2000]'"
-          . " -o " . $out_dir . "/picard_merge.out -e " . $out_dir . "/merge.err \\\n"
+          . " -o " . $out_dir . "/picard_merge.out -e " . $out_dir . "/picard_merge.err \\\n"
           . " /vol/software/linux-x86_64/jdk1.6.0_01/bin/java -Xmx2g"
           . " -jar /software/solexa/bin/aligners/picard/picard-tools-1.47/MergeSamFiles.jar \\\n";
   foreach my $input ( @{ $files_ref } ) {
