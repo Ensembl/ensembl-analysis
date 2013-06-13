bb8d62445b24998871ae09212162478d3802dc20
Fergal <fergal.martin@sanger.ac.uk>
2013-06-13 08:38:49 +0000

    Changed the formatting on the write_ouput fix so that Steve won't complain as much

--- modules/Bio/EnsEMBL/Analysis/RunnableDB/LayerAnnotation.pm
+++ modules/Bio/EnsEMBL/Analysis/RunnableDB/LayerAnnotation.pm
@@ -33,7 +33,7 @@ Bio::EnsEMBL::Analysis::RunnableDB::LayerAnnotation -
 =cut
 
 # $Source: /tmp/ENSCOPY-ENSEMBL-ANALYSIS/modules/Bio/EnsEMBL/Analysis/RunnableDB/LayerAnnotation.pm,v $
-# $Revision: 1.12 $
+# $Revision: 1.13 $
 package Bio::EnsEMBL::Analysis::RunnableDB::LayerAnnotation;
 
 use warnings ;
@@ -147,30 +147,24 @@ sub write_output {
   my $total = 0;
   my $fails = 0;
   my $out = $self->output;
-  while(my $g = shift @$out) 
-  {
+  while (my $g = shift @$out) {
     fully_load_Gene($g);
-     
+
     # Putting this in to stop the wrong dbIDs being assigned
     empty_Gene($g);
-    eval 
-    {
+    eval {
       $g_adap->store($g);
     };
     
-    if ($@)
-    {
+    if ($@) {
       $self->warning("Unable to store gene!!\n");
       print STDERR "$@\n";
       $fails++;  
     }
-
     $total++;
-
   }
   
-  if ($fails > 0) 
-  {
+  if ($fails > 0) {
     throw("Not all genes could be written successfully " .
           "($fails fails out of $total)");
   }
