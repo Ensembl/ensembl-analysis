674f15ee79da0bda8db11bf1815b7bb79f16c45d
James Gilbert <jgrg@sanger.ac.uk>
2013-10-29 15:00:34 +0000

    error pattern should not be wrapped

--- modules/Bio/EnsEMBL/Analysis/Runnable/Finished/Blast.pm
+++ modules/Bio/EnsEMBL/Analysis/Runnable/Finished/Blast.pm
@@ -1,5 +1,5 @@
 # $Source: /tmp/ENSCOPY-ENSEMBL-ANALYSIS/modules/Bio/EnsEMBL/Analysis/Runnable/Finished/Blast.pm,v $
-# $Revision: 1.22 $
+# $Revision: 1.23 $
 package Bio::EnsEMBL::Analysis::Runnable::Finished::Blast;
 
 =head1 NAME - Bio::EnsEMBL::Analysis::Runnable::Finished::Blast
@@ -26,8 +26,8 @@ BLAST_CONFIG =>
                                -database_type => 'pep',
                                -threshold_type => 'PVALUE',
                                -threshold => 0.01,
-			       -coverage => 10,
-			       -discard_overlaps => 1,
+                   -coverage => 10,
+                   -discard_overlaps => 1,
 
                               },
             BLAST_FILTER =>'Bio::EnsEMBL::Analysis::Tools::FeatureFilter',
@@ -57,7 +57,7 @@ BLAST_CONFIG =>
             },
 
            BLAST_AB_INITIO_LOGICNAME => 'Genscan'
-	   }
+       }
 
 
 =head1 AUTHOR
@@ -76,36 +76,36 @@ use Bio::EnsEMBL::Analysis::Tools::BlastDBTracking;
 use Bio::EnsEMBL::Utils::Exception qw(throw warning info);
 use base ("Bio::EnsEMBL::Analysis::Runnable::Blast");
 
-$ENV{BLASTMAT} 		= $MAT_DIR;
-$ENV{WUBLASTMAT}	= $MAT_DIR;
-$ENV{BLASTFILTER} 	= $BIN_DIR;
+$ENV{BLASTMAT}      = $MAT_DIR;
+$ENV{WUBLASTMAT}    = $MAT_DIR;
+$ENV{BLASTFILTER}   = $BIN_DIR;
 $ENV{WUBLASTFILTER} = $BIN_DIR;
-$ENV{BLASTDB} 		= $BLAST_DIR;
+$ENV{BLASTDB}       = $BLAST_DIR;
 
 BEGIN {
-	print STDERR "\nUSING " . __PACKAGE__ . "\n\n";
+    print STDERR "\nUSING " . __PACKAGE__ . "\n\n";
 }
 
 sub get_analysis {
 
-	my ($self) = @_;
+    my ($self) = @_;
 
-	my ($ana);
-	unless ( $ana = $self->{'_analysis'} ) {
-		my ($source) = $self->program =~ m{([^/]+)$}
-		  or throw(
-			"Can't parse last element from path: '" . $self->program . "'" );
-		$ana = $self->{'_analysis'} = Bio::EnsEMBL::Analysis->new(
-			-db              => $self->database,
-			-db_version      => 1,                 # ARUUGA!!!
-			-program         => $source,
-			-program_version => 1,
-			-gff_source      => $source,
-			-gff_feature     => 'similarity',
-			-logic_name      => 'blast',
-		);
-	}
-	return $ana;
+    my ($ana);
+    unless ( $ana = $self->{'_analysis'} ) {
+        my ($source) = $self->program =~ m{([^/]+)$}
+          or throw(
+            "Can't parse last element from path: '" . $self->program . "'" );
+        $ana = $self->{'_analysis'} = Bio::EnsEMBL::Analysis->new(
+            -db              => $self->database,
+            -db_version      => 1,                 # ARUUGA!!!
+            -program         => $source,
+            -program_version => 1,
+            -gff_source      => $source,
+            -gff_feature     => 'similarity',
+            -logic_name      => 'blast',
+        );
+    }
+    return $ana;
 }
 
 sub run_analysis {
@@ -149,8 +149,8 @@ sub run_analysis {
       if(/FATAL:(.+)/){
         my $match = $1;
         next DB if $match =~ /There is nothing in the requested database to search/;
-		# clean up before dying
-		$self->delete_files;
+        # clean up before dying
+        $self->delete_files;
         if($match =~ /no valid contexts/){
           die qq{"VOID"\n}; # hack instead
         }elsif($match =~ /Bus Error signal received/){
@@ -162,8 +162,7 @@ sub run_analysis {
           #requested."
           die qq{"OUT_OF_MEMORY"\n};
           # resenD to big mem machine by rulemanager
-        }elsif($match =~ /the query sequence is shorter
-               than the word length/){
+        }elsif($match =~ /the query sequence is shorter than the word length/){
           #no valid context
           die qq{"VOID"\n}; # hack instead
         }else{
@@ -200,27 +199,27 @@ sub run_analysis {
 
 sub parse_results {
 
-	my ($self)           = @_;
-	my $results          = $self->results_files;
-	my $bplites          = $self->parser->parse_files($results);
-	my $threshold_type   = $self->parser->threshold_type;
-	my $threshold        = $self->parser->threshold;
-	my $discard_overlaps = $self->parser->discard_overlaps;
-	my $coverage         = $self->parser->coverage;
-	my $hits             =
-	  $self->parser->get_best_hits( $bplites, $threshold_type, $threshold );
-	my $query_length = $self->query->length
-	  or throw("Couldn't get query length");
-	my $output =
-	  $self->parser->_apply_coverage_filter( $query_length, $hits,
-		$threshold_type, $threshold, $coverage, $discard_overlaps );
-	$self->output($output);
-	return $output;
+    my ($self)           = @_;
+    my $results          = $self->results_files;
+    my $bplites          = $self->parser->parse_files($results);
+    my $threshold_type   = $self->parser->threshold_type;
+    my $threshold        = $self->parser->threshold;
+    my $discard_overlaps = $self->parser->discard_overlaps;
+    my $coverage         = $self->parser->coverage;
+    my $hits             =
+      $self->parser->get_best_hits( $bplites, $threshold_type, $threshold );
+    my $query_length = $self->query->length
+      or throw("Couldn't get query length");
+    my $output =
+      $self->parser->_apply_coverage_filter( $query_length, $hits,
+        $threshold_type, $threshold, $coverage, $discard_overlaps );
+    $self->output($output);
+    return $output;
 }
 
 sub clean_databases {
-	my ( $self) = @_;
-	$self->{databases} = [];
+    my ( $self) = @_;
+    $self->{databases} = [];
 }
 
 sub clean_results_files {
@@ -229,56 +228,56 @@ sub clean_results_files {
 }
 
 sub databases {
-	my ( $self, @vals ) = @_;
+    my ( $self, @vals ) = @_;
 
-	if ( not exists $self->{databases} ) {
-		$self->{databases} = [];
-	}
+    if ( not exists $self->{databases} ) {
+        $self->{databases} = [];
+    }
 
-	foreach my $val (@vals) {
-		my $db_names = $val;
-		my @databases;
-		$db_names =~ s/\s//g;
+    foreach my $val (@vals) {
+        my $db_names = $val;
+        my @databases;
+        $db_names =~ s/\s//g;
 
-		foreach my $dbname ( split( ",", $db_names ) )
-		{    # allows the use of a comma separated list in $self->database
-			    # prepend the environment variable $BLASTDB if
-			    # database name is not an absoloute path
-			unless ( $dbname =~ m!^/! ) {
-				$dbname = $ENV{BLASTDB} . "/" . $dbname;
-			}
+        foreach my $dbname ( split( ",", $db_names ) )
+        {    # allows the use of a comma separated list in $self->database
+                # prepend the environment variable $BLASTDB if
+                # database name is not an absoloute path
+            unless ( $dbname =~ m!^/! ) {
+                $dbname = $ENV{BLASTDB} . "/" . $dbname;
+            }
 
-			# If the expanded database name exists put this in
-			# the database array.
-			#
-			# If it doesn't exist then see if $database-1,$database-2 exist
-			# and put them in the database array
-			if ( -f $dbname ) {
-				push( @databases, $dbname );
-			}
-			else {
-				my $count = 1;
-				my $db_filename;
-				while ( -f ( $db_filename = "${dbname}-${count}" ) ) {
-					push( @databases, $db_filename );
-					$count++;
-				}
-				$! = undef
-				  ; # to stop pollution as it will be "No such file or directory" after while loop above.
-			}
-		}
-		if ( scalar(@databases) == 0 ) {
-			throw( "No databases exist for " . $db_names );
-		} else {
-			foreach my $db_name (@databases){
-				$self->get_db_version($db_name) if $db_name =~ /emnew_/;
-			}
-			$self->get_db_version($databases[0]);
-			push @{$self->{databases}}, @databases;
-		}
-	}
+            # If the expanded database name exists put this in
+            # the database array.
+            #
+            # If it doesn't exist then see if $database-1,$database-2 exist
+            # and put them in the database array
+            if ( -f $dbname ) {
+                push( @databases, $dbname );
+            }
+            else {
+                my $count = 1;
+                my $db_filename;
+                while ( -f ( $db_filename = "${dbname}-${count}" ) ) {
+                    push( @databases, $db_filename );
+                    $count++;
+                }
+                $! = undef
+                  ; # to stop pollution as it will be "No such file or directory" after while loop above.
+            }
+        }
+        if ( scalar(@databases) == 0 ) {
+            throw( "No databases exist for " . $db_names );
+        } else {
+            foreach my $db_name (@databases){
+                $self->get_db_version($db_name) if $db_name =~ /emnew_/;
+            }
+            $self->get_db_version($databases[0]);
+            push @{$self->{databases}}, @databases;
+        }
+    }
 
-	return $self->{databases};
+    return $self->{databases};
 
 }
 
@@ -298,7 +297,7 @@ sub databases {
 =cut
 
 sub get_db_version {
-	my ( $self, $db ) = @_;
+    my ( $self, $db ) = @_;
         my $ver = Bio::EnsEMBL::Analysis::Tools::BlastDBTracking::get_db_version_mixin(
             $self, '_db_version_searched', $db,
             );
@@ -306,14 +305,14 @@ sub get_db_version {
         printf STDERR "B:E:A:Runnable::Finished::Blast::get_db_version '%s' => '%s'\n",
                       $db || '<read_only>', $ver;
 
-	return $ver;
+    return $ver;
 }
 
 sub DESTROY
 {
-	my ( $self ) = @_;
-	# just do the cleanup
-	$self->delete_files;
+    my ( $self ) = @_;
+    # just do the cleanup
+    $self->delete_files;
 }
 
 1;
