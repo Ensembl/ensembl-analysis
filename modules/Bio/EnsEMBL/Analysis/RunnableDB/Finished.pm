0d3b48e0043a738a20444755983eb526fc74bc50
Rishi Nag <rn6@sanger.ac.uk>
2013-12-05 17:00:49 +0000

    Apache 2.0 licence update

--- modules/Bio/EnsEMBL/Analysis/RunnableDB/Finished.pm
+++ modules/Bio/EnsEMBL/Analysis/RunnableDB/Finished.pm
@@ -1,4 +1,16 @@
-### Bio::EnsEMBL::Analysis::RunnableDB::Finished
+# Copyright [1999-2013] Genome Research Ltd. and the EMBL-European Bioinformatics Institute
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#      http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 package Bio::EnsEMBL::Analysis::RunnableDB::Finished;
 
@@ -235,8 +247,4 @@ sub get_adaptor {
 
 1;
 
-=head1 NAME - Bio::EnsEMBL::Analysis::RunnableDB::Finished
 
-=head1 AUTHOR
-
-Mustapha Larbaoui B<email> ml6@sanger.ac.uk
