9eb37a54650484df1c031a77fc6f6d569ed1eadc
Rishi Nag <rn6@sanger.ac.uk>
2014-04-29 16:29:24 +0100

    package line fixes

--- modules/Bio/EnsEMBL/Analysis/Runnable/ProteinAnnotation/Superfamily_wormbase.pm
+++ modules/Bio/EnsEMBL/Analysis/Runnable/ProteinAnnotation/Superfamily_wormbase.pm
@@ -10,7 +10,8 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License.package Bio::EnsEMBL::Analysis::Runnable::ProteinAnnotation::Superfamily_wormbase;
+# limitations under the License.
+package Bio::EnsEMBL::Analysis::Runnable::ProteinAnnotation::Superfamily_wormbase;
 
 use warnings ;
 use vars qw(@ISA);
