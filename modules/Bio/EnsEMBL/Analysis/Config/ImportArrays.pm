64f0c997a0f2bcda54389a73bec6de8ab64b0e58
Nathan Johnson <njohnson@ebi.ac.uk>
2014-10-31 14:44:45 +0000

    added config for RaGene-2_1-st-v1

--- modules/Bio/EnsEMBL/Analysis/Config/ImportArrays.pm
+++ modules/Bio/EnsEMBL/Analysis/Config/ImportArrays.pm
@@ -746,7 +746,6 @@ use vars qw( %Config );
                             #-description => '',
                             -class   => 'AFFY_ST',
                           },
-
 					  
        'RaGene-1_0-st-v1' => {
                               -name => 'RaGene-1_0-st-v1',
@@ -757,6 +756,16 @@ use vars qw( %Config );
                               #-description => '',
                               -class   => 'AFFY_ST',
                              },
+
+       'RaGene-2_1-st-v1' => {
+                              -name => 'RaGene-2_1-st-v1',
+                              -vendor => 'AFFY',
+                              #-setsize => undef,
+                              -format  => 'EXPRESSION',
+                              -type    => 'OLIGO',
+                              #-description => '',
+                              -class   => 'AFFY_ST',
+                             },                             
 
        #Human
        'HuGene-1_0-st-v1' => {-name => 'HuGene-1_0-st-v1',
