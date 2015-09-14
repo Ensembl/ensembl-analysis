c92a5b26516e67ef12edd7032c10fb6757507c3f
fergalmartin <fergal@ebi.ac.uk>
2015-09-14 16:41:25 +0100

    Change to HiveClusterSourceGenes.pm Added in some code to work with cluster slices, to be used in cases where the location of a cluster has been predetermined. There are some issues with this at the moment, as test cases show more genes are being made than expected. Porbably due to the existence of sub clusters, as the API fetches the genes on overlap of the cluster slice so sub cluster genes are probably getting represented twice. Committing now anyway, but may remove later and instead just work with gene ids directly

--- modules/Bio/EnsEMBL/Analysis/Hive/RunnableDB/HiveClusterSourceGenes.pm
+++ modules/Bio/EnsEMBL/Analysis/Hive/RunnableDB/HiveClusterSourceGenes.pm
@@ -22,6 +22,21 @@ use feature 'say';
 use Data::Dumper;
 use Bio::EnsEMBL::Analysis::Tools::Algorithms::ClusterUtils;
 use Bio::EnsEMBL::Analysis::Tools::GeneBuildUtils::GeneUtils qw(empty_Gene);
+use Bio::EnsEMBL::DBSQL::SliceAdaptor;
+
+
+use vars qw (@EXPORT);
+
+use Exporter 'import';
+
+@EXPORT = qw(
+fetch_source_genes
+get_all_genes
+get_all_biotypes
+process_clusters
+master_genes_hash
+);
+
 use parent ('Bio::EnsEMBL::Analysis::Hive::RunnableDB::HiveBaseRunnableDB');
 
 # This module is built with the view of in some ways being the opposite to our current
@@ -57,24 +72,58 @@ sub fetch_input {
   $self->hrdb_set_con($dba,'dna_db');
 
   my $out_dba = $self->hrdb_get_dba($self->param('output_db'));
+  $out_dba->dnadb($dba);
   $self->hrdb_set_con($out_dba,'output_db');
 
   my $input_id = $self->param('iid');
-  my $slice = $self->fetch_sequence($input_id,$dba);
-  $self->query($slice);
 
-  if($input_gene_dbs) {
-    my $input_genes = $self->fetch_source_genes($input_gene_dbs,$allowed_input_sets);
-    $master_genes_hash->{'input_genes'} = $input_genes;
+  unless($self->param('input_gene_dbs')) {
+    $self->throw("You must specify an arrayref of input dbs via the 'input_gene_dbs' parameter");
   }
 
+  my $slice;
+  my $cluster_strand;
+  if($self->param('iid_type') eq 'slice') {
+    $slice = $self->fetch_sequence($input_id,$dba);
+  } elsif($self->param('iid_type') eq 'cluster_slice') {
+    unless(split(':',$input_id) == 7) {
+      $self->throw("You have selected to use the 'cluster_slice' input id type, but it was not possible to parse ".
+                   "the input_id correctly. Cluster slice input ids are similar to slice ids, but with an additional ".
+                   "column at the end to define the strand of the genes the cluster represents, e.g.:\n".
+                   "\"chromosome:PapAnu2.0:18:808824:882888:1:-1\"\nOffending input id:\n".$input_id);
+    }
+
+    unless($input_id =~ /^(.+)\:(\-?1)$/) {
+      $self->throw("You have selected to use the 'cluster_slice'!!!!!!!!!!!!, but it was not possible to parse ".
+                   "the input_id correctly. Cluster slice input ids are similar to slice ids, but with an additional ".
+                      "column at the end to define the strand of the genes the cluster represents, e.g.:\n".
+                   "\"chromosome:PapAnu2.0:18:808824:882888:1:-1\"\nOffending input id:\n".$input_id);
+    }
+
+    my $slice_name = $1;
+    $cluster_strand = $2;
+    $slice = $self->fetch_sequence($slice_name,$dba);
+  } else {
+    $self->throw("You must specify an input_id type in the config using the 'iid_type' parameter");
+  }
+
+  # Set the slice at this point
+  $self->query($slice);
+
+
+  my $input_genes = $self->fetch_source_genes($input_gene_dbs,$allowed_input_sets);
+  if($self->param('iid_type') eq 'cluster_slice') {
+    $input_genes = $self->filter_by_strand($input_genes,$cluster_strand);
+  }
+
+  $master_genes_hash->{'input_genes'} = $input_genes;
   $self->master_genes_hash($master_genes_hash);
 
   # Now need to think how this would be represented in config
   # input_gene_dbs => ['rnaseq_blast_db','genblast_db','exonerate_db'],
-  # rnaseq_allowed_sets => {'baboon_liver_models' => {'protein_coding_80_100' => 1},
-  #                         'baboon_lung_models' => {'protein_coding_80_100' => 1,
-  #                         'protein_coding_50_80' => 1}}
+  # allowed_gene_sets => {'baboon_liver_models' => {'protein_coding_80_100' => 1},
+  #                       'baboon_lung_models' => {'protein_coding_80_100' => 1,
+  #                                                'protein_coding_50_80' => 1}}
   #
   # e.g unless($rnaseq_allowed_biotypes->{$transcript->logic_name}->{$transcript->biotype})
   #
@@ -89,6 +138,15 @@ sub run {
   my $self = shift;
 
   my $master_genes_array = $self->get_all_genes();
+
+  my $test_transcripts = $$master_genes_array[0]->get_all_Transcripts;
+  my $test_transcript = $$test_transcripts[0];
+      say "TTRANSCRIPT START: ".$test_transcript->start;
+      say "TTRANSCRIPT END: ".$test_transcript->end;
+      say "TTRANSCRIPT STRAND: ".$test_transcript->strand;
+      say "TTRANSCRIPT SLICE: ".$test_transcript->slice->name;
+      my $exons = $test_transcript->get_all_Exons;
+      say "TT EXONS: ".scalar(@{$exons});
 
   my $master_biotypes_hash = $self->get_all_biotypes($master_genes_array);
   my $master_biotypes_array = [keys(%$master_biotypes_hash)];
@@ -106,6 +164,7 @@ sub run {
   say "...finished processing gene clusters into single gene models";
   say "Made output genes: ".scalar(@{$output_genes});
 
+#  say "FM2 DUMPER OUTGENES: ".Dumper($output_genes);
   $self->output_genes($output_genes);
 
   return 1;
@@ -119,6 +178,27 @@ sub write_output {
 
   say "Writing genes to output db";
   foreach my $gene (@output){
+    $gene->slice($self->query);
+#    say "GENE START: ".$gene->start;
+#    say "GENE END: ".$gene->end;
+ #   say "GENE STRAND: ".$gene->strand;
+#    say "GENE SLICE: ".$gene->slice->name;
+  #  $gene->slice($self->query->seq_region_Slice);
+
+#    my $transcript = ${$gene->get_all_Transcripts}[0];
+#    say "TRANSCRIPT START: ".$transcript->seq_region_start;
+#    say "TRANSCRIPT END: ".$transcript->seq_region_end;
+#    say "TRANSCRIPT STRAND: ".$transcript->seq_region_strand;
+#    say "TRANSCRIPT SLICE: ".$transcript->slice->name;
+#    my $exons = $transcript->get_all_Exons;
+
+#    foreach my $exon (@{$exons}) {
+ #     say "EXON START: ".$exon->seq_region_start;
+ #     say "EXON END: ".$exon->seq_region_end;
+#      say "EXON STRAND: ".$exon->seq_region_strand;
+#      say "EXON SLICE: ".$exon->slice->name;
+#    }
+
     empty_Gene($gene);
     $adaptor->store($gene);
   }
@@ -128,7 +208,7 @@ sub write_output {
 }
 
 sub fetch_source_genes {
-  my ($self,$gene_source_dbs,$gene_allowed_sets) = @_;
+  my ($self,$gene_source_dbs,$allowed_gene_sets) = @_;
 
   my $final_filtered_gene_set = [];
   my $slice = $self->query;
@@ -139,16 +219,16 @@ sub fetch_source_genes {
     my $gene_adaptor = $db_adaptor->get_GeneAdaptor();
     my $all_genes = $gene_adaptor->fetch_all_by_Slice($slice);
     my $filtered_genes = [];
-    if($gene_allowed_sets) {
+    if($allowed_gene_sets) {
       foreach my $gene (@{$all_genes}) {
         my $logic_name = $gene->analysis->logic_name;
-        if($gene_allowed_sets->{$logic_name}) {
-          if(ref($gene_allowed_sets->{$logic_name}) eq 'HASH') {
+        if($allowed_gene_sets->{$logic_name}) {
+          if(ref($allowed_gene_sets->{$logic_name}) eq 'HASH') {
             my $biotype = $gene->biotype;
-            if($gene_allowed_sets->{$logic_name}->{$biotype}) {
+            if($allowed_gene_sets->{$logic_name}->{$biotype}) {
               push(@{$filtered_genes},$gene);
             }
-          } elsif($gene_allowed_sets->{$logic_name}) {
+          } elsif($allowed_gene_sets->{$logic_name}) {
             push(@{$filtered_genes},$gene);
           }
         }
@@ -204,28 +284,64 @@ sub master_genes_hash {
   return($self->param('_master_gene_hash'));
 }
 
+
+sub filter_by_strand {
+  my ($self,$unfiltered_genes,$strand) = @_;
+
+  my $filtered_genes = [];
+  my $slice = $self->query();
+  my $cluster_start = $slice->start;
+  my $cluster_end = $slice->end;
+  foreach my $gene (@{$unfiltered_genes}) {
+    # If the gene is not completely within the cluster boundary or is on the wrong slice then skip it
+    if($gene->seq_region_start < $cluster_start || $gene->seq_region_end > $cluster_end || $gene->strand != $strand) {
+      next;
+    }
+    push(@{$filtered_genes},$gene);
+  }
+  return($filtered_genes);
+}
+
+
 sub process_clusters {
 
   my ($self,$clustered,$unclustered) = @_;
 
-  my $output_genes = [];
-  foreach my $single_cluster (@{$unclustered}) {
-    my $cluster_genes = $single_cluster->get_Genes();
-    foreach my $single_gene (@{$cluster_genes}) {
-      my $output_gene = Bio::EnsEMBL::Gene->new();
-      $output_gene->slice($self->query());
-      $output_gene->analysis($self->analysis);
-      $output_gene->biotype($self->analysis->logic_name);
-      my $transcripts = $single_gene->get_all_Transcripts();
-      my $single_transcript = shift(@{$transcripts});
-      $output_gene->add_Transcript($single_transcript);
-      push(@{$output_genes},$output_gene);
-    }
-  }
 
-  foreach my $single_cluster (@{$clustered}) {
+  my $all_clusters = [@{$clustered},@{$unclustered}];
+
+  my $output_genes = [];
+#  foreach my $single_cluster (@{$unclustered}) {
+#    my $cluster_genes = $single_cluster->get_Genes();
+#    foreach my $single_gene (@{$cluster_genes}) {
+#      my $output_gene = Bio::EnsEMBL::Gene->new();
+#      $output_gene->adaptor($self->hrdb_get_con('dna_db'));
+#      $output_gene->analysis($self->analysis);
+#      $output_gene->biotype($self->analysis->logic_name);
+#      my $transcripts = $single_gene->get_all_Transcripts();
+#      my $single_transcript = shift(@{$transcripts});
+#      my $exons = $single_transcript->get_all_Exons();
+
+#      say "TRANSCRIPT START: ".$single_transcript->start;
+#      say "TRANSCRIPT END: ".$single_transcript->end;
+#      say "TRANSCRIPT STRAND: ".$single_transcript->strand;
+#      say "TRANSCRIPT SLICE: ".$single_transcript->slice->name;
+#      say "FM2 DUMPER TSLICE: ".Dumper($single_transcript->slice);
+#      say "EXON COUNT: ".scalar(@{$exons});
+#      my $slice = $self->query;
+#      $single_transcript->flush_Exons();
+#      foreach my $exon (@{$exons}) {
+#        $exon->slice($slice);
+#        $single_transcript->add_Exon($exon);
+#      }
+
+#      $output_gene->add_Transcript($single_transcript);
+#      push(@{$output_genes},$output_gene);
+#    }
+#  }
+
+  foreach my $single_cluster (@{$all_clusters}) {
     my $combined_gene = Bio::EnsEMBL::Gene->new();
-    $combined_gene->slice($self->query());
     $combined_gene->analysis($self->analysis);
     $combined_gene->biotype($self->analysis->logic_name);
 
@@ -233,8 +349,16 @@ sub process_clusters {
 
     foreach my $single_gene (@{$cluster_genes}) {
       my $transcripts = $single_gene->get_all_Transcripts();
-      my $single_transcript = shift(@{$transcripts});
-      $combined_gene->add_Transcript($single_transcript);
+      foreach my $transcript (@{$transcripts}) {
+        my $exons = $transcript->get_all_Exons();
+        my $slice = $self->query;
+        $transcript->flush_Exons();
+        foreach my $exon (@{$exons}) {
+          $exon->slice($slice);
+          $transcript->add_Exon($exon);
+        }
+        $combined_gene->add_Transcript($transcript);
+      }
     }
     push(@{$output_genes},$combined_gene);
   }
