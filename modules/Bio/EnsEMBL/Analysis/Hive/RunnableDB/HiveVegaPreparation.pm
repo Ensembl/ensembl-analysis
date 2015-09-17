9faf98ff310e69a0ccec7e08521094530ec0bf29
fergalmartin <fergal@ebi.ac.uk>
2015-09-17 10:00:54 +0100

    Change to HiveCCDSAddition.pm, HiveCopyGenes.pm, HiveCreateDatabase.pm, HiveDeleteTranscripts.pm, HiveListUnprocessed.pm, HiveMerge.pm, HiveVegaChecks.pm, HiveVegaPreparation.pm Removed the use of RunnableDB as these modules use HiveBaseRunnableDB instead and have no need to call RunnableDB

--- modules/Bio/EnsEMBL/Analysis/Hive/RunnableDB/HiveVegaPreparation.pm
+++ modules/Bio/EnsEMBL/Analysis/Hive/RunnableDB/HiveVegaPreparation.pm
@@ -68,7 +68,6 @@ package Bio::EnsEMBL::Analysis::Hive::RunnableDB::HiveVegaPreparation;
 use strict;
 use warnings;
 
-use Bio::EnsEMBL::Analysis::RunnableDB;
 use Bio::EnsEMBL::Analysis::Tools::Utilities;
 use Bio::EnsEMBL::Utils::Exception qw(warning throw);
 use parent ('Bio::EnsEMBL::Analysis::Hive::RunnableDB::HiveBaseRunnableDB');
