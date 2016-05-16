ce8df1a09a2fbbefa8c294bf52831eef7f8f573a
fergalmartin <fergal@ebi.ac.uk>
2016-05-16 19:31:39 +0100

    Change to HiveBlast.pm Added in some flow to write output

--- modules/Bio/EnsEMBL/Analysis/Hive/RunnableDB/HiveAssemblyLoading/HiveBlast.pm
+++ modules/Bio/EnsEMBL/Analysis/Hive/RunnableDB/HiveAssemblyLoading/HiveBlast.pm
@@ -208,6 +208,12 @@ sub write_output{
       }
     }
   }
+
+  my $output_hash = {};
+  $output_hash->{'iid'} = $self->param('iid');
+  $self->dataflow_output_id($output_hash,4);
+  $self->dataflow_output_id($output_hash,1);
+
   return 1;
 }
 
