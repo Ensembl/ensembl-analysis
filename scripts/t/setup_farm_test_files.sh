9516a5a5a5604acfab0666f051225e937c3f9631
Rishi Nag <rishi@ebi.ac.uk>
2016-06-29 10:56:51 +0100

    farm test script generation for t dir set

--- scripts/t/setup_farm_test_files.sh
+++ scripts/t/setup_farm_test_files.sh
@@ -17,11 +17,11 @@ do
  filename=${f##*/}
  output_file=${filename%.*}
  echo Using template $filename to generate $output_file
- gawk '{ gsub("EAT_DB_USER", "$db_user"); print }' | \
-     gawk '{ gsub("EAT_DB_PASS", "$db_pass"); print }' | \
-     gawk '{ gsub("EAT_DB_NAME", "$db_name"); print }' | \
-     gawk '{ gsub("EAT_DB_USER", "$db_user_ro"); print }' | \
-     gawk '{ gsub("EAT_DB_HOST", "$db_host"); print }' > $farm_test_dir/$output_file
+ gawk '{ gsub("EAT_DB_USER", "'$db_user'"); print }' $f | \
+     gawk '{ gsub("EAT_DB_PASS", "'$db_pass'"); print }' | \
+     gawk '{ gsub("EAT_DB_NAME", "'$db_name'"); print }' | \
+     gawk '{ gsub("EAT_DB_USER", "'$db_user_ro'"); print }' | \
+     gawk '{ gsub("EAT_DB_HOST", "'$db_host'"); print }' > $farm_test_dir/$output_file
 done
 
 echo Working in $farm_test_dir/conf
