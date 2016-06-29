f28a6eef085cfef4cd0d5e9579f5ef090b835a76
Rishi Nag <rishi@ebi.ac.uk>
2016-06-29 13:21:53 +0100

    filename updates

--- scripts/t/setup_farm_test_files.sh
+++ scripts/t/setup_farm_test_files.sh
@@ -54,4 +54,11 @@ do
  filename=${f##*/}
  output_file=${filename%.*}
  echo Using template $filename to generate $output_file
+ gawk '{ gsub("EAT_DB_USER", "'$db_user'"); print }' $f | \
+     gawk '{ gsub("EAT_DB_PASS", "'$db_pass'"); print }' | \
+     gawk '{ gsub("EAT_DB_PORT", "'$db_port'"); print }' | \
+     gawk '{ gsub("EAT_DB_NAME", "'$db_name'"); print }' | \
+     gawk '{ gsub("EAT_DB_USER", "'$db_user_ro'"); print }' | \
+     gawk '{ gsub("EAT_DB_HOST", "'$db_host'"); print }' > $farm_test_dir/conf/$output_file
+
 done
