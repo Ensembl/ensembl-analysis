ee1a1e4269248acdded79c9be031ec3e37805205
kbillis <kbillis@ebi.ac.uk>
2016-11-14 16:11:29 +0000

    changes on HiveLoadTaxonomyInfo.pm I change the module to an old version. I did small changes too. So, now it is not using production_name, but taxon_id

--- modules/Bio/EnsEMBL/Analysis/Hive/RunnableDB/HiveAssemblyLoading/HiveLoadTaxonomyInfo.pm
+++ modules/Bio/EnsEMBL/Analysis/Hive/RunnableDB/HiveAssemblyLoading/HiveLoadTaxonomyInfo.pm
@@ -1,7 +1,6 @@
 #!/usr/bin/env perl
 
-# Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
-# Copyright [2016] EMBL-European Bioinformatics Institute
+# Copyright [1999-2016] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -31,20 +30,24 @@ sub fetch_input {
                  "into must be passed in with write access");
   }
 
-#  unless($self->param('production_db')) {
-#    throw("You have used the populate_production_tables flag but have not passed in the production db connection hash with ".
-#          "the production_db flag");
-#  }
+  unless($self->param('production_db')) {
+    $self->throw("You have used the populate_production_tables flag but have not passed in the production db connection hash with ".
+          "the production_db flag");
+  }
 
 
   unless($self->param('enscode_root_dir')) {
     $self->throw("enscode_root_dir flag not passed into parameters hash. You need to specify where your code checkout is");
   }
 
+  unless($self->param('output_path')) {
+    $self->throw("You have not specified the path to the main output directory with the -output_path flag, ".
+                 "this is needed to dump the backup tables into");
+  }
 
-#  unless($self->param('taxonomy_db')) {
-#    throw("You have not passed in the taxonomy db connection hash with the taxonomy_db flag");
-#  }
+  unless($self->param('taxonomy_db')) {
+    $self->throw("You have not passed in the taxonomy db connection hash with the taxonomy_db flag");
+  }
 
   return 1;
 }
@@ -53,13 +56,12 @@ sub run {
   my $self = shift;
 
   my $target_db = $self->param('target_db');
-#  my $taxonomy_db = $self->param('taxonomy_db');
+  my $taxonomy_db = $self->param('taxonomy_db');
   my $enscode_dir = $self->param('enscode_root_dir');
-#  my $taxon_id = $self->param('taxon_id');
+  my $taxon_id = $self->param('taxon_id');
 
   say "Running load_taxonomy script on target db...\n";
-  $self->load_taxonomy($target_db,$enscode_dir);
-#  $self->load_taxonomy($target_db,$taxonomy_db,$enscode_dir,$taxon_id);
+  $self->load_taxonomy($target_db,$taxonomy_db,$enscode_dir,$taxon_id);
   say "...finished running script on target db\n";
   return 1;
 }
@@ -71,8 +73,8 @@ sub write_output {
 }
 
 sub load_taxonomy {
- my ($self,$target_db,$enscode_dir) = @_;
-#  my ($self,$target_db,$taxonomy_db,$enscode_dir,$taxon_id) = @_;
+  my ($self,$target_db,$taxonomy_db,$enscode_dir,$taxon_id) = @_;
+
 
   my $target_dbname;
   my $target_host;
@@ -88,45 +90,37 @@ sub load_taxonomy {
 
 
   unless($target_dbname && $target_host && $target_user && $target_pass) {
-    throw("The connection info for the database could not be recovered from the parameters passed in. ".
+    $self->throw("The connection info for the database could not be recovered from the parameters passed in. ".
           "Make sure you pass in user_w, pass_w and either a db string or a hash to target_db");
   }
 
   unless($target_dbname && $target_host && $target_user && $target_pass) {
-    throw("The connection info for the database could not be recovered from the parameters passed in. ".
+    $self->throw("The connection info for the database could not be recovered from the parameters passed in. ".
           "Make sure you pass in user_w, pass_w and either a db string or a hash to target_db");
   }
 
-#  my $taxonomy_host = $taxonomy_db->{'-host'};
-#  my $taxonomy_user = $taxonomy_db->{'-user'};
-#  my $taxonomy_port = $taxonomy_db->{'-port'};
-#  my $taxonomy_dbname = $taxonomy_db->{'-dbname'};
+  my $taxonomy_host = $taxonomy_db->{'-host'};
+  my $taxonomy_user = $taxonomy_db->{'-user'};
+  my $taxonomy_port = $taxonomy_db->{'-port'};
+  my $taxonomy_dbname = $taxonomy_db->{'-dbname'};
 
-
-
-  my $taxonomy_script = $self->param('enscode_root_dir')."/ensembl-production/scripts/production_database/populate_species_meta.pl";
+  my $taxonomy_script = $self->param('enscode_root_dir')."/ensembl-pipeline/scripts/load_taxonomy.pl";
   my $cmd = "perl ".$taxonomy_script.
-            " -h ".$target_host.
-            " -u ".$target_user.
-            " -p ".$target_pass.
-            " -d ".$target_dbname.
-            " -P ".$target_port;
-
-#  my $cmd = "perl ".$taxonomy_script.
-#            " -lcdbhost ".$target_host.
-#            " -lcdbuser ".$target_user.
-#            " -lcdbpass ".$target_pass.
-#            " -lcdbname ".$target_dbname.
-#            " -lcdbport ".$target_port.
-#            " -taxondbhost ".$taxonomy_host.
-#            " -taxondbport ".$taxonomy_port.
-#            " -taxondbname ".$taxonomy_dbname.
-#            " -taxon_id ".$taxon_id;
+            " -lcdbhost ".$target_host.
+            " -lcdbuser ".$target_user.
+            " -lcdbpass ".$target_pass.
+            " -lcdbname ".$target_dbname.
+            " -lcdbport ".$target_port.
+            " -taxondbhost ".$taxonomy_host.
+            " -taxondbport ".$taxonomy_port.
+            " -taxondbname ".$taxonomy_dbname.
+            " -taxon_id ".$taxon_id;
             # Note script has no flag for -taxonuser, maybe it should be added. Hardcoded to ensro at the moment
+print "DEBUG:: $cmd \n"; 
 
   my $return = system($cmd);
   if($return) {
-    throw("The load_taxonomy script returned a non-zero exit value. Commandline used:\n".$cmd);
+    $self->throw("The load_taxonomy script returned a non-zero exit value. Commandline used:\n".$cmd);
   }
 
 }
