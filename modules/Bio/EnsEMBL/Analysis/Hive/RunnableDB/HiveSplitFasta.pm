ab5fc04b13a3f4acf6da97b156970ff845d8cad4
Thibaut Hourlier <thibaut@ebi.ac.uk>
2017-02-21 10:50:21 +0000

    HiveSplitFasta is now a JobFactory, refactor some code

--- modules/Bio/EnsEMBL/Analysis/Hive/RunnableDB/HiveSplitFasta.pm
+++ modules/Bio/EnsEMBL/Analysis/Hive/RunnableDB/HiveSplitFasta.pm
@@ -1,6 +1,7 @@
 =head1 LICENSE
 
-Copyright [2009-2014] EMBL-European Bioinformatics Institute
+Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+Copyright [2016-2017] EMBL-European Bioinformatics Institute
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,8 +15,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 
-=cut
-
 =head1 Bio::EnsEMBL::Analysis::Hive::RunnableDB::HiveSplitFasta
 =cut
 
@@ -23,17 +22,21 @@ package Bio::EnsEMBL::Analysis::Hive::RunnableDB::HiveSplitFasta;
 
 use strict;
 use warnings;
-use base ('Bio::EnsEMBL::Analysis::Hive::RunnableDB::HiveBaseRunnableDB');
 
 use Bio::SeqIO;
 use File::Basename qw(dirname fileparse);
 use File::Path qw(make_path remove_tree);
+use File::Spec::Functions qw(catdir catfile);
 use POSIX qw(ceil);
+
+use parent ('Bio::EnsEMBL::Hive::RunnableDB::JobFactory');
+
 
 sub param_defaults {
   my ($self) = @_;
   
   return {
+    %{$self->SUPER::param_defaults},
     'out_dir'                 => undef,  # Top-level directory for output
     'max_seqs_per_file'       => 100,    # Maximum number of records in a file
     'max_seq_length_per_file' => undef,  # Maximum sequence length in a file
@@ -42,6 +45,7 @@ sub param_defaults {
     'delete_existing_files'   => 0,      # Ensure that directories only contains files generated with the latest execution
     'unique_file_names'       => 0,      # Ensure that output file names are unique across all directories?
     'delete_original_file'    => 0,      # After splitting, delete original fasta file?
+    'append_filename'         => 0,      # If you want to append the basename as a directory set it to 1, useful if you have many files to process
   };
   
 }
@@ -55,31 +59,32 @@ sub fetch_input {
     $self->throw("Fasta file '$fasta_file' does not exist");
   }
   
+  # Do nothing if there's nothing to do...
+  if (-s $fasta_file == 0) {
+    $self->input_job->autoflow(0);
+    $self->complete_early("$fasta_file is empty");
+  }
+
   if (!defined $out_dir) {
     $out_dir = dirname($fasta_file);
-    $self->param('out_dir', $out_dir)
-  } else {
-    if (!-e $out_dir) {
-      $self->warning("Output directory '$out_dir' does not exist. I shall create it.");
-      make_path($out_dir) or $self->throw("Failed to create output directory '$out_dir'");
-    }
   }
-  
-}
+  my ($basename, undef, undef) = fileparse($fasta_file, qr/\.[^.]*/);
+  if ($self->param('append_filename')) {
+    $out_dir = catdir($out_dir, $basename);
+  }
+  if (!-d $out_dir) {
+    $self->warning("Output directory '$out_dir' does not exist. I shall create it.");
+    make_path($out_dir) or $self->throw("Failed to create output directory '$out_dir'");
+  }
 
-sub run {
-  my $self = shift @_;
-  
   # All these should have appropriate values, either from param_defaults
   # or fetch_input. (If the user has provided anything but integers for
   # the max_* parameters, the script will automatically fail when it does
   # numeric comparisons with those values, so don't bother checking them.)
-  my $fasta_file = $self->param('fasta_file');
   my $max_records = $self->param('max_seqs_per_file');
   my $max_seq_length = $self->param('max_seq_length_per_file');
   my $max_files = $self->param('max_files_per_directory');
   my $max_dirs = $self->param('max_dirs_per_directory');
-  $self->param('split_files', []);
   
   # Tracking parameters to determine when we need a new file or directory.
   my $record_count = 0;
@@ -87,18 +92,14 @@ sub run {
   my $file_count = 1;
   my $dir_index = 0;
   
-  # Do nothing if there's nothing to do...
-  if (-s $fasta_file == 0) { 
-    $self->input_job->autoflow(0);
-    return;
-  }
-  
   # Need to calculate required degree of subdirectory nesting.
   $self->directory_structure();
   $self->delete_existing_files() if $self->param('delete_existing_files');
   
-  my ($basename, undef, undef) = fileparse($fasta_file, qr/\.[^.]*/);
-  my $split_file = $self->new_filename($dir_index, $basename, $file_count);
+  my $dirs = $self->param('dirs');
+  my @files;
+  my $split_file = $self->new_filename(catdir($out_dir, $dirs->[$dir_index]), $basename, $file_count);
+  push(@files, $split_file);
   my $original = Bio::SeqIO->new(-format => 'Fasta', -file => $fasta_file);
   my $split = Bio::SeqIO->new(-format => 'Fasta', -file => ">$split_file");
   
@@ -123,7 +124,8 @@ sub run {
       } else {
         $file_count++;
       }
-      $split_file = $self->new_filename($dir_index, $basename, $file_count);
+      $split_file = $self->new_filename(catdir($out_dir, $dirs->[$dir_index]), $basename, $file_count);
+      push(@files, $split_file);
       $split = Bio::SeqIO->new(-format => 'Fasta', -file => ">$split_file");
     }
     
@@ -131,27 +133,16 @@ sub run {
     $self->throw("Failed to write sequence to '$split_file'") unless $success;
     
   }
+  $self->param('inputlist', \@files);
   
   if ($self->param('delete_original_file')) {
     unlink $fasta_file;
   }
-  
 }
 
-sub write_output {
-  my ($self) = @_;
-  
-  foreach my $split_file (@{$self->param('split_files')}) {
-    $self->dataflow_output_id({'split_file' => $split_file}, 2);
-  }
-  
-}
 
 sub new_filename {
-  my ($self, $dir_index, $basename, $file_count) = @_;
-  my $out_dir = $self->param('out_dir');
-  my @dirs = @{$self->param('dirs')};
-  my $sub_dir = "$out_dir/".$dirs[$dir_index];
+  my ($self, $sub_dir, $basename, $file_count) = @_;
   
   if (!-e $sub_dir) {
     make_path($sub_dir) or $self->throw("Failed to create output directory '$sub_dir'");
@@ -159,15 +150,11 @@ sub new_filename {
   
   my $split_file;
   if ($self->param('unique_file_names')) {
-    $split_file = "$sub_dir/$basename.$dir_index.$file_count.fa";
+    $split_file = catfile($sub_dir, "$basename.$$.$file_count.fa");
   } else {
-    $split_file = "$sub_dir/$basename.$file_count.fa";
+    $split_file = catfile($sub_dir, "$basename.$file_count.fa");
   }
-  my @split_files = (@{$self->param('split_files')}, $split_file);
-  $self->param('split_files', \@split_files);
-  
   return $split_file;
-  
 }
 
 sub directory_structure {
