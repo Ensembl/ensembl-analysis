7480f32bb554121da7080987a15cf6f27dd31395
fergalmartin <fergal@ebi.ac.uk>
2017-06-22 20:34:00 +0100

    Change to HiveAnalysisSanityCheck.pm Major refactoring to be a big more sensible and work off a single list of logic names for the genome prep stuff. Then added in the gene/transcript db checks. Had to make it so that the biotype is not an exact match, but instead matched off a regex. This is a bit dangerous but the other option would be to fill the corresponding static config with endless biotypes that are too fine grained to check reliably

--- modules/Bio/EnsEMBL/Analysis/Hive/RunnableDB/HiveAnalysisSanityCheck.pm
+++ modules/Bio/EnsEMBL/Analysis/Hive/RunnableDB/HiveAnalysisSanityCheck.pm
@@ -19,12 +19,16 @@ package Bio::EnsEMBL::Analysis::Hive::RunnableDB::HiveAnalysisSanityCheck;
 use strict;
 use warnings;
 use feature 'say';
+use Data::Dumper;
 
 use parent ('Bio::EnsEMBL::Analysis::Hive::RunnableDB::HiveBaseRunnableDB');
 
 sub fetch_input {
   my $self = shift;
 
+  if($self-param('skip_check')) {
+    $self->complete_early('Skip check flag is enabled, so no check will be carried out');
+  }
   $self->param_required('sanity_check_type');
 
   return 1;
@@ -50,8 +54,10 @@ sub write_output {
 sub run_sanity_checks {
   my ($self,$type) = @_;
 
-  if($type eq 'post_genome_preparation') {
+  if($type eq 'genome_preparation_checks') {
     $self->post_genome_preparation();
+  } elsif($type eq 'gene_db_checks') {
+    $self->gene_db_checks();
   } else {
     $self->throw('The sanity check type you specified is not recognised. Type: '.$type);
   }
@@ -61,104 +67,110 @@ sub run_sanity_checks {
 sub post_genome_preparation {
   my ($self) = @_;
 
-  # ToDo, add in a hash containing min counts based on average counts for other species in the clade
-  my $logic_names = $self->param_required('feature_logic_names');
-#  my $simple_logic_names = $self->param_required('simple_feature_logic_names');
-#  my $daf_logic_names = $self->param_required('dna_align_feature_logic_names');
-#  my $paf_logic_names = $self->param_required('protein_align_feature_logic_names');
-#  my $prediction_logic_names = $self->param_required('prediction_transcript_logic_names');
-
+  my $logic_names = $self->param_required('min_allowed_feature_counts');
   my $target_db = $self->param_required('target_db');
   my $genome_prep_dba = $self->hrdb_get_dba($target_db);
 
   my $feature_adaptors = {
                            'repeat' => $genome_prep_dba->get_RepeatFeatureAdaptor(),
                            'simple' => $genome_prep_dba->get_SimpleFeatureAdaptor(),
-                           'prediction' => $genome_prep_dba->get_PredictionTranscriptAdaptor(),
+                           'prediction transcript' => $genome_prep_dba->get_PredictionTranscriptAdaptor(),
                            'dna align' => $genome_prep_dba->get_DnaAlignFeatureAdaptor(),
                            'protein align' => $genome_prep_dba->get_ProteinAlignFeatureAdaptor(),
                          };
-#  my $repeat_feature_adaptor = $genome_prep_dba->get_RepeatFeatureAdaptor();
-#  my $simple_feature_adaptor = $genome_prep_dba->get_SimpleFeatureAdaptor();
-#  my $daf_adaptor = $genome_prep_dba->get_DnaAlignFeatureAdaptor();
-#  my $paf_adaptor = $genome_prep_dba->get_ProteinAlignFeatureAdaptor();
-#  my $prediction_transcript_adaptor = $genome_prep_dba->get_PredictionTranscriptAdaptor();
 
   my $feature_counts = {};
   my $previous_feature_type = "";
-  foreach my $logic_name (sort {$logic_names->{$a} cmp $logic_names->{$b}} keys %{$logic_names}) {
-    my $feature_type = $logic_names->{$logic_name};
-    my $feature_adaptor = $feature_adaptors->{$feature_type};
+  foreach my $logic_name (sort {$logic_names->{$a}->[1] cmp $logic_names->{$b}->[1]} keys %{$logic_names}) {
+    my ($min_count,$feature_type) = @{$logic_names->{$logic_name}};
     if($feature_type ne $previous_feature_type) {
       say "\nChecking ".$feature_type." features";
       $previous_feature_type = $feature_type;
     }
-    $self->count_features($logic_name,$feature_adaptor,$feature_counts);
-  }
-
-#  say "Checking repeat features";
-#  my $repeat_counts = $self->count_features($repeat_logic_names,$repeat_feature_adaptor);
-#  say "\nChecking simple features";
-#  my $simple_feature_counts = $self->count_features($simple_logic_names,$simple_feature_adaptor);
-#  say "\nChecking dna align features";
-#  my $daf_counts = $self->count_features($daf_logic_names,$daf_adaptor);
-#  say "\nChecking protein align features";
-#  my $paf_counts = $self->count_features($paf_logic_names,$paf_adaptor);
-#  say "\nChecking prediction transcript features";
-#  my $prediction_transcript_counts = $self->count_features($prediction_logic_names,$prediction_transcript_adaptor);
-
-  if($self->param('min_allowed_feature_counts')) {
-#    my $all_counts = {%$repeat_counts,%$simple_feature_counts,%$daf_counts,%$paf_counts,%$prediction_transcript_counts};
-#    $self->check_min_counts($all_counts);
-    $self->check_min_counts($feature_counts);
+    my $observed_count = $self->count_features($logic_name,$feature_adaptors->{$feature_type});
+    say "Count for ".$logic_name.": ".$observed_count;
+    unless($observed_count >= $min_count) {
+      $self->throw("Observed value was too low for ".$logic_name.", min allowed: ".$min_count.", observed: ".$observed_count);
+    }
   }
 }
 
+sub gene_db_checks {
+  my ($self) = @_;
+
+  my $logic_names = $self->param_required('min_allowed_feature_counts')->{'logic_names'};
+  my $biotypes = $self->param_required('min_allowed_feature_counts')->{'biotypes'};
+
+  my $target_db = $self->param_required('target_db');
+  my $genome_prep_dba = $self->hrdb_get_dba($target_db);
+
+  # Note the check will be done on the transcript level for now as that's where we typically label biotypes
+  my $transcript_adaptor = $genome_prep_dba->get_TranscriptAdaptor();
+  unless($logic_names || $biotypes) {
+    $self->throw("No entries for either logic_names or biotypes where found in the min_allowed_feature_counts hash");
+  }
+
+  # Count the transcripts per logic name
+  my $transcripts = $transcript_adaptor->fetch_all();
+
+  # Count all the biotypes once, since there's no biotype constraint for transcript adaptors
+  my $observed_biotype_counts = {};
+  my $observed_logic_name_counts = {};
+
+  foreach my $transcript (@{$transcripts}) {
+    my $biotype = $transcript->biotype;
+    my $logic_name = $transcript->analysis->logic_name;
+    if($observed_biotype_counts->{$biotype}) {
+      $observed_biotype_counts->{$biotype}++;
+    } else {
+      $observed_biotype_counts->{$biotype} = 1;
+    }
+    if($observed_logic_name_counts->{$logic_name}) {
+      $observed_logic_name_counts->{$logic_name}++;
+    } else {
+      $observed_logic_name_counts->{$logic_name} = 1;
+    }
+  }
+
+  say "Checking logic names:";
+ foreach my $logic_name (keys(%{$logic_names})) {
+   my $min_count = $logic_names->{$logic_name};
+   my $observed_count = $observed_logic_name_counts->{$logic_name};
+   say "Observed gene/transcript count for ".$logic_name.": ".$observed_count;
+   unless($observed_count >= $min_count) {
+     $self->throw("Gene/transcript count is below the min value of ".$min_count." for ".$logic_name.", observed count: ".$observed_count);
+   }
+ }
+
+  # Count the transcripts per biotype
+  # Not particularly happy with this, I think it is good to have a way to specify generic parts of a biotype, but it comes at the
+  # expense of having to pattern match, which is dangerous
+  say "\nChecking biotypes:";
+  my $observed_biotype_keys = qr/${\ join('|', map quotemeta, keys %$observed_biotype_counts) }/;
+  foreach my $biotype (keys(%{$biotypes})) {
+    my $min_count = $biotypes->{$biotype};
+    my $observed_count = 0;
+    while($observed_biotype_keys =~ s/$biotype[^|]*//) {
+      my $matching_biotype = $&;
+      my $matching_biotype_count = $observed_biotype_counts->{$matching_biotype};
+      $observed_count += $matching_biotype_count;
+    }
+
+    say "Observed gene/transcript count for ".$biotype.": ".$observed_count;
+    unless($observed_count && ($observed_count >= $min_count)) {
+      $self->throw("Gene/transcript count is below the min value of ".$min_count." for ".$biotype.", observed count: ".$observed_count);
+    }
+  }
+
+}
+
+
 sub count_features {
-  my ($self,$logic_name,$adaptor,$feature_counts) = @_;
+  my ($self,$logic_name,$adaptor) = @_;
 
   my $feature_set = $adaptor->fetch_all_by_logic_name($logic_name);
   my $feature_count = scalar(@{$feature_set});
-  unless($feature_count) {
-    $self->throw("Feature count is 0 for ".$logic_name);
-  } else {
-    say "Feature count for ".$logic_name.": ".$feature_count;
-  }
-
-  $feature_counts->{$logic_name} = $feature_count;
+  return($feature_count);
 }
-
-#sub count_features {
-#  my ($self,$logic_names,$adaptor) = @_;
-#  my $feature_counts = {};
-#  foreach my $logic_name (keys(%{$logic_names})) {
-#    my $feature_set = $adaptor->fetch_all_by_logic_name($logic_name);
-#    my $feature_count = scalar(@{$feature_set});
-#    unless($feature_count) {
-#      $self->throw("Feature count is 0 for ".$logic_name);
-#    } else {
-#      say "Feature count for ".$logic_name.": ".$feature_count;
-#    }
-#    $feature_counts->{$logic_name} = $feature_count;
-#  }
-#  return($feature_counts);
-#}
-
-
-sub check_min_counts {
-  my ($self,$observed_counts) = @_;
-  my $min_allowed_counts = $self->param('min_allowed_feature_counts');
-  foreach my $logic_name (keys(%$min_allowed_counts)) {
-    my $expected_value = $min_allowed_counts->{$logic_name};
-    unless(exists $observed_counts->{$logic_name}) {
-      $self->throw("A logic name from the min counts hash was not found in the observed hash. Logic name: ".$logic_name);
-    }
-    my $observed_value = $observed_counts->{$logic_name};
-    unless($observed_value >= $expected_value) {
-      $self->throw("Observed value was too low for ".$logic_name.", min allowed: ".$expected_value.", observed: ".$observed_value);
-    }
-  }
-}
-
 
 1;
