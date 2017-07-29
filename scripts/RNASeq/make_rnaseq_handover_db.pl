2293335849658035bab6c49fb341dc26a2f7b05d
fergalmartin <fergal@ebi.ac.uk>
2017-07-29 20:31:21 +0100

    Change to make_rnaseq_handover_db.pl Disabled the core API copy of the dna_align_features and replaced with a mysqldump that tracks when the analysis ids are out of sync and corrects them

--- scripts/RNASeq/make_rnaseq_handover_db.pl
+++ scripts/RNASeq/make_rnaseq_handover_db.pl
@@ -72,18 +72,18 @@ $sth_clean->execute();
 # There are a bunch of ways this could be done, including parallelising it outside
 # this script, but I think that the pipeline should just be fixed and then this code
 # deleted and the code commented out below it should be activated
-my $refine_daf_adaptor = $refine_db->get_DnaAlignFeatureAdaptor();
-my $blast_daf_adaptor = $blast_db->get_DnaAlignFeatureAdaptor();
-my $refine_slice_adaptor = $refine_db->get_SliceAdaptor();
-my $slices = $refine_slice_adaptor->fetch_all('toplevel');
-say "Transferring DAFs from refine db to blast db...";
-foreach my $slice (@$slices) {
-  my $refine_dafs = $refine_daf_adaptor->fetch_all_by_Slice($slice);
-  foreach my $refine_daf (@$refine_dafs) {
-    $blast_daf_adaptor->store($refine_daf);
-  }
-}
-say "Completed transfer of DAFs from refine db to blast db";
+#my $refine_daf_adaptor = $refine_db->get_DnaAlignFeatureAdaptor();
+#my $blast_daf_adaptor = $blast_db->get_DnaAlignFeatureAdaptor();
+#my $refine_slice_adaptor = $refine_db->get_SliceAdaptor();
+#my $slices = $refine_slice_adaptor->fetch_all('toplevel');
+#say "Transferring DAFs from refine db to blast db...";
+#foreach my $slice (@$slices) {
+#  my $refine_dafs = $refine_daf_adaptor->fetch_all_by_Slice($slice);
+#  foreach my $refine_daf (@$refine_dafs) {
+#    $blast_daf_adaptor->store($refine_daf);
+#  }
+#}
+#say "Completed transfer of DAFs from refine db to blast db";
 
 # Note this code cannot be used currently as the refine and blast tables seem to be
 # out of sync with one another, if the pipeline is fixed to do this then reactivate it
@@ -212,6 +212,7 @@ foreach my $logic_name (keys(%initial_logic_names)) {
                                                                         $dna_align_feature_logic_name;
   }
 
+  # Update gene and transcript tables
   $sth_update = $blast_db->dbc->prepare('UPDATE gene join analysis using(analysis_id) set'.
                                         ' gene.analysis_id=? where gene.analysis_id=?');
   $sth_update->bind_param(1,$gene_analysis_id);
@@ -220,6 +221,7 @@ foreach my $logic_name (keys(%initial_logic_names)) {
   $sth_update = $blast_db->dbc->prepare('UPDATE transcript join gene using(gene_id) set transcript.analysis_id=gene.analysis_id');
   $sth_update->execute();
 
+  # Update intron supporting evidence table
   $sth_update = $blast_db->dbc->prepare('UPDATE intron_supporting_evidence join analysis using(analysis_id) set'.
                                         ' intron_supporting_evidence.analysis_id=? where intron_supporting_evidence.analysis_id=?');
   $sth_update->bind_param(1,$intron_supporting_evidence_analysis_id);
@@ -233,21 +235,27 @@ foreach my $logic_name (keys(%initial_logic_names)) {
 
   # This SQL does not do anything with the current pipeline, but should work when the pipeline is updated
   # to point the approriate rnaseq analyses to the data files
-  $sth_update = $blast_db->dbc->prepare('UPDATE data_file join analysis using(analysis_id) set'.
-                                  ' data_file.analysis_id=? where data_file.analysis_id=?');
-  $sth_update->bind_param(1,$data_file_analysis_id);
-  $sth_update->bind_param(2,$analysis_id);
-  $sth_update->execute();
+#  $sth_update = $blast_db->dbc->prepare('UPDATE data_file join analysis using(analysis_id) set'.
+#                                  ' data_file.analysis_id=? where data_file.analysis_id=?');
+#  $sth_update->bind_param(1,$data_file_analysis_id);
+#  $sth_update->bind_param(2,$analysis_id);
+#  $sth_update->execute();
 
 
-  $sth_update = $blast_db->dbc->prepare('UPDATE dna_align_feature join analysis using(analysis_id) set'.
-                                  ' dna_align_feature.analysis_id=? where dna_align_feature.analysis_id=?');
-  $sth_update->bind_param(1,$dna_align_feature_analysis_id);
-  $sth_update->bind_param(2,$analysis_id);
-  $sth_update->execute();
-}
 
-# Delete code below when the pipeline is updated!!!!!
+  # This SQL does not do anything with the current pipeline, but should work when the pipeline is updated
+  # to sync the analyses tables
+#  $sth_update = $blast_db->dbc->prepare('UPDATE dna_align_feature join analysis using(analysis_id) set'.
+#                                  ' dna_align_feature.analysis_id=? where dna_align_feature.analysis_id=?');
+#  $sth_update->bind_param(1,$dna_align_feature_analysis_id);
+#  $sth_update->bind_param(2,$analysis_id);
+#  $sth_update->execute();
+
+
+} # end foreach my $logic_name
+
+# DELETE CODE SECTION: Delete code below when the pipeline is updated to assign correct logic names to data file and daf!!!!!
+# This will fix the data file and daf tables
 my $sth_fix_merged = $blast_db->dbc->prepare('SELECT name from data_file where name != "merged"');
 $sth_fix_merged->execute();
 my $example_name = $sth_fix_merged->fetchrow_array;
@@ -259,11 +267,14 @@ $sth_fix_merged = $blast_db->dbc->prepare('UPDATE data_file set name = ? where n
 $sth_fix_merged->bind_param(1,$fixed_merged_name);
 $sth_fix_merged->execute();
 
+update_daf($blast_db,$refine_db,\%initial_logic_names);
+# END DELTE CODE SECTION
+
 # Now do a final clean up of logic_names
 my $sth_clean_logic_names = $blast_db->dbc->prepare('DELETE from analysis where logic_name not like "'.$species.'_%_rnaseq_gene"'.
                                                     ' and logic_name not like "'.$species.'_%_rnaseq_ise"'.
                                                     ' and logic_name not like "'.$species.'_%_rnaseq_daf"'.
-                                                    ' and logic_name not like "'.$species.'_%_rnaseq_bwa"'.
+                                                    ' and logic_name not like "'.$species.'_%_rnaseq_bam"'.
                                                     ' and logic_name != "other_protein"');
 $sth_clean_logic_names->execute();
 
@@ -315,7 +326,7 @@ sub update_data_file {
   # This whole sub needs to be deleted once the pipeline is updated
   my $tissue = $data_file_logic_name;
   $tissue =~ s/$species\_//;
-  $tissue =~ s/\_rnaseq\_bwa//;
+  $tissue =~ s/\_rnaseq\_bam//;
 
   my $name_match;
   my $sth_data_file_update = $blast_db->dbc->prepare('UPDATE data_file join analysis using(analysis_id) set'.
@@ -332,3 +343,81 @@ sub update_data_file {
   $sth_data_file_update->execute();
 }
 
+sub update_daf {
+  my ($blast_db,$refine_db,$initial_logic_names) = @_;
+
+  my $shift_value = 1000000;
+  my $dump_path = "/tmp/".$refine_db->dbc->dbname.".".time().".tmp";
+
+  # Retrieve all the refine rnaseq logic names and store their analysis ids
+  my $sth_select_refine = $refine_db->dbc->prepare('SELECT analysis_id,logic_name from analysis where logic_name like "'.$species.'_%_rnaseq"');
+  $sth_select_refine->execute();
+
+  my %refine_logic_names = ();
+  while(my ($analysis_id,$logic_name) =  $sth_select_refine->fetchrow_array()) {
+    $refine_logic_names{$logic_name} = $analysis_id;
+  }
+
+  unless(scalar(keys(%refine_logic_names))) {
+    die "Could not find any logic names of the format: ".$species.'_%_rnaseq';
+  }
+
+  my %blast_logic_names = %{$initial_logic_names};
+
+  my $dump_daf_table = "mysqldump -u".$refineuser.
+                              " -h".$refinehost.
+                              " -P".$refineport.
+                              " ".$refinedbname." dna_align_feature > ".$dump_path;
+
+  my $cmd_result = system($dump_daf_table);
+  if($cmd_result) {
+    die "Failed to dump refine db dna_align_feature table. Command line used:\n".$dump_daf_table;
+  }
+
+  my $load_daf_table = "mysql -u".$blastuser.
+                            " -h".$blasthost.
+                            " -p".$blastpass.
+                            " -P".$blastport.
+                            " -D".$blastdbname.
+                            " < ".$dump_path;
+
+  $cmd_result = system($load_daf_table);
+  if($cmd_result) {
+    die "Failed to laod refine db dna_align_feature table. Command line used:\n".$load_daf_table;
+  }
+
+  # Clean the dump. Heheheh.
+  system("rm ".$dump_path);
+
+  # Need to shift analysis ids in case of conflict, so will temporarily shift
+  # them outside of the allowed range of analysis ids. The small int range is only 65000 or so
+  my $sth_update_daf = $blast_db->dbc->prepare('ALTER TABLE dna_align_feature MODIFY analysis_id INT');
+  $sth_update_daf->execute();
+  $sth_update_daf = $blast_db->dbc->prepare('UPDATE dna_align_feature set analysis_id=(analysis_id + ?)');
+  $sth_update_daf->bind_param(1,$shift_value);
+  $sth_update_daf->execute();
+
+  # Now all analysis ids are shifted by 1000000, can cleanly update with no conflicts
+  foreach my $refine_logic_name (keys(%refine_logic_names)) {
+    my $refine_analysis_id = $refine_logic_names{$refine_logic_name};
+    my $blast_logic_name = $refine_logic_name."_daf";
+
+    my $sth_select_blast_id = $blast_db->dbc->prepare('SELECT analysis_id from analysis where logic_name = ?');
+    $sth_select_blast_id->bind_param(1,$blast_logic_name);
+    $sth_select_blast_id->execute();
+    my $blast_analysis_id = $sth_select_blast_id->fetchrow_array;
+    unless($blast_analysis_id) {
+      die "Could not find an analysis id for the following refine db logic_name in the blast db: ".$blast_logic_name;
+    }
+
+    $sth_update_daf = $blast_db->dbc->prepare('UPDATE dna_align_feature set analysis_id=? where analysis_id=?');
+    $sth_update_daf->bind_param(1,$blast_analysis_id);
+    $sth_update_daf->bind_param(2,$refine_analysis_id + $shift_value);
+    $sth_update_daf->execute();
+  }
+
+  # Now alter the table back
+  $sth_update_daf = $blast_db->dbc->prepare('ALTER TABLE dna_align_feature MODIFY analysis_id SMALLINT');
+  $sth_update_daf->execute();
+
+}
