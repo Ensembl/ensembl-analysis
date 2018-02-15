7c2d61ffd6d9f54dae0f5f24ab8aee5f53a8f661
Thibaut Hourlier <thibaut@ebi.ac.uk>
2018-02-15 17:51:46 +0000

    Added missing copyright to scripts and modules

--- scripts/Merge/check_vega_met_stop.pl
+++ scripts/Merge/check_vega_met_stop.pl
@@ -1,4 +1,18 @@
 #!/usr/bin/env perl
+# Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+# Copyright [2016-2018] EMBL-European Bioinformatics Institute
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 # $Id: check_vega_met_stop.pl,v 1.9 2013-03-05 13:47:18 fm2 Exp $
 
