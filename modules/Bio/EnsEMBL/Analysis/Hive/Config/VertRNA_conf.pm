ccf1966cd8c29e7981026e78d5b05cf1a4dd2d84
Thibaut Hourlier <thibaut@ebi.ac.uk>
2018-02-22 16:03:21 +0000

    Reformat of the analysis. Send a message on Slack when the DB is ready

--- modules/Bio/EnsEMBL/Analysis/Hive/Config/VertRNA_conf.pm
+++ modules/Bio/EnsEMBL/Analysis/Hive/Config/VertRNA_conf.pm
@@ -60,10 +60,12 @@ sub default_options {
     return {
         %{ $self->SUPER::default_options() },
 
-        'base_ena_ftp'  => $ENV{'BASE_ENA_FTP'} || 'ftp://ftp.ebi.ac.uk/pub/databases/ena/sequence/release/std',
-        'vertrna_file'  => $ENV{'VERTRNA_FILE'} || 'embl_vertrna-1',
-        'embl2fasta_script'  => $ENV{'EMBL2FASTA_SCRIPT'} || catfile($ENV{ENSCODE}, 'ensembl-analysis', 'scripts', 'databases', 'EMBL2fasta.pl'),
-        'vertrna_version'  => $ENV{'VERTRNA_VERSION'},
+        base_ena_ftp  => $ENV{'BASE_ENA_FTP'} || 'ftp://ftp.ebi.ac.uk/pub/databases/ena/sequence/release/std',
+        vertrna_file  => $ENV{'VERTRNA_FILE'} || 'embl_vertrna-1',
+        embl2fasta_script  => $ENV{'EMBL2FASTA_SCRIPT'} || catfile($ENV{ENSCODE}, 'ensembl-analysis', 'scripts', 'databases', 'EMBL2fasta.pl'),
+        vertrna_version  => $ENV{'VERTRNA_VERSION'},
+        slack_token   => $ENV{SLACK_GENEBUILD},
+        slack_hook_url => 'https://hooks.slack.com/services/'.$self->o('slack_token'),
     };
 }
 
@@ -79,146 +81,170 @@ sub resource_classes {
 
 sub pipeline_analyses {
     my ($self) = @_;
+
     my @analyses = (
-
-        {   -logic_name => 'setup_directory',
-            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
-            -parameters => {
-                'cmd'     => 'mkdir #vertrna_dir#; lfs getname #vertrna_dir#; if [ $? -eq 0 ]; then lfs setstripe -c -1 #vertrna_dir#;fi',
-            },
-            -input_ids  => [ { vertrna_dir => $self->o('vertrna_dir'), blast_type => $self->o('blast_type'), vertrna_file => $self->o('vertrna_file')} ],
-            -flow_into => {
-              '1' => ['create_tax_ids'],
-            }
-
+        {
+          -logic_name => 'setup_directory',
+          -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
+          -parameters => {
+            cmd     => 'mkdir #vertrna_dir#; which lfs &> /dev/null; if [ $? -eq 0 ]; then lfs getstripe #vertrna_dir# &> /dev/null; if [ $? -eq 0 ];then lfs setstripe -c -1 #vertrna_dir#;fi;fi',
+          },
+          -input_ids  => [
+            { vertrna_dir => $self->o('vertrna_dir'), blast_type => $self->o('blast_type'), vertrna_file => $self->o('vertrna_file')},
+          ],
+          -flow_into => {
+          '1' => ['create_tax_ids'],
+          },
         },
 
-        {   -logic_name => 'create_tax_ids',
-            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::JobFactory',
-            -parameters => {
-                'inputlist'     => ['hum', 'mus', 'rod', 'mam', 'vrt', 'syn'],
-                'column_names' => ['tax_id'],
-            },
-            -flow_into => {
-              '2->A' => ['create_type_ids'],
-              'A->1' => ['concat_file'],
-            },
+        {
+          -logic_name => 'create_tax_ids',
+          -module     => 'Bio::EnsEMBL::Hive::RunnableDB::JobFactory',
+          -parameters => {
+            inputlist    => ['hum', 'vrt', 'mus', 'mam', 'rod', 'syn'],
+            column_names => ['tax_id'],
+          },
+          -flow_into => {
+            '2->A' => ['create_type_ids'],
+            'A->1' => ['concat_file'],
+          },
         },
 
-        {   -logic_name => 'create_type_ids',
-            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::JobFactory',
-            -parameters => {
-                'inputlist'     => ['htc', 'std'],
-                'column_names' => ['type'],
-            },
-            -analysis_capacity  => 6,
-            -flow_into => {
-              '2' => {'download_files' => {tax_id => '#tax_id#', type => '#type#'}},
-            },
+        {
+          -logic_name => 'create_type_ids',
+          -module     => 'Bio::EnsEMBL::Hive::RunnableDB::JobFactory',
+          -parameters => {
+            inputlist     => ['std', 'htc'],
+            column_names => ['type'],
+          },
+          -analysis_capacity  => 6,
+          -flow_into => {
+            2 => {'download_files' => {tax_id => '#tax_id#', type => '#type#'}},
+          },
         },
 
-        {   -logic_name => 'download_files',
-            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
-            -parameters => {
-                'cmd'     => 'wget -q -P '.$self->o('vertrna_dir').' "'.$self->o('base_ena_ftp').'/rel_#type#_#tax_id#_*_r'.$self->o('vertrna_version').'.dat.gz"',
-            },
-            -analysis_capacity  => 12,
-            -flow_into => {
-              '1' => ['create_file_ids'],
-            },
+        {
+          -logic_name => 'download_files',
+          -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
+          -parameters => {
+            cmd => 'wget -q -P '.$self->o('vertrna_dir').' "'.$self->o('base_ena_ftp').'/rel_#type#_#tax_id#_*_r'.$self->o('vertrna_version').'.dat.gz"',
+          },
+          -analysis_capacity  => 12,
+          -flow_into => {
+            1 => ['create_file_ids'],
+          },
         },
 
-        {   -logic_name => 'create_file_ids',
-            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::JobFactory',
-            -parameters => {
-                'inputcmd'     => 'ls '.$self->o('vertrna_dir').'/rel_#type#_#tax_id#_*_r'.$self->o('vertrna_version').".dat.gz | sed 's/.dat.gz//'",
-                'column_names' => ['input_file'],
-            },
-            -analysis_capacity  => 12,
-            -flow_into => {
-              '2' => ['gunzip'],
-            },
+        {
+          -logic_name => 'create_file_ids',
+          -module     => 'Bio::EnsEMBL::Hive::RunnableDB::JobFactory',
+          -parameters => {
+            inputcmd     => 'ls '.$self->o('vertrna_dir').'/rel_#type#_#tax_id#_*_r'.$self->o('vertrna_version').".dat.gz | sed 's/.dat.gz//'",
+            column_names => ['input_file'],
+          },
+          -analysis_capacity  => 12,
+          -flow_into => {
+            2 => ['gunzip'],
+          },
         },
 
-        {   -logic_name => 'gunzip',
-            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
-            -parameters => {
-                'cmd'         => 'gunzip #input_file#.dat.gz',
-            },
-            -analysis_capacity  => 12,
-            -flow_into => {
-                1 => [ 'embl2fasta' ],
-            },
+        {
+          -logic_name => 'gunzip',
+          -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
+          -parameters => {
+            cmd => 'gunzip #input_file#.dat.gz',
+          },
+          -analysis_capacity  => 12,
+          -flow_into => {
+            1 => [ 'embl2fasta' ],
+          },
         },
 
-        {   -logic_name => 'embl2fasta',
-            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
-            -parameters => {
-                'cmd'   => 'perl '.$self->o('embl2fasta_script').' -vertrna -embl #input_file#.dat -fasta #input_file#',
-            },
-            -analysis_capacity  => 30,
-            -flow_into => {
-                1 => ['delete_embl_file'],
-            },
+        {
+          -logic_name => 'embl2fasta',
+          -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
+          -parameters => {
+            cmd => 'perl '.$self->o('embl2fasta_script').' -vertrna -embl #input_file#.dat -fasta #input_file#',
+          },
+          -analysis_capacity  => 30,
+          -flow_into => {
+            1 => ['delete_embl_file'],
+          },
         },
 
-        {   -logic_name => 'delete_embl_file',
-            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
-            -parameters => {
-                'cmd'   => 'rm #input_file#.dat',
-            },
-            -analysis_capacity  => 12,
-            -flow_into => {
-                1 => ['?accu_name=input_file&accu_input_variable=input_file&accu_address=[]'],
-            },
+        {
+          -logic_name => 'delete_embl_file',
+          -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
+          -parameters => {
+            cmd => 'rm #input_file#.dat',
+          },
+          -analysis_capacity  => 12,
+          -flow_into => {
+            1 => ['?accu_name=input_file&accu_input_variable=input_file&accu_address=[]'],
+          },
         },
 
-        {   -logic_name => 'concat_file',
-            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
-            -parameters => {
-                'cmd'   => 'cat #expr(join(" ", @{#input_file#}))expr# > #vertrna_dir#/#vertrna_file#; rm #expr(join(" ", @{#input_file#}))expr#',
-            },
-            -flow_into => {
-              1 => ['check_against_previous'],
-            }
+        {
+          -logic_name => 'concat_file',
+          -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
+          -parameters => {
+            cmd => 'cat #expr(join(" ", @{#input_file#}))expr# > #vertrna_dir#/#vertrna_file#; rm #expr(join(" ", @{#input_file#}))expr#',
+          },
+          -flow_into => {
+            1 => ['check_against_previous'],
+          }
         },
 
-        {   -logic_name => 'check_against_previous',
-            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
-            -parameters => {
-                'cmd'   => 'VERSION='.$self->o('vertrna_version').';DIR="#vertrna_dir#";OLDDIR="${DIR%$VERSION}/$((VERSION-1))";if [ -d "$OLDDIR" ]; then OLD=`grep -c \> $OLDDIR/#vertrna_file#`;NEW=`grep -c \> #vertrna_dir#/#vertrna_file#`; if [ $NEW -lt $OLD ];then exit 2;fi;fi',
-            },
-            -flow_into => WHEN( '#blast_type# eq "ncbi"' => 'ncbi_format', ELSE 'wu_format',),
+        {
+          -logic_name => 'check_against_previous',
+          -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
+          -parameters => {
+            cmd => 'DIR="#vertrna_dir#";VERSION=`ls $DIR/.. | sort -nr | head -n 2 | tail -n1`;OLDDIR="${DIR}/../$VERSION";if [ -d "$OLDDIR" ]; then OLD=`grep -c \> $OLDDIR/#vertrna_file#`;NEW=`grep -c \> #vertrna_dir#/#vertrna_file#`; if [ $NEW -lt $OLD ];then exit 2;fi;fi',
+          },
+          -flow_into => WHEN( '#blast_type# eq "ncbi"' => 'ncbi_format', ELSE 'wu_format',),
         },
 
-        {   -logic_name => 'ncbi_format',
-            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
-            -parameters => {
-                'cmd'   => 'makeblastdb -dbtype nucl -in #vertrna_dir#/#vertrna_file# -title "EMBL VertRNA"',
-            },
-            -flow_into => {
-              1 => ['finalise_dbs'],
-            }
+        {
+          -logic_name => 'ncbi_format',
+          -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
+          -parameters => {
+            cmd => 'makeblastdb -dbtype nucl -in #vertrna_dir#/#vertrna_file# -title "EMBL VertRNA"',
+          },
+          -flow_into => {
+            1 => ['finalise_dbs'],
+          }
         },
 
-        {   -logic_name => 'wu_format',
-            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
-            -parameters => {
-                'cmd'   => 'xdformat -n #vertrna_dir#/#vertrna_file# -t "EMBL VertRNA"',
-            },
-            -flow_into => {
-              1 => ['finalise_dbs'],
-            }
+        {
+          -logic_name => 'wu_format',
+          -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
+          -parameters => {
+            cmd => 'xdformat -n #vertrna_dir#/#vertrna_file# -t "EMBL VertRNA"',
+          },
+          -flow_into => {
+            1 => ['finalise_dbs'],
+          }
         },
 
-
-        {   -logic_name => 'finalise_dbs',
-            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
-            -parameters => {
-                'cmd'         => 'chmod g-w #vertrna_dir#; find #vertrna_dir# -type f -execdir chmod g-w {} \;',
-            },
+        {
+          -logic_name => 'finalise_dbs',
+          -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
+          -parameters => {
+            cmd => 'chmod g-w #vertrna_dir#; find #vertrna_dir# -type f -execdir chmod g-w {} \;',
+          },
+          -flow_into => {
+            1 => ['propagate_the_word'],
+          }
         },
 
+        {
+          -logic_name => 'propagate_the_word',
+          -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SlackNotification',
+          -parameters => {
+            slack_webhook => $self->o('slack_hook_url'),
+            text => 'The VertRNA blast database has been updated to '.$self->o('vertrna_version'),
+          },
+        },
     );
 
     foreach my $analysis (@analyses) {
@@ -228,4 +254,3 @@ sub pipeline_analyses {
 }
 
 1;
-
