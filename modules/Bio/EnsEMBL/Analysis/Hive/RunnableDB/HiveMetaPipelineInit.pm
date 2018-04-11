5279def2df2938bf944492cd25bfb99a5acc06bb
Thibaut Hourlier <thibaut@ebi.ac.uk>
2018-04-11 17:26:20 +0100

    Module and configuration file to start a meta pipeline
    The configuration file is for collapsing a whole set of long read data

---
+++ modules/Bio/EnsEMBL/Analysis/Hive/RunnableDB/HiveMetaPipelineInit.pm
@@ -0,0 +1,129 @@
+=head1 LICENSE
+
+Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+Copyright [2016-2017] EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=head1 CONTACT
+
+Please email comments or questions to the public Ensembl
+developers list at <http://lists.ensembl.org/mailman/listinfo/dev>.
+
+Questions may also be sent to the Ensembl help desk at
+<http://www.ensembl.org/Help/Contact>.
+
+=head1 NAME
+
+Bio::EnsEMBL::Analysis::Hive::RunnableDB::HiveMetaPipelineInit
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+
+=cut
+
+package Bio::EnsEMBL::Analysis::Hive::RunnableDB::HiveMetaPipelineInit;
+
+use strict;
+use warnings;
+
+use File::Spec::Functions qw(catfile);
+use parent ('Bio::EnsEMBL::Hive::RunnableDB::SystemCmd');
+
+
+=head2 param_defaults
+
+ Arg [1]    : None
+ Description: Set 'hive_init_script' to use 'enscode_root_dir'
+               hive_init_script => catfile('#enscode_root_dir#', 'ensembl-hive', 'scripts', 'init_pipeline.pl'),
+ Returntype : Hashref
+ Exceptions : None
+
+=cut
+
+sub param_defaults {
+  my ($self) = @_;
+
+  return {
+    %{$self->SUPER::param_defaults},
+    hive_init_script => catfile('#enscode_root_dir#', 'ensembl-hive', 'scripts', 'init_pipeline.pl'),
+  }
+}
+
+
+=head2 fetch_input
+
+ Arg [1]    : None
+ Description: Create the command for initialising the sub pipeline. You need to provide
+              'ehive_url', 'meta_pipeline_db' and 'hive_config'. If you want to specify
+              databases you need to add the name of the database(s) in 'databases'. The
+              parameters will be created by using "${db_name}_name" where $db_name is the
+              name of the databases in 'hive_config'. You should also have paramters in
+              the config like "${db_name}_name", "${db_name}_port", "${db_name}_server",
+              "${db_name}_user", "${db_name}_password".
+ Returntype : None
+ Exceptions : Throws if 'ehive_url', 'pipeline_name', 'meta_pipeline_db' or 'hive_config' is not set
+              Throws if 'hive_init_script' does not exists
+
+=cut
+
+sub fetch_input {
+  my $self = shift;
+
+  my $url = $self->param_required('ehive_url');
+  my $pipeline_db = $self->param_required('meta_pipeline_db');
+  $self->throw('"hive_init_script" does not exist: '.$self->param('hive_init_script'))
+    unless (-e $self->param('hive_init_script'));
+  my $dna_db;
+  if ($self->param_is_defined('dna_db')) {
+    $dna_db = $self->param_required('dna_db');
+  }
+  else {
+    $self->warning('No "dna_db" was defined');
+  }
+  my @cmd;
+  push(@cmd, 'perl', $self->param('hive_init_script'), $self->param_required('hive_config'),
+    '-host', $pipeline_db->{'-host'},
+    '-port', $pipeline_db->{'-port'},
+    '-user', $pipeline_db->{'-user'},
+    '-password', $pipeline_db->{'-pass'},
+    '-pipe_dbname', $pipeline_db->{'-dbname'},
+    '-pipeline_name', $self->param_required('pipeline_name'));
+  push(@cmd, '-enscode_root_dir', $self->param('enscode_root_dir'))
+    if ($self->param_is_defined('enscode_root_dir'));
+  if ($dna_db) {
+    push(@cmd, '-dna_db_name', $dna_db->{'-dbname'});
+    push(@cmd, '-dna_db_host', $dna_db->{'-host'});
+    push(@cmd, '-dna_db_port', $dna_db->{'-port'});
+    push(@cmd, '-dna_db_user', $dna_db->{'-user'}) if (exists $dna_db->{'-user'});
+    push(@cmd, '-dna_db_pass', $dna_db->{'-pass'}) if (exists $dna_db->{'-pass'} and $dna_db->{'-pass'});
+  }
+  foreach my $db_title (@{$self->param('databases')}) {
+    my $db = $self->param($db_title);
+    push(@cmd, '-'.$db_title.'_name', $db->{'-dbname'});
+    push(@cmd, '-'.$db_title.'_host', $db->{'-host'});
+    push(@cmd, '-'.$db_title.'_port', $db->{'-port'});
+    push(@cmd, '-'.$db_title.'_user', $db->{'-user'}) if (exists $db->{'-user'});
+    push(@cmd, '-'.$db_title.'_pass', $db->{'-pass'}) if (exists $db->{'-pass'} and $db->{'-pass'});
+  }
+  if ($self->param_is_defined('commandline_params')) {
+    push(@cmd, $self->param('commandline_params'));
+  }
+  $self->param('cmd', join(' ', @cmd));
+}
+
+
+1;
---
+++ modules/Bio/EnsEMBL/Analysis/Hive/RunnableDB/HiveMetaPipelineRun.pm
@@ -0,0 +1,97 @@
+=head1 LICENSE
+
+Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+Copyright [2016-2017] EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=head1 CONTACT
+
+Please email comments or questions to the public Ensembl
+developers list at <http://lists.ensembl.org/mailman/listinfo/dev>.
+
+Questions may also be sent to the Ensembl help desk at
+<http://www.ensembl.org/Help/Contact>.
+
+=head1 NAME
+
+Bio::EnsEMBL::Analysis::Hive::RunnableDB::HiveMetaPipelineRun
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+
+=cut
+
+package Bio::EnsEMBL::Analysis::Hive::RunnableDB::HiveMetaPipelineRun;
+
+use strict;
+use warnings;
+use Bio::EnsEMBL::Analysis::Tools::Utilities qw(execute_with_wait);
+
+use parent ('Bio::EnsEMBL::Analysis::Hive::RunnableDB::HiveBaseRunnableDB');
+
+
+sub param_defaults {
+  my ($self) = @_;
+
+  return {
+    %{$self->SUPER::param_defaults},
+    action => 'loop', # this can be loop, run, 
+    _input_id_name => 'target_db',
+    perl_path => 'perl',
+  }
+}
+
+sub fetch_input {
+  my ($self) = @_;
+
+  my $url = $self->param_required('ehive_url');
+  my $base_cmd = $self->param_required('beekeeper_script').' -url '.$url;
+  if (-e $self->param('beekeeper_script')) {
+    $base_cmd = $self->param('perl_path').' '.$base_cmd;
+  }
+  execute_with_wait($base_cmd.' -sync');
+  $self->param('base_cmd', $base_cmd);
+}
+
+sub run {
+  my ($self) = @_;
+
+  my $cmd = $self->param('base_cmd').' -'.$self->param('action');
+  $cmd .= ' '.$self->param('commandline_params')
+    if ($self->param_is_defined('commandline_params') and $self->param('commandline_params'));
+  execute_with_wait($cmd);
+  execute_with_wait($self->param('base_cmd').' -sync');
+  execute_with_wait($cmd);
+}
+
+
+sub write_output {
+  my ($self) = @_;
+
+  $self->dataflow_output_id({$self->param('_input_id_name') => $self->param($self->param('_input_id_name'))}, $self->param('_branch_to_flow_to'));
+}
+
+sub pre_cleanup {
+  my ($self) = @_;
+
+# I need to execute fetch_input to set base_cmd and it's also doing some checks
+  $self->fetch_input;
+  execute_with_wait($self->param('base_cmd').' -unkwn');
+  execute_with_wait($self->param('base_cmd').' -reset_failed_jobs');
+}
+
+1;
