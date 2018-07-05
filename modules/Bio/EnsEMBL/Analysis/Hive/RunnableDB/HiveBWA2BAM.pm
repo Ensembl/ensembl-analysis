303552819648cdd707461b93cb6b5fce721775e4
Thibaut Hourlier <thibaut@ebi.ac.uk>
2018-07-05 11:22:11 +0100

    Complete early and no dataflow if there is no file to process

--- modules/Bio/EnsEMBL/Analysis/Hive/RunnableDB/HiveBWA2BAM.pm
+++ modules/Bio/EnsEMBL/Analysis/Hive/RunnableDB/HiveBWA2BAM.pm
@@ -63,42 +63,49 @@ use parent ('Bio::EnsEMBL::Analysis::Hive::RunnableDB::HiveBaseRunnableDB');
 
 sub fetch_input {
   my ($self) = @_;
-  my $program = $self->param('wide_short_read_aligner');
-  $self->throw("BWA program not defined in analysis\n") unless (defined $program);
-  my $fastqfile;
-  my $fastqpair;
 
-  my $method = $self->param('is_paired') ? ' sampe '.$self->param('sampe_options') : ' samse '.$self->param('samse_options');
-  foreach my $fastq (@{$self->param('fastq')}) {
-      my $abs_filename = $self->param('wide_input_dir').'/'.$fastq->{filename};
-      $self->throw("Fastq file $abs_filename not found\n") unless (-e $abs_filename);
-      if ($fastq->{is_mate_1} == 1) {
-          $fastqfile = $abs_filename;
-      }
-      else {
-          $fastqpair = $abs_filename;
-      }
-  }
-  my $analysis = $self->create_analysis;
-  $analysis->parameters('-use_threads => '.$self->param('use_threads')) if ($self->param_is_defined('use_threads'));
-  my $runnable = Bio::EnsEMBL::Analysis::Runnable::BWA2BAM->new
-    (
-     -analysis   => $analysis,
-     -program    => $program,
-     -fastq      => $fastqfile,
-     -fastqpair  => $fastqpair,
-     -options    => $method,
-     -outdir     => $self->param('wide_output_dir'),
-     -genome     => $self->param('wide_genome_file'),
-     -samtools => $self->param('wide_samtools'),
-     -header => $self->param('header_file'),
-     -min_mapped => $self->param('min_mapped'),
-     -min_paired => $self->param('min_paired'),
-    );
-    if ($self->param_is_defined('bam_prefix')) {
-        $runnable->bam_prefix($self->param($self->param('bam_prefix')));
+  if ($self->param_is_defined('fastq')) {
+    my $program = $self->param('wide_short_read_aligner');
+    $self->throw("BWA program not defined in analysis\n") unless (defined $program);
+    my $fastqfile;
+    my $fastqpair;
+
+    my $method = $self->param('is_paired') ? ' sampe '.$self->param('sampe_options') : ' samse '.$self->param('samse_options');
+    foreach my $fastq (@{$self->param('fastq')}) {
+        my $abs_filename = catfile($self->param('wide_input_dir'), $fastq->{filename});
+        $self->throw("Fastq file $abs_filename not found\n") unless (-e $abs_filename);
+        if ($fastq->{is_mate_1} == 1) {
+            $fastqfile = $abs_filename;
+        }
+        else {
+            $fastqpair = $abs_filename;
+        }
     }
-  $self->runnable($runnable);
+    my $analysis = $self->create_analysis;
+    $analysis->parameters('-use_threads => '.$self->param('use_threads')) if ($self->param_is_defined('use_threads'));
+    my $runnable = Bio::EnsEMBL::Analysis::Runnable::BWA2BAM->new
+      (
+       -analysis   => $analysis,
+       -program    => $program,
+       -fastq      => $fastqfile,
+       -fastqpair  => $fastqpair,
+       -options    => $method,
+       -outdir     => $self->param('wide_output_dir'),
+       -genome     => $self->param('wide_genome_file'),
+       -samtools => $self->param('wide_samtools'),
+       -header => $self->param('header_file'),
+       -min_mapped => $self->param('min_mapped'),
+       -min_paired => $self->param('min_paired'),
+      );
+      if ($self->param_is_defined('bam_prefix')) {
+          $runnable->bam_prefix($self->param($self->param('bam_prefix')));
+      }
+    $self->runnable($runnable);
+  }
+  else {
+    $self->input_job->autoflow(0);
+    $self->complete_early('There is no fastq files for this job');
+  }
 }
 
 
