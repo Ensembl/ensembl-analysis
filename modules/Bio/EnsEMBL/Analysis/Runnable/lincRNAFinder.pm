01fb7578c48a4b2bc3b81b6c0648f95168308127
Thibaut Hourlier <thibaut@ebi.ac.uk>
2019-02-07 11:51:41 +0000

    These changes might need to be checked again

--- modules/Bio/EnsEMBL/Analysis/Runnable/lincRNAFinder.pm
+++ modules/Bio/EnsEMBL/Analysis/Runnable/lincRNAFinder.pm
@@ -47,7 +47,7 @@ package Bio::EnsEMBL::Analysis::Runnable::lincRNAFinder;
 use strict;  
 use warnings;
 
-use Bio::EnsEMBL::Analysis::Tools::Algorithms::ClusterUtils; 
+use Bio::EnsEMBL::Analysis::Tools::Algorithms::ClusterUtils qw(cluster_Genes make_types_hash get_oneway_clustering_genes_of_set get_twoway_clustering_genes_of_set); 
 use Bio::EnsEMBL::Analysis::Tools::GeneBuildUtils::GeneUtils qw(compute_6frame_translations Gene_info) ; 
 
 use parent ('Bio::EnsEMBL::Analysis::Runnable');
@@ -94,138 +94,103 @@ sub run{
   print scalar(@unclustered_cdna_genes) . " genes including single exons genes\n";
 
   print "\nStage 2) check for 6-frame translations in lincRNA candidates\n";
-  print "result_set: " . scalar(@{$self->result_set}) . " and unclustered set: " . scalar(@unclustered_cdna_genes) . " for 3 step \n";
+#  print "result_set: " . scalar(@{$self->result_set}) . " and unclustered set: " . scalar(@unclustered_cdna_genes) . " for 3 step \n";
 
-  my @genes_with_translations ;  
-  my @genes_withOUT_translations ; 
-  RG: for my $rg( @unclustered_cdna_genes ) {
+  my @lincrna_candidate_genes;  
+  my $gene_with_translation_count = 0;
+  my $gene_without_translation_count = 0;
+  my $short_gene_count = 0;
+  my $long_gene_count = 0;
+  foreach my $rg( @unclustered_cdna_genes ) {
  	# WARNING: when stand specific models this should be only against 3 frames of correct strand... 
     my $new_gene = compute_6frame_translations($rg);  # compute_translation() # 
     $new_gene->biotype("pre_finder_round1");  
     # print " translations found for gene " . Gene_info($rg) . "::" . $rg->display_id() . "\n"; 
     print scalar(@{ $rg->get_all_Transcripts} ) ." translations found for old gene " . Gene_info($rg) . "::" . $rg->display_id() . "\n";  # " seq_region: " . $rg->seq_region_name . " start: " . $rg->seq_region_start . " end: " . $rg->seq_region_end . " strand: " . $rg->seq_region_strand . " \n" ;
 
-    if (!defined $new_gene->get_all_Transcripts) {
-      $self->warning('  Could not compute translation for cDNA: gene dbID '. $rg->dbID . ' ' . $rg->seq_region_name . ' ' .
-             $rg->seq_region_start . ' ' . $rg->seq_region_end.' '.$rg->length);
-      next RG;
-      $rg->biotype('gene_WITHOUT_translation');
-      push @genes_withOUT_translations, $rg ; 
-    }     
-    print scalar(@{ $new_gene->get_all_Transcripts} ) ." translations found for new gene " . Gene_info($new_gene) . "::" . $new_gene->display_id() . "\n";  # " seq_region: " . $rg->seq_region_name . " start: " . $rg->seq_region_start . " end: " . $rg->seq_region_end . " strand: " . $rg->seq_region_strand . " \n";
+    if ($new_gene->get_all_Transcripts) {
+      ++$gene_with_translation_count;
+      print scalar(@{ $new_gene->get_all_Transcripts} ) ." translations found for new gene " . Gene_info($new_gene) . "::" . $new_gene->display_id() . "\n";  # " seq_region: " . $rg->seq_region_name . " start: " . $rg->seq_region_start . " end: " . $rg->seq_region_end . " strand: " . $rg->seq_region_strand . " \n";
 
-    push @genes_with_translations, $new_gene ; 
-  }
-  print "--  ".scalar(@genes_with_translations) . " genes with 6-frame-translations found\n" ;     
-  print "--  ".scalar(@genes_withOUT_translations) . " genes WITHOUT 6-frame-translations found\n" ;  # I migth need to report them too!!! 
-
-  # cap the number of transcripts per gene according to config 
-  my $capped_genes = $self->cap_number_of_translations_per_gene(\@genes_with_translations) ;  
-  print "--  ".scalar(@$capped_genes) . " after genes with 6-frame-translations found\n" ;     
- 
-  my ($short, $long ) = $self->filter_genes_with_long_translations($capped_genes); 
-  $capped_genes = ""; # WARNING: I am keeping too many @ and objects. This is a test!! Probably I can delete this... 
-  print "--  ".scalar(@$long)." genes have long translations, discarded from lincRNA pipeline.\n";
-  print "--  ".scalar(@$short)." genes with short translations found. They will be our lincRNAFinder final output.\n\n";
-
-  # $self->update_and_copy_cdna($long, 'has_long_translation');   # cDNA DEBUG
-  $self->output($short); 
-}
-
-
-sub print_output {
-	my ( $self) = @_ ; 
-}
-
-sub cap_number_of_translations_per_gene { 
-  my ( $self, $genes_with_6f_translations ) = @_ ;   
-  my @capped_longest_genes;
-  GENES: for my $g ( @$genes_with_6f_translations ) {
-    my $transcripts = $g->get_all_Transcripts;
-    if (defined $self->max_translations_stored_per_gene and @$transcripts > $self->max_translations_stored_per_gene) {
-      $g->flush_Transcripts;
-      my @sorted_t = sort {$a->translate->length <=> $a->translate->length} @$transcripts;
-      for my $lt (splice @sorted_t, 0, $self->max_translations_stored_per_gene) {
-        $g->add_Transcript($lt);
+      $self->cap_number_of_translations_per_gene($new_gene);
+      if ($self->has_genes_with_long_translations($new_gene)) {
+        ++$long_gene_count;
+      }
+      else {
+        ++$short_gene_count;
+        push(@lincrna_candidate_genes, $new_gene);
       }
     }
-    push(@capped_longest_genes, $g);
-  }   
-  return \@capped_longest_genes; 
-} 
+    else {
+      ++$gene_without_translation_count;
+      $self->warning('  Could not compute translation for cDNA: gene dbID '. $rg->dbID . ' ' . $rg->seq_region_name . ' ' .
+             $rg->seq_region_start . ' ' . $rg->seq_region_end.' '.$rg->length);
+      next;
+    }
+  }
+  print "--  $gene_with_translation_count genes with 6-frame-translations found\n" ;     
+  print "--  $gene_with_translation_count genes WITHOUT 6-frame-translations found\n" ;  # I migth need to report them too!!! 
+
+  # cap the number of transcripts per gene according to config 
+ 
+  print "--  $long_gene_count genes have long translations, discarded from lincRNA pipeline.\n";
+  print "--  $short_gene_count genes with short translations found. They will be our lincRNAFinder final output.\n\n";
+
+  # $self->update_and_copy_cdna($long, 'has_long_translation');   # cDNA DEBUG
+  $self->output(\@lincrna_candidate_genes); 
+}
 
 
-sub filter_genes_with_long_translations {  
-  my ( $self, $capped_longest_genes ) = @_ ; 
+
+sub cap_number_of_translations_per_gene {
+  my ($self, $gene) = @_;
+
+  my $transcripts = $gene->get_all_Transcripts;
+  if (defined $self->max_translations_stored_per_gene and @$transcripts > $self->max_translations_stored_per_gene) {
+    $gene->flush_Transcripts;
+    my @sorted_t = sort {$a->translate->length <=> $a->translate->length} @$transcripts;
+    foreach my $lt (splice @sorted_t, 0, $self->max_translations_stored_per_gene) {
+      $gene->add_Transcript($lt);
+    }
+  }
+  return $gene;
+}
+
+
+sub has_genes_with_long_translations {  
+  my ( $self, $gene ) = @_ ; 
 
   my $max_trans_length_ratio;
-   if ( defined $self->maximum_translation_length_ratio ){  
-     if ( $self->maximum_translation_length_ratio < 101 ) {  
-       $max_trans_length_ratio =  $self->maximum_translation_length_ratio; 
-       print "-- your MAXIMUM_TRANSLATION_LENGTH_RATIO is: " . $max_trans_length_ratio . "\n";
-     } else {  
-       $self->throw("translation-length-to-transcript length ratio > 100 does not make sense.\n"); 
-     } 
-   }else { 
-     $max_trans_length_ratio = 100 ; 
-   }   
+  if ( defined $self->maximum_translation_length_ratio ){  
+    if ( $self->maximum_translation_length_ratio < 101 ) {  
+      $max_trans_length_ratio =  $self->maximum_translation_length_ratio; 
+      print "-- your MAXIMUM_TRANSLATION_LENGTH_RATIO is: " . $max_trans_length_ratio . "\n";
+    } else {  
+      $self->throw("translation-length-to-transcript length ratio > 100 does not make sense.\n"); 
+    } 
+  }else { 
+    $max_trans_length_ratio = 100 ; 
+  }   
 
-  # if a gene has a transcript with a translation over a specified % threshold transcript
-  # length, the gene will not be taken as lincRNA candidate.
-  
-  # if WRITE_DEBUG_OPTION is turned on, the cDNA "genes" with long translations could 
-  # be written to the DEBUG_OUTPUT_DB with new biotype "long translation".
-  
-  my (@long_genes, @short_genes );  
+# if a gene has a transcript with a translation over a specified % threshold transcript
+# length, the gene will not be taken as lincRNA candidate.
 
-  GENES: for my $g ( @$capped_longest_genes ) { 
-    for my $t ( @{$g->get_all_Transcripts } ) {   
+# if WRITE_DEBUG_OPTION is turned on, the cDNA "genes" with long translations could 
+# be written to the DEBUG_OUTPUT_DB with new biotype "long translation".
 
-       my $tl_length = $t->translate->length * 3 ; 
-       my $tr_length = $t->length ;    
-       my $ratio = sprintf('%.1f',$tl_length*100 /  $tr_length);
-       if ( $ratio > $max_trans_length_ratio ) { 
-         # The cDNA "genes" have no usable display IDs, hence not printing any in the next line:
-         print "LONG_TRANSLATION: " .  Gene_info($g)   .  "  translation-length to transcript-length ratio ($ratio) higher than max. value allowed in config ($max_trans_length_ratio).\n";
-         my $analysis = Bio::EnsEMBL::Analysis->new(
-                                       -logic_name => 'long_translation_cDNA' ,
-                                       );
-         $g->analysis($analysis);
-         $g->biotype('long_translation_cDNA');
-         push @long_genes, $g; 
-         next GENES; 
-       } 
-    }     
-    push @short_genes , $g ;
+  foreach my $t (@{$gene->get_all_Transcripts }) {
+    my $ratio = (($t->translate->length*3)*100)/$t->length;
+    if ( $ratio > $max_trans_length_ratio ) {
+# The cDNA "genes" have no usable display IDs, hence not printing any in the next line:
+      print 'LONG_TRANSLATION: '.Gene_info($gene).'  translation-length to transcript-length ratio ('.sprintf('%.1f', $ratio).") higher than max. value allowed in config ($max_trans_length_ratio).\n";
+      $gene->biotype('long_translation_cDNA');
+      return 1;
+    }
   }
-  return ( \@short_genes, \@long_genes );  
+  return 0;
 } 
 
 
-=head2 result_set
-
-  Arg [1]   : Bio::EnsEMBL::Analysis::RunnableDB
-  Arg [2]   : arrayref of result_set
-  Function  : push array passed into onto result_set array
-  Returntype: arrayref
-  Exceptions: throws if not passed correct type
-  Example   : $self->result_set(\@output);
-
-=cut
-
-sub result_set{
-  my ($self, $result_set) = @_;
-  if(!$self->{'result_set'}){
-    $self->{'result_set'} = [];
-  }
-  if($result_set){
-    if(ref($result_set) ne 'ARRAY'){
-      $self->throw('Must pass RunnableDB:result_set an array ref not a '.$result_set);
-    }
-    push(@{$self->{'result_set'}}, @$result_set);
-  }
-  return $self->{'result_set'};
-}
 
 sub updated_cdnas {  
   my ( $self,$arg ) = @_ ;  
@@ -269,17 +234,58 @@ sub filter_cdna_genes_by_exon_count {
   return ( \@single_exon, \@multi_exon );
 } 
 
-# it is not nice, but it is working 
-use vars '$AUTOLOAD';
-sub AUTOLOAD {
- my ($self,$val) = @_;
- (my $routine_name=$AUTOLOAD)=~s/.*:://; #trim package name
- if ( defined $val ) { 
-   $self->{$routine_name}=$val;
-   print "routine name for AUTOLOAD:: $routine_name \n"; 
+sub maximum_translation_length_ratio {
+  my ($self, $value) = @_;
+
+  if (defined $value) {
+    $self->{maximum_translation_length_ratio} = $value;
+  }
+  return $self->{maximum_translation_length_ratio};
 }
-return $self->{$routine_name} ;
+
+sub find_single_exon_candidates {
+  my ($self, $value) = @_;
+
+  if (defined $value) {
+    $self->{find_single_exon_candidates} = $value;
+  }
+  return $self->{find_single_exon_candidates};
 }
-sub DESTROY {} # required due to AUTOLOAD
+
+sub max_translations_stored_per_gene {
+  my ($self, $value) = @_;
+
+  if (defined $value) {
+    $self->{max_translations_stored_per_gene} = $value;
+  }
+  return $self->{max_translations_stored_per_gene};
+}
+
+sub set_1_cdna_genes {
+  my ($self, $value) = @_;
+
+  if (defined $value) {
+    $self->{set_1_cdna_genes} = $value;
+  }
+  return $self->{set_1_cdna_genes};
+}
+
+sub set_2_prot_genes {
+  my ($self, $value) = @_;
+
+  if (defined $value) {
+    $self->{set_2_prot_genes} = $value;
+  }
+  return $self->{set_2_prot_genes};
+}
+
+sub ignore_strand {
+  my ($self, $value) = @_;
+
+  if (defined $value) {
+    $self->{ignore_strand} = $value;
+  }
+  return $self->{ignore_strand};
+}
 
 1;  
