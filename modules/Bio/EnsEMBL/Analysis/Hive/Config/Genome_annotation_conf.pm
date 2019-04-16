e930ab39061edae94fa56c0941a27524c52656e8
fergalmartin <fergal@ebi.ac.uk>
2019-04-16 11:24:21 +0100

    Change to Genome_annotation_conf.pm Added a missing key for updating the assembly name if it changes mid production. Still missing the script it should actually point to, but this will be updated soon

--- modules/Bio/EnsEMBL/Analysis/Hive/Config/Genome_annotation_conf.pm
+++ modules/Bio/EnsEMBL/Analysis/Hive/Config/Genome_annotation_conf.pm
@@ -327,6 +327,7 @@ sub default_options {
     meta_levels_script         => catfile($self->o('ensembl_misc_script'), 'meta_levels.pl'),
     frameshift_attrib_script   => catfile($self->o('ensembl_misc_script'), 'frameshift_transcript_attribs.pl'),
     select_canonical_script    => catfile($self->o('ensembl_misc_script'),'canonical_transcripts', 'select_canonical_transcripts.pl'),
+    assembly_name_script       => '',
 
     rnaseq_daf_introns_file => catfile($self->o('output_dir'), 'rnaseq_daf_introns.dat'),
 
@@ -6860,7 +6861,7 @@ sub pipeline_analyses {
                         '1->A' => ['fan_otherfeatures_db', 'fan_rnaseq_db'],
                         'A->1' => ['update_assembly_name'],},
        },
-        
+
 
       {
         -logic_name => 'fan_otherfeatures_db',
@@ -7508,8 +7509,9 @@ sub pipeline_analyses {
 
         -rc_name    => '4GB',
       },
-     
-      { 
+
+
+      {
         -logic_name => 'update_assembly_name',
         -module => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
         -parameters => { 
