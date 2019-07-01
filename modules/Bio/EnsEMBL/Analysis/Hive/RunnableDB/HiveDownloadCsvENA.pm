5e0cd22031b6bd1511ec1395cd4836ba41665e2e
fergalmartin <fergal@ebi.ac.uk>
2019-07-01 15:01:11 +0100

    Change to HiveDownloadCsvENA.pm Added in code to look for different data types. Default behaviour continues to be short read, but there is now an isoseq option and this can easily be extended for other long read data

--- modules/Bio/EnsEMBL/Analysis/Hive/RunnableDB/HiveDownloadCsvENA.pm
+++ modules/Bio/EnsEMBL/Analysis/Hive/RunnableDB/HiveDownloadCsvENA.pm
@@ -82,7 +82,9 @@ sub param_defaults {
     _read_length => 1,
     _centre_name => 'ENA',
     print_all_info => 0,
-    paired_end_only => 1, #by default, module will only add paired-end data to the csv, add "paired_end_only => 0" to pipeline config to include single end data 
+    paired_end_only => 1, #by default, module will only add paired-end data to the csv, add "paired_end_only => 0" to pipeline config to include single end data
+    read_type => 'short_read',
+    instrument_platform => 'ILLUMINA',
   }
 }
 
@@ -101,13 +103,17 @@ sub param_defaults {
 sub fetch_input {
   my ($self) = @_;
 
+  if($self->param_required('read_type') eq 'isoseq') {
+    $self->param('paired_end_only',0);
+    $self->param('instrument_platform','PACBIO_SMRT'),
+  }
   $self->param_required('inputfile');
   if (-e $self->param('inputfile')) {
     $self->complete_early("'inputfile' exists so I will use that");
   } elsif ($self->param_is_defined('study_accession') and $self->param('study_accession')) {
     $self->_populate_query($self->param('study_accession'), 'study_accession=%s');
   } elsif ($self->param_is_defined('taxon_id') and $self->param('taxon_id')) {
-    $self->_populate_query($self->param('taxon_id'), 'tax_tree(%s) AND instrument_platform=ILLUMINA AND library_source=TRANSCRIPTOMIC');
+    $self->_populate_query($self->param('taxon_id'), 'tax_tree(%s) AND instrument_platform='.$self->param('instrument_platform').' AND library_source=TRANSCRIPTOMIC');
   } else {
     $self->throw('"inputfile" does not exist and neither "study_accession" nor "taxon_id" were defined');
   }
@@ -206,8 +212,7 @@ sub run {
             if ($row[$fields_index{library_layout}] eq 'PAIRED') {
               $read_length /= 2;
             }
-	    next if ($read_length < 75);
-
+            next if ($read_length < 75);
             my %line = (
               run_accession => $row[$fields_index{run_accession}],
               instrument_model => $row[$fields_index{instrument_model}],
@@ -430,20 +435,30 @@ sub write_output {
             }
           }
           $description =~ tr/:\t/ /;
-          print FH sprintf("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s, %s, %s\n",
-            lc($sample_name),
-            $experiment->{run_accession},
-            $experiment->{library_layout} eq 'PAIRED' ? 1 : 0,
-            $filename,
-            -1,
-            $experiment->{read_length},
-            0,
-            $experiment->{center_name} || $self->param('_centre_name'),
-            $experiment->{instrument_platform},
-            $study_accession,
-            $sample,
-            $description,
-          );
+          if($self->param('read_type') eq 'short_read') {
+            print FH sprintf("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s, %s, %s\n",
+              lc($sample_name),
+              $experiment->{run_accession},
+              $experiment->{library_layout} eq 'PAIRED' ? 1 : 0,
+              $filename,
+              -1,
+              $experiment->{read_length},
+              0,
+              $experiment->{center_name} || $self->param('_centre_name'),
+              $experiment->{instrument_platform},
+              $study_accession,
+              $sample,
+              $description,
+            );
+	  } elsif($self->param('read_type') eq 'isoseq') {
+            print FH sprintf("%s\t%s\t%s\n",
+              lc($sample_name),
+              $filename,
+              $description,
+            );
+	  } else {
+            $self->throw('Read type unknown: '.$self->param('read_type'));
+	  }
           push(@output_ids, {url => $file, download_method => $download_method, checksum => $checksums[$index++]});
         }
       }
