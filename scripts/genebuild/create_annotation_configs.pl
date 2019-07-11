c8516eaeef2b648e8527b1b550ccd74cabf90bda
fergalmartin <fergal@ebi.ac.uk>
2019-07-11 12:29:33 +0100

    Change to create_annotation_configs.pl Updated to put the lastZ database registry in the example command line in ini.cmds as leaving this out of the commandline will mean lastZ fails at various points

--- scripts/genebuild/create_annotation_configs.pl
+++ scripts/genebuild/create_annotation_configs.pl
@@ -652,6 +652,8 @@ sub custom_load_data {
 sub init_pipeline {
     my ($assembly_hash,$hive_directory,$force_init,$fh) = @_;
 
+    my $reg_conf_path = catfile($assembly_hash->{'output_path'},$assembly_hash->{'accession'},"Databases.pm");
+
     chdir($assembly_hash->{'output_path'});
     my $cmd;
     if ($hive_directory) {
@@ -693,7 +695,7 @@ sub init_pipeline {
 
 
     my $loop_command = $sync_command;
-    $loop_command =~ s/sync/loop \-sleep 0.3/;
+    $loop_command =~ s/sync/loop \-reg_conf $reg_conf_path \-sleep 0.3/;
     my ($ehive_url) = $sync_command =~ /url\s+(\S+)/;
     my ($driver, $user, $password, $host, $port, $dbname) = $ehive_url =~ /^(\w+)[:\/]+(\w*):(\w+)@([^:]+):(\d+)\/(\w+)$/;
     if ($password) {
