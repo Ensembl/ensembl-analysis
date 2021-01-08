fe0411612f6eb8f4cef5d252c4d15695be8e30b7
ens-carlos <ens-carlos@users.noreply.github.com>
2021-01-08 14:36:55 +0000

    Removed duplicated/initialised 'projection_source_db_name'.

--- modules/Bio/EnsEMBL/Analysis/Hive/Config/Genome_annotation_conf.pm
+++ modules/Bio/EnsEMBL/Analysis/Hive/Config/Genome_annotation_conf.pm
@@ -137,7 +137,6 @@ sub default_options {
 
     'red_logic_name'            => 'repeatdetector', # logic name for the Red repeat finding analysis
 
-    'projection_source_db_name'    => 'homo_sapiens_core_100_38', # This is generally a pre-existing db, like the current human/mouse core for example
     'projection_source_db_name'    => '', # This is generally a pre-existing db, like the current human/mouse core for example
     'projection_source_db_server'  => 'mysql-ens-mirror-1',
     'projection_source_db_port'    => '4240',
