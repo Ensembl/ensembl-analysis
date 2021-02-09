0498d0f5d432c6aaa43d07e2799344f02e830bef
Denye Ogeh <do1@ebi.ac.uk>
2021-02-09 15:02:12 +0000

    Modified how rfam library is passed to both run_cmsearch and extract_rfam analysis

--- scripts/genebuild/sncrna/filter_cm.pl
+++ scripts/genebuild/sncrna/filter_cm.pl
@@ -44,11 +44,11 @@ sub filter_rfam {
 
 my $rfam_cm_file       = $ARGV[0]; #$self->param_required('rfam_cm_file');
 my $rfam_accessions   = $ARGV[1]; #$self->param_required('rfam_accessions');
-my $working_dir        = $ARGV[2]; #$self->param_required("working_dir");
+#my $working_dir        = $ARGV[2]; #$self->param_required("working_dir");
 
 my $cm_path = path($rfam_cm_file);
 my $ra_path = path($rfam_accessions);
-my $output = path($working_dir . "/Rfam.cm");
+my $output = $ARGV[2];#path($working_dir . "/Rfam.cm");
 
 my $cm = $cm_path->slurp;
 my $ra = $ra_path->slurp;
