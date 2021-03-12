a7885bb8b990ffdab2a721dd5fa9537414193d5b
ens-carlos <carlos@ebi.ac.uk>
2021-03-12 14:03:56 +0000

    Removed use of HiveGeneric_conf.

--- modules/Bio/EnsEMBL/Analysis/Hive/Config/StarScallopRnaseq.pm
+++ modules/Bio/EnsEMBL/Analysis/Hive/Config/StarScallopRnaseq.pm
@@ -26,7 +26,6 @@ use File::Spec::Functions;
 
 use Bio::EnsEMBL::ApiVersion qw/software_version/;
 use Bio::EnsEMBL::Analysis::Tools::Utilities qw(get_analysis_settings);
-use Bio::EnsEMBL::Hive::PipeConfig::HiveGeneric_conf;
 use base ('Bio::EnsEMBL::Analysis::Hive::Config::HiveBaseConfig_conf');
 
 sub default_options {
