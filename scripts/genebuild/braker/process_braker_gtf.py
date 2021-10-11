01ed521eb88c7c621c3f5f85c8b9fe86b2a6ce96
ens-ftricomi <ftricomi@codon-login-03.ebi.ac.uk>
2021-10-11 16:58:12 +0100

    gtf protein mode

--- scripts/genebuild/braker/process_braker_gtf.py
+++ scripts/genebuild/braker/process_braker_gtf.py
@@ -30,16 +30,16 @@ def analyse_genmark(df):
     ]
     
     gene_list = set(df_genmark["gene_id"])
-    df_genmark_final = pd.DataFrame(df.columns)
+    df_genmark_final = pd.DataFrame()
     for i in gene_list:
 
         df_subset = df_genmark[df_genmark["gene_id"] == str(i)]
-        start = min(df_subset["start"])
-        stop = max(df_subset["end"])
-        index = min(df_subset.index)
-        print (index)
-        line = pd.DataFrame(
-            {
+        if len(df_subset)>1:
+            start = min(df_subset["start"])
+            stop = max(df_subset["end"])
+            index = min(df_subset.index)
+            line = pd.DataFrame(
+                {
                 "seqname": df_subset["seqname"][index],
                 "source": df_subset["source"][index],
                 "feature": "transcript",
@@ -50,22 +50,19 @@ def analyse_genmark(df):
                 "frame": ".",
                 "transcript_id": df_subset["transcript_id"][index],
                 "gene_id": df_subset["gene_id"][index],
-            },
-            index=[index],
-        )
-
-        if i == list(gene_list)[0]:
-            df_genmark_final = pd.concat(
-                [df_subset.iloc[index - 1 :], line, df_subset.iloc[: index - 1]]
-            ).reset_index(drop=True)
-        else:
-            df_genmark_final = df_genmark_final.append(
-                pd.concat(
-                    [df_subset.iloc[index - 1 :], line, df_subset.iloc[: index - 1]]
-                ).reset_index(drop=True)
-            ).reset_index(drop=True)
-
-    df_genmark_final = df_genmark_final.replace(np.nan, "")
+                },
+                index=[index],
+            )
+            df_subset = pd.concat([df_subset.iloc[index - 1 :], line, df_subset.iloc[: index - 1]]).reset_index(drop=True)                                                  
+            df_subset.fillna('',inplace=True)    
+            df_subset["score"] = df_subset["score"].replace(0.0, ".")
+            old_index = df_subset[df_subset["feature"] == "transcript"].index.item() 
+            df_subset_new = df_subset.T.reindex(columns=([old_index] + list([a for a in df_subset.T.columns if a != old_index])
+                        )
+                )
+            df_genmark_final = df_genmark_final.append(df_subset_new.T).reset_index(
+                    drop=True
+                )
     
     return df_genmark_final
 
@@ -78,56 +75,65 @@ def analyse_augustus(df):
     df_augustus["frame"][df_augustus["feature"] == "intron"] = "."
     df_augustus["frame"][df_augustus["feature"] == "gene"] = "."
     df_augustus["frame"][df_augustus["feature"] == "transcript"] = "."
-    
-    df_augustus = df_augustus.drop(
-        df_augustus[df_augustus.duplicated()].index, axis=0
-    ).reset_index(drop=True)
-    df_augustus = df_augustus.drop(
-        df_augustus[df_augustus["feature"] == "gene"].index, axis=0
-    ).reset_index(drop=True)
-    df_augustus = df_augustus.drop(
-        df_augustus[df_augustus["feature"] == "intron"].index, axis=0
-    ).reset_index(drop=True)
-    df_augustus = df_augustus.drop_duplicates().reset_index(drop=True)
     df_augustus["transcript_id"] = [
-        re.sub(r"^jg", "braker_augustus", str(x)) for x in df_augustus["transcript_id"]
+        re.sub(r"^g", "braker_augustus", str(x)) for x in df_augustus["transcript_id"]
     ]
     df_augustus["gene_id"] = [
-        re.sub(r"^jg", "braker_augustus", str(x)) for x in df_augustus["gene_id"]
-    ]
-    transcript_list = set(df_augustus["transcript_id"])
+        re.sub(r"^g", "braker_augustus", str(x)) for x in df_augustus["gene_id"]
+    ]    
+
+    df_augustus = df_augustus.drop(df_augustus[df_augustus["feature"] == "gene"].index, axis=0).reset_index(drop=True)
+    df_augustus = df_augustus.drop(df_augustus[df_augustus["feature"] == "intron"].index, axis=0).reset_index(drop=True)
+
+    for i in df_augustus[df_augustus['feature']=='transcript'].index.tolist():
+        df_augustus['transcript_id'][i]=df_augustus['transcript_id'][i-1]
+        df_augustus['gene_id'][i]=df_augustus['gene_id'][i-1]
+
     df_augustus_final = pd.DataFrame()
-    for i in transcript_list:
-        
-        df_subset = df_augustus[df_augustus["transcript_id"] == i]
 
-        old_index = df_subset[df_subset["feature"] == "transcript"].index.item()
+    for i in set(df_augustus["gene_id"]):
+        df_gene = df_augustus[df_augustus["gene_id"] == i]
+        for j in set(df_gene["transcript_id"]):
 
-        df_subset_new = df_subset.T.reindex(
-            columns=(
-                [old_index] + list([a for a in df_subset.T.columns if a != old_index])
-            )
-        )
+                
+            df_transcript = df_gene[df_gene["transcript_id"] == j]
 
-        df_augustus_final = df_augustus_final.append(df_subset_new.T).reset_index(
-            drop=True
-        )
-        
+            if len(df_transcript[df_transcript["feature"] == "transcript"]) > 1 or len(df_transcript[df_transcript["feature"] == "transcript"]) ==0:
 
-    
+                df_transcript=df_transcript.drop(df_transcript[df_transcript["feature"] == "transcript"].index, axis=0).reset_index(drop=True)           
+                start=min(df_transcript['start'])
+                stop=max(df_transcript['end'])
+                index=min(df_transcript.index)          
+                line = pd.DataFrame({"seqname":df_transcript['seqname'][index],"source":df_transcript['source'][index], "feature":'transcript',"start":start, "end":stop,"score":'.', "strand":df_transcript['strand'][index], "frame":'.', "transcript_id":df_transcript['transcript_id'][index], "gene_id":df_transcript['gene_id'][index],}, index=[index])                
+                df_transcript=pd.concat([df_transcript.iloc[index-1:], line, df_transcript.iloc[:index-1]]).reset_index(drop=True)
+                df_transcript.fillna('',inplace=True)
+            print(df_transcript)
+            old_index = df_transcript[df_transcript["feature"] == "transcript"].index.item()
+            df_subset_new = df_transcript.T.reindex(
+                columns=(
+                    [old_index] + list([a for a in df_transcript.T.columns if a != old_index])
+                        )
+                )
+
+            df_augustus_final = df_augustus_final.append(df_subset_new.T).reset_index(
+                    drop=True
+                )
+
     return df_augustus_final
-
 
 def create_file(df):
     new_file = []
-
+    df.fillna('',inplace=True)
     for l in range(0, len(df)):
         temp = []
         first_line_part = "\t".join([str(i) for i in df.loc[l, "seqname":"frame"]])
         second_line_part = df.loc[l, "transcript_id":].tolist()
         col_index = 8
+        #print(second_line_part)
         for t in second_line_part:
-            if len(t) != 0:
+            #print(t)
+            if t!='':
+            #if len(t) != 0:
                 temp.append(df.columns.values.tolist()[col_index] + " " + '"' + t + '"')
                 col_index = col_index + 1
 
@@ -162,5 +168,5 @@ if __name__ == "__main__":
         df_augustus_final = analyse_augustus(df)
         augustus_list = create_file(df_augustus_final)
 
-    output = genmark_list+augustus_list
+    output = genmark_list + augustus_list
     save_file(output, output_file)
