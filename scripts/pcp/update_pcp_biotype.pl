a50715aff8b3c7f9cfc1cc96f8a123d4c0d9979f
ens-carlos <ens-carlos@users.noreply.github.com>
2022-01-14 12:59:13 +0000

    Simplify rnasamba results keys throw.

--- scripts/pcp/update_pcp_biotype.pl
+++ scripts/pcp/update_pcp_biotype.pl
@@ -84,18 +84,9 @@ my %selected_genes;
 if (%cpc_results ne %rnasamba_results) {
   throw("Results files have different number of gene models\n");
 }
-else {
-  my %compare = map {$_ => 1} keys %cpc_results;
-  for my $key (keys %rnasamba_results) {
-    last unless exists $compare{$key};
-    delete $compare{$key};
-  }
-  if (%compare) {
-    throw("Results files contain different gene models\n");
-  }
-}
 
 foreach my $key (keys %cpc_results) {
+  throw("Results files contain different gene models\n") unless (exists $rnasamba_results{$key));
   ## Currently using AND to account for relative sensitivity  / specifity differences
   ## between the algorithms.
   if ($rnasamba_results{$key} eq 'coding' and $cpc_results{$key} eq 'coding') {
