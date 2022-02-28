8a2c535c7097e837b128a83dff3554981ec61ff8
Leanne Haggerty <leanne@codon-login-01.ebi.ac.uk>
2022-02-28 12:30:49 +0000

    Yearly copyright update

--- scripts/delete_big_dir.pl
+++ scripts/delete_big_dir.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/env perl
 
-# Copyright [2018-2020] EMBL-European Bioinformatics Institute
+# Copyright [2018-2022] EMBL-European Bioinformatics Institute
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
