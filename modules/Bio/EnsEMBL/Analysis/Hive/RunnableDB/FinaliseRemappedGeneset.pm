4c411a93676048df112fe19469241298c7757518
Fergal Martin <fergal@codon-login-01.ebi.ac.uk>
2022-04-12 19:42:11 +0100

    Change to FinaliseRemappedGeneset.pm Updated to take into account updated description. Biggest change was to update the filter overlap method to remove genes based on unexpected genomic overlap. This is still quite problematic and it leaves many overlapping genes, but it does get rid of most straightforward cases

--- modules/Bio/EnsEMBL/Analysis/Hive/RunnableDB/FinaliseRemappedGeneset.pm
+++ modules/Bio/EnsEMBL/Analysis/Hive/RunnableDB/FinaliseRemappedGeneset.pm
@@ -44,6 +44,7 @@ use File::Spec::Functions qw(tmpdir catfile);
 
 use Bio::EnsEMBL::Analysis::Runnable::GeneBuilder;
 use Bio::EnsEMBL::Analysis::Tools::Algorithms::ClusterUtils;
+use Bio::EnsEMBL::Analysis::Tools::GeneBuildUtils::TranscriptUtils qw(features_overlap clone_Transcript);
 use Bio::EnsEMBL::Analysis::Tools::Utilities qw(create_file_name align_nucleotide_seqs);
 use Bio::EnsEMBL::Variation::Utils::FastaSequence qw(setup_fasta);
 use Bio::EnsEMBL::Analysis::Runnable::Minimap2Remap;
@@ -90,7 +91,14 @@ sub fetch_input {
 
 
   my $gene_adaptor = $target_dba->get_GeneAdaptor();
+
+# TEST CODE FOR DEBUGGING !!!!!!!!!!!!!!!!!!!
+#  my $slice_adaptor = $target_dba->get_SliceAdaptor();
+#  my $slice = $slice_adaptor->fetch_by_region('toplevel','4');
+#  my $all_initial_genes = $gene_adaptor->fetch_all_by_Slice($slice);
+
   my $all_initial_genes = $gene_adaptor->fetch_all();
+
 
   # First we'll parse the descriptions of all the gene/transcripts and filter out the potential paralogues, as these have already been qc'd at this point
   my $primary_genes = $self->parse_descriptions($all_initial_genes);
@@ -115,8 +123,10 @@ sub run {
   my $duplicate_genes_hash = $self->generate_duplicate_genes_hash($first_filtering);
   my $duplicate_filtering = $self->filter_duplicates($first_filtering,$duplicate_genes_hash);
 
-  my $genes_to_remove = $self->genes_to_remove($initial_genes,$duplicate_filtering);
-  $self->output($genes_to_remove);
+  my $duplicate_genes_to_remove = $self->genes_to_remove($initial_genes,$duplicate_filtering);
+#  my $low_identity_genes_to_remove = $self->low_identity_genes_to_remove($initial_genes);
+
+  $self->output($duplicate_genes_to_remove);#@$low_identity_genes_to_remove]);
 }
 
 
@@ -126,13 +136,69 @@ sub write_output {
   my $output_gene_adaptor = $output_dba->get_GeneAdaptor();
   my $output_slice_adaptor = $output_dba->get_SliceAdaptor();
   my $genes_to_remove = $self->output();
+  my $genes_to_write = $self->genes_to_write();
 
   # Remove the initial set of paralogues prior to collapsing
+  my $removed_genes = {};
   foreach my $gene (@$genes_to_remove) {
-    $output_gene_adaptor->remove($gene);
+    unless($removed_genes->{$gene->dbID()}) {
+      say "Gene to remove: ".$gene->stable_id()." ".$gene->seq_region_name().":".$gene->biotype().":".$gene->seq_region_start().":".$gene->seq_region_end().":".$gene->strand();
+      $removed_genes->{$gene->dbID()} = 1;
+      $output_gene_adaptor->remove($gene);
+    }
+  }
+
+  # This code can be used to update the biotypes of remaining genes with unexpected overlaps to investigate them
+#  my $genes_to_tag = $self->genes_to_tag();
+#  foreach my $id (keys(%$genes_to_tag)) {
+#    my $gene = $genes_to_tag->{$id};
+#    $gene->biotype('overlap');
+#    $output_gene_adaptor->update($gene);
+#  }
+
+
+  foreach my $gene (@$genes_to_write) {
+    say "Gene to write: ".$gene->stable_id()." ".$gene->seq_region_name().":".$gene->biotype().":".$gene->seq_region_start().":".$gene->seq_region_end().":".$gene->strand();
+    $output_gene_adaptor->store($gene);
   }
 
   $self->add_gene_symbols($output_gene_adaptor,$output_slice_adaptor);
+}
+
+
+sub low_identity_genes_to_remove {
+  my ($self,$genes) = @_;
+
+  my $low_identity_genes = [];
+  my $average_identity_cutoff = 97;
+  my $average_coverage_cutoff = 50;
+
+  foreach my $gene (@$genes) {
+    if($gene->description() =~ /Potential paralogue/) {
+      next;
+    }
+    my $transcripts = $gene->get_all_Transcripts();
+    my $cumulative_identity = 0;
+    my $cumulative_coverage = 0;
+    foreach my $transcript (@$transcripts) {
+      my $description = $transcript->description();
+      $description =~ /Coverage\: ([^,]+)\, Perc id\: ([^,]+)\,/;
+      my $coverage = $1;
+      my $perc_id = $2;
+      $cumulative_coverage += $coverage;
+      $cumulative_identity += $perc_id;
+    }
+
+    my $transcript_count = scalar(@$transcripts);
+    my $average_coverage = $cumulative_coverage/$transcript_count;
+    my $average_identity = $cumulative_identity/$transcript_count;
+    unless($average_coverage >= $average_identity_cutoff and $average_identity >= $average_identity_cutoff) {
+      say "Flagging ".$gene->stable_id()." for having a low average transcript mapping score";
+      push(@$low_identity_genes,$gene);
+    }
+  }
+
+  return($low_identity_genes);
 }
 
 
@@ -146,6 +212,11 @@ sub parse_descriptions {
   my $avg_cov_threshold = 80;
   my $avg_perc_id_threshold = 95;
   my $primary_genes = [];
+  my $annotation_method_rankings = $self->annotation_method_rankings();
+  # Used to give the gene itself an annotation method, picks the method of the transcript that scores the highest in the
+  # list below. The list is ranked on how much we trust the approaches. Approaches that use the genomic context are trusted
+  # more that those that don't. Pure projection is best as minimap/exonerate sometimes introduce mapping artifacts
+
   foreach my $gene (@{$genes}) {
     my $gene_description = $gene->description();
     $gene_description =~ /^Parent\: ([^\,]+)\, Type\: (.+)$/;
@@ -153,30 +224,32 @@ sub parse_descriptions {
     my $gene_type = $2;
     $gene->{'parent_stable_id'} = $gene_stable_id; # Not really needed, since the stable id should be assigned, but doing it as it may be useful
     $gene->{'type'} = $gene_type;
-
-    # TEST TILL FIX DESCRIPTIONS IN COLLAPSE/FINDPARA
-    # I mean technically these aren't actually needed anyway
-#    if($gene_type eq 'Potential paralogue' or $gene_type eq 'Potential Paralogue') {
-#      next;
-#    }
+    $gene->{'annotation_method'} = 'not_assigned';
 
     my $total_cov = 0;
     my $total_perc_id = 0;
     my $transcripts = $gene->get_all_Transcripts();
     if (scalar(@$transcripts) == 0) {
-      next;
+      $self->throw("Found a gene with no transcripts. Gene: ".$gene->stable_id());
     }
+
     foreach my $transcript (@$transcripts) {
       # Parse the cov and percent id out, also track the total cov/perc id to get average for gene
       my $transcript_description = $transcript->description();
 
-      $transcript_description =~ /^Parent\: ([^\,]+)\, Coverage\: ([^\,]+)\, Perc id\: (.+)$/;
+      $transcript_description =~ /^Parent\: ([^\,]+)\, Coverage\: ([^\,]+)\, Perc id\: ([^\,]+)\,.+Annotation method\: (.+)$/;
       my $transcript_stable_id = $1;
       my $cov = $2;
       my $perc_id = $3;
+      my $annotation_method = $4;
       $transcript->{'parent_stable_id'} = $transcript_stable_id;
       $transcript->{'cov'} = $cov;
       $transcript->{'perc_id'} = $perc_id;
+      $transcript->{'annotation_method'} = $annotation_method;
+      if($annotation_method_rankings->{$annotation_method} > $annotation_method_rankings->{$gene->{'annotation_method'}}) {
+        $gene->{'annotation_method'} = $annotation_method;
+      }
+
       $total_cov += $cov;
       $total_perc_id += $perc_id;
     }
@@ -244,6 +317,149 @@ sub generate_duplicate_genes_hash {
 
 
 sub filter_overlapping {
+  my ($self,$genes_by_slice_hash) = @_;
+
+  say "Looking for overlapping genes";
+  my $source_gene_db = $self->hrdb_get_con('source_gene_db');
+  my $source_gene_adaptor = $source_gene_db->get_GeneAdaptor();
+  my $annotation_method_rankings = $self->annotation_method_rankings();
+  # This is a tricky one, we want to get overlapping genes on the current gene, and if the there are good genes of certain biotype groups, then remove the bad one
+  my $filtered_genes = [];
+  my $merged_genes = [];
+  foreach my $slice_name (keys(%{$genes_by_slice_hash})) {
+    my $genes = $genes_by_slice_hash->{$slice_name};
+    my @sorted_genes = sort { $a->strand <=> $b->strand or $a->start <=> $b->start } @$genes;
+
+    for(my $i=0; $i<scalar(@sorted_genes)-1; $i++) {
+      my $gene_left = $sorted_genes[$i];
+      unless($gene_left) {
+        next;
+      }
+
+      say "Processing ".$gene_left->stable_id()." for overlapping genes";
+      for(my $j=$i+1; $j<scalar(@sorted_genes); $j++) {
+        my $gene_right = $sorted_genes[$j];
+        unless($gene_right) {
+          next;
+        }
+
+        # If the strands differ or the genes don't overlap then move on
+        unless($gene_left->strand == $gene_right->strand and features_overlap($gene_left,$gene_right)) {
+          last;
+        }
+
+        # At this point we know the genes are on the same strand and overlap with one another. The next thing
+        # to check is whether they overlap in the reference
+        my $parent_stable_id_left = $gene_left->{'parent_stable_id'};
+        my $parent_stable_id_right = $gene_right->{'parent_stable_id'};
+
+        my $unversioned_id_left = $parent_stable_id_left;
+        $unversioned_id_left =~ s/\.\d+$//;
+        my $parent_gene_left = $source_gene_adaptor->fetch_by_stable_id($unversioned_id_left);
+
+        my $unversioned_id_right = $parent_stable_id_right;
+        $unversioned_id_right =~ s/\.\d+$//;
+        my $parent_gene_right = $source_gene_adaptor->fetch_by_stable_id($unversioned_id_right);
+
+        unless($parent_gene_left and $parent_gene_right) {
+          $self->throw("Issue fetching source genes with the following stable ids: ".$parent_stable_id_left.", ".$parent_stable_id_right);
+        }
+
+
+        # First check if the parent stable ids are the same, because there's a few cases where there are unusual genes, where there are transcript
+        # in GRCh38 that don't have exon overlap with anything else in the gene. These ones might be incorrectly separated at this point
+        if($parent_stable_id_left eq $parent_stable_id_right) {
+          my $merged_gene = $self->merge_genes($gene_left,$gene_right,$parent_gene_left);
+          $sorted_genes[$i] = 0;
+          $sorted_genes[$j] = 0;
+          push(@$merged_genes,$merged_gene);
+          last;
+        }
+
+        # If the parent genes also overlap then move on
+        if(features_overlap($parent_gene_left,$parent_gene_right)) {
+          last;
+        }
+
+        say "Found genes not overlapping in parent, that overlap in target on the same strand. Left gene: ".$unversioned_id_left.", Right gene: ".$unversioned_id_right;
+        # Now we know we have two overlapping genes on the same strand that aren't overlapping in the reference
+        # At this point we want to see if we can figure out if one is clearly a better mapping
+        my $avg_coverage_left = $gene_left->{'avg_cov'};
+        my $avg_identity_left = $gene_left->{'avg_perc_id'};
+        my $combined_score_left = $avg_coverage_left + $avg_identity_left;
+        my $annotation_method_left = $gene_left->{'annotation_method'};
+        my $avg_coverage_right = $gene_right->{'avg_cov'};
+        my $avg_identity_right = $gene_right->{'avg_perc_id'};
+        my $combined_score_right = $avg_coverage_right + $avg_identity_right;
+        my $annotation_method_right = $gene_right->{'annotation_method'};
+        if(($combined_score_left) >= 198 and ($combined_score_right) < 198) {
+          say "Left gene passes quailty check, right doesn't, will remove right";
+          $sorted_genes[$j] = 0;
+          next;
+        } elsif(($combined_score_left) < 198 and ($combined_score_right) >= 198) {
+          say "Right gene passes quailty check, left doesn't, will remove left";
+          $sorted_genes[$i] = 0;
+          last;
+        } elsif(($combined_score_left) >= 198 and ($combined_score_right) >= 198) {
+          say "Both genes are high scoring, will consider identity then method";
+#          if($annotation_method_left eq $annotation_method_right) {
+#            if($avg_identity_left > $avg_identity_right) {
+#              say "Left gene has the higher identity, remove right";
+#              $sorted_genes[$j] = 0;
+#              next;
+#            } elsif($avg_identity_left < $avg_identity_right) {
+#              say "Right gene has the higher identity, remove left";
+#              $sorted_genes[$i] = 0;
+#              last;
+#            }
+#          }
+
+          unless($gene_left->biotype() eq $gene_right->biotype()) {
+            next;
+          }
+
+          $gene_left->{'overlap_issue'} = 1;
+          $gene_right->{'overlap_issue'} = 1;
+          $self->genes_to_tag($gene_left);
+          $self->genes_to_tag($gene_right);
+
+        } else {
+          # In this case both genes have some issues, if there's a 5 percent difference across their combined scores, just take the
+          # best one
+          say "Neither method passes score check, will look for a noteable difference in scores";
+          my $score_diff = $combined_score_left - $combined_score_right;
+          if($score_diff >= 5) {
+            say "Left scores better, remove right";
+            $sorted_genes[$j] = 0;
+            next;
+          } elsif($score_diff <= -5) {
+            say "Right scores better, remove left";
+            $sorted_genes[$i] = 0;
+            last;
+          } else {
+            say "No noteable score difference, so keep both";
+            next;
+          }
+        } # end else
+      } # for j
+    } # for i
+
+    foreach my $gene (@sorted_genes) {
+      if($gene) {
+        push(@$filtered_genes,$gene);
+      }
+    }
+  } # End foreach my $slice_name
+
+  say "Have a total of ".scalar(@$merged_genes)." merged genes";
+  $self->genes_to_write($merged_genes);
+
+  say "Have a total of ".scalar(@$filtered_genes)." genes after filtering overlapping genes";
+  return($filtered_genes);
+}
+
+
+sub filter_overlapping_old {
   my ($self,$genes_by_slice_hash) = @_;
 
   # This is a tricky one, we want to get overlapping genes on the current gene, and if the there are good genes of certain biotype groups, then remove the bad one
@@ -347,12 +563,6 @@ sub filter_duplicates {
           }
         } # foreach my $transcript
       } # foreach my $duplicate_gene
-
-      # This actually doesn't work, as sometimes the canonical is in two genes
-      # If the canonical is good, then just go with that gene
- #     if($canonical_gene and $canonical_gene->{'quality_call'} eq 'good') {
- #       push(@$filtered_genes,$gene);
- #     } else {
 
       my $good_coding = 0;
       foreach my $gene (@$good_genes) {
@@ -464,5 +674,84 @@ sub add_gene_symbols {
   } # End foreach my $slice
 }
 
+
+sub annotation_method_rankings {
+  my ($self) = @_;
+  my $annotation_method_rankings = { 'alignment_projection' => 5,
+                                     'minimap_local'        => 4,
+                                     'exonerate_local'      => 3,
+                                     'minimap_global'       => 2,
+                                     'minimap_paralogue'    => 1,
+                                     'not_assigned'         => 0,
+                                   };
+
+  return($annotation_method_rankings);
+}
+
+
+sub merge_genes {
+  my ($self,$gene1,$gene2,$source_gene) = @_;
+
+  say "Merging genes with stable id ".$gene1->stable_id();
+  my $merged_gene = clone_Gene($gene1);
+  my $transcript1_list = {};
+  my $transcripts1 = $gene1->get_all_Transcripts();
+  foreach my $transcript (@$transcripts1) {
+    my $transcript_stable_id = $transcript->{'parent_stable_id'};
+    my $unversioned_stable_id = $transcript_stable_id;
+    $unversioned_stable_id =~ s/\.(\d+)$//;
+    $transcript1_list->{$unversioned_stable_id} = 1;
+  }
+
+  my $source_transcript_list = {};
+  my $source_transcripts = $source_gene->get_all_Transcripts();
+  foreach my $transcript (@$source_transcripts) {
+    $source_transcript_list->{$transcript->stable_id} = 1;
+  }
+
+  my $transcripts2 = $gene2->get_all_Transcripts();
+  foreach my $transcript (@$transcripts2) {
+    my $stable_id = $transcript->{'parent_stable_id'};
+    my $unversioned_stable_id = $stable_id;
+    $unversioned_stable_id =~ s/\.(\d+)$//;
+    if(!($transcript1_list->{$unversioned_stable_id}) and $source_transcript_list->{$unversioned_stable_id}) {
+      say "Adding transcript ".$stable_id." into merged gene ".$source_gene->stable_id;
+      my $new_transcript = clone_Transcript($transcript);
+      $merged_gene->add_Transcript($new_transcript);
+    } elsif(!($transcript1_list->{$stable_id})) {
+      say "Did not find transcript stable id in source transcript stable id list, this is unusual, so not adding";
+    }
+  }
+  return($merged_gene);
+}
+
+
+sub genes_to_write {
+  my ($self,$genes) = @_;
+
+  if($self->{'_genes_to_write'}) {
+    foreach my $gene (@$genes) {
+      push(@{$self->{'_genes_to_write'}},$gene);
+    }
+  } else {
+    $self->{'_genes_to_write'} = $genes;
+  }
+  return($self->{'_genes_to_write'});
+}
+
+
+sub genes_to_tag {
+  my ($self,$gene) = @_;
+
+  unless($self->{'_genes_to_tag'}) {
+    $self->{'_genes_to_tag'} = {};
+  }
+
+  if($gene) {
+    $self->{'_genes_to_tag'}->{$gene->dbID()} = $gene;
+  }
+
+  return($self->{'_genes_to_tag'});
+}
 
 1;
