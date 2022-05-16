25a1e4926b03d7b5e556086c88abbafc354de994
ens-carlos <ens-carlos@users.noreply.github.com>
2022-05-16 15:36:20 +0100

    Replaced muscle alignment with mafft using accuracy-oriented method on Minimap2Remap.

--- modules/Bio/EnsEMBL/Analysis/Runnable/Minimap2Remap.pm
+++ modules/Bio/EnsEMBL/Analysis/Runnable/Minimap2Remap.pm
@@ -1195,24 +1195,24 @@ sub project_gene_coords {
 #      say "Alignment:\n".$aligned_source_seq."\n".$aligned_target_seq;
       $projected_exons_by_id->{$projected_exon->{'source_stable_id'}} = $projected_exon;
     } elsif ($projected_exon) {
-      # if the projected exon is longer than the source exon by more than 20% of the length of the source exon, realign using muscle
-      # if muscle alignment is better and complies with the length restriction, use it; if not, leave the exon as not projected
+      # if the projected exon is longer than the source exon by more than 20% of the length of the source exon, realign using mafft accuracy-oriented method
+      # if mafft accuracy-oriented method alignment is better and complies with the length restriction, use it; if not, leave the exon as not projected
       
-      my $muscle_coverage = 0;
-      my $muscle_percent_id = 0;
-      my $muscle_aligned_source_seq;
-      my $muscle_aligned_target_seq;
+      my $mafftaccu_coverage = 0;
+      my $mafftaccu_percent_id = 0;
+      my $mafftaccu_aligned_source_seq;
+      my $mafftaccu_aligned_target_seq;
 
       eval {
-        ($muscle_coverage,$muscle_percent_id,$muscle_aligned_source_seq,$muscle_aligned_target_seq) = align_nucleotide_seqs($source_genome_seq,$target_genome_seq,'muscle');
+        ($mafftaccu_coverage,$mafftaccu_percent_id,$mafftaccu_aligned_source_seq,$mafftaccu_aligned_target_seq) = align_nucleotide_seqs($source_genome_seq,$target_genome_seq,'mafft --localpair --maxiterate 1000');
       };
       if ($@) {
-        $self->warning("Issue with running MUSCLE on target region");
+        $self->warning("Issue with running 'mafft --localpair --maxiterate 1000' on target region");
       } else {
-        say "Aligned source and target regions with MUSCLE: Coverage: ".$muscle_coverage.", Percent id: ".$muscle_percent_id;
-        if (($muscle_coverage + $muscle_percent_id) > ($coverage + $percent_id)) {
-          $aligned_source_seq = $muscle_aligned_source_seq;
-          $aligned_target_seq = $muscle_aligned_target_seq;
+        say "Aligned source and target regions with 'mafft --localpair --maxiterate 1000': Coverage: ".$mafftaccu_coverage.", Percent id: ".$mafftaccu_percent_id;
+        if (($mafftaccu_coverage + $mafftaccu_percent_id) > ($coverage + $percent_id)) {
+          $aligned_source_seq = $mafftaccu_aligned_source_seq;
+          $aligned_target_seq = $mafftaccu_aligned_target_seq;
           $projected_exon = $self->project_feature(undef,$exon,$source_region_start,$exon_region_start,$exon_region_end,$aligned_source_seq,$aligned_target_seq,$target_region_slice,$target_strand);
           if ($projected_exon and $projected_exon->length() <= $exon->length()*1.2) {
             my ($proj_coverage,$proj_percent_id,$aligned_source_seq,$aligned_target_seq) = align_nucleotide_seqs($exon->seq->seq(),$projected_exon->seq->seq());
