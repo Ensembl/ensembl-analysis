15e3f6a4b8ddfea14d580ab0bc21af496ae27abc
ens-carlos <carlos@ebi.ac.uk>
2022-06-10 09:34:57 +0100

    Added parent_gene att in gene description in case it does not exist.

--- modules/Bio/EnsEMBL/Analysis/Hive/RunnableDB/FindRecentParalogues.pm
+++ modules/Bio/EnsEMBL/Analysis/Hive/RunnableDB/FindRecentParalogues.pm
@@ -367,7 +367,10 @@ sub filter_by_cutoffs {
 
     #my $gene_description = ";parent_gene=".$source_transcript->{'parent_gene_stable_id'}.";mapping_type=potential_paralogue";
     #$gene->description($gene_description);
-    $gene->description($source_gene->description()." ;mapping_type=potential_paralogue");
+    if ($gene->description() !~ /;parent_gene=([^;]+);/) {
+      $gene->description($source_gene->description().";parent_gene=".$source_transcript->{'parent_gene_stable_id'});
+    }
+    $gene->description($source_gene->description().";mapping_type=potential_paralogue");
 
     # add source gene stable id as gene attribute
     $parent_attribute = Bio::EnsEMBL::Attribute->new(-CODE => 'proj_parent_g',-VALUE => $source_transcript->{'parent_gene_stable_id'});
