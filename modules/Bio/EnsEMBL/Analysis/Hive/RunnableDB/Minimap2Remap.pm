ef89e8fbd491981298295740aea50794e7c8f399
Thibaut Hourlier <thibaut@ebi.ac.uk>
2022-06-15 23:59:01 +0100

    Use unique ids instead of all

--- modules/Bio/EnsEMBL/Analysis/Hive/RunnableDB/Minimap2Remap.pm
+++ modules/Bio/EnsEMBL/Analysis/Hive/RunnableDB/Minimap2Remap.pm
@@ -199,7 +199,8 @@ sub fetch_source_genes {
   }
   close IN or $self->throw("Could not close $input_id_file");
   $source_genes = $source_gene_dba->get_GeneAdaptor->fetch_all_by_dbID_list(\@id_list);
-  if (@$source_genes != @id_list) {
+  my %unique_ids = map {$_ => 1} @id_list;
+  if (@$source_genes != scalar(keys %unique_ids)) {
     $self->throw("Fetched ".scalar(@$source_genes).' genes but expected '.scalar(@id_list));
   }
 
