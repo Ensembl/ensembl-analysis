d90f35573ae43f01bcfeeed071f6ae75b758fa8d
Fergal Martin <fergal@codon-login-01.ebi.ac.uk>
2022-06-26 16:08:52 +0100

    Change to CollapseParalogues.p Removed layering/genebuilder step and replaced with a much simpler layering. Any new potential paralogues that cluster on exon overlap are now sorted by biotype priority and only the longest transcript of whatever the highest biotype priority at the locus is is kept as the final gene. This will prevent new genes having multiple parents and unusual biotype combinations

--- modules/Bio/EnsEMBL/Analysis/Hive/RunnableDB/CollapseParalogues.pm
+++ modules/Bio/EnsEMBL/Analysis/Hive/RunnableDB/CollapseParalogues.pm
@@ -100,32 +100,32 @@ sub fetch_input {
 sub run {
   my ($self) = @_;
 
-  foreach my $runnable (@{$self->runnable()}) {
-    $runnable->run();
-    my $output_genes = $runnable->output();
-    my @parent_gene_ids = @{$runnable->{'parent_gene_ids'}};
+#  foreach my $runnable (@{$self->runnable()}) {
+#    $runnable->run();
+#    my $output_genes = $runnable->output();
+#    my @parent_gene_ids = @{$runnable->{'parent_gene_ids'}};
 
     # For some reason there seems to be an issue with the same id getting added multiple times
     # and this is happening for every new paralogue
     # Should debug at some point, but for now it's fine to just make unique here
-    my $id_seen = {};
-    my $id_string = "";
-    foreach my $id (@parent_gene_ids) {
-      unless($id_seen->{$id}) {
-        $id_string .= $id.":";
-        $id_seen->{$id} = 1;
-      }
-    }
-    $id_string =~ s/\:$//;
-    foreach my $output_gene (@$output_genes) {
-      my $gene_description = $output_gene->description().";parent_gene=".$id_string.";mapping_type=potential_paralogue";
-      $output_gene->description($gene_description);
-      # add string of source genes stable ids as gene attribute
-      my $parent_attribute = Bio::EnsEMBL::Attribute->new(-CODE => 'proj_parent_g',-VALUE => $id_string);
-      $output_gene->add_Attributes($parent_attribute);
-      $self->output([$output_gene]);
-    }
-  }
+#    my $id_seen = {};
+#    my $id_string = "";
+#    foreach my $id (@parent_gene_ids) {
+#      unless($id_seen->{$id}) {
+#        $id_string .= $id.":";
+#        $id_seen->{$id} = 1;
+#      }
+#    }
+#    $id_string =~ s/\:$//;
+#    foreach my $output_gene (@$output_genes) {
+#      my $gene_description = $output_gene->description().";parent_gene=".$id_string.";mapping_type=potential_paralogue";
+#      $output_gene->description($gene_description);
+#      # add string of source genes stable ids as gene attribute
+#      my $parent_attribute = Bio::EnsEMBL::Attribute->new(-CODE => 'proj_parent_g',-VALUE => $id_string);
+#      $output_gene->add_Attributes($parent_attribute);
+#      $self->output([$output_gene]);
+#    }
+#  }
 }
 
 
@@ -163,33 +163,40 @@ sub make_runnables {
 
   foreach my $cluster (@$clusters) {
     my $clustered_genes = $cluster->get_Genes();
-    my $layered_clustered_genes = $self->layer_cluster_genes($clustered_genes);
-    my $parent_gene_ids = [];
+    my $output_gene = $self->simple_layer_genes($clustered_genes);
+    $self->output([$output_gene]);
+
+    # NOTE!: Disabiling this for the moment as there seem to be genes getting through that have multiple parents with incompatible biotypes
+    # Instead we'll just take the parent biotype group with a basic heirarchy and then take the transcript with the most exons for that
+    # group
+
+#    my $layered_clustered_genes = $self->layer_cluster_genes($clustered_genes);
+#    my $parent_gene_ids = [];
 
     # There is a slight chance that this cluster has genes from multiple parents, so track this
-    foreach my $gene (@$clustered_genes) {
-      my $description = $gene->description();
-      $description =~ /;parent_gene=([^;]+);/;
-      my $parent_stable_id = $1;
-      push(@$parent_gene_ids,$parent_stable_id)
-    }
+#    foreach my $gene (@$clustered_genes) {
+#      my $description = $gene->description();
+#      $description =~ /;parent_gene=([^;]+);/;
+#      my $parent_stable_id = $1;
+#      push(@$parent_gene_ids,$parent_stable_id)
+#    }
 
-    my $output_biotype = ${$clustered_genes}[0]->biotype();
-    my $runnable = Bio::EnsEMBL::Analysis::Runnable::GeneBuilder->new(
-                     -query => $slice,
-                     -analysis => $self->analysis(),
-                     -genes => $clustered_genes,
-                     -output_biotype => $output_biotype,
-                     -max_transcripts_per_cluster => 1000,
-                     -min_short_intron_len => 0,
-                     -max_short_intron_len => 15,
-                     -blessed_biotypes => {},
-                     -skip_readthrough_check => 1,
-                     -max_exon_length => 50000,
-                     -coding_only => 1,
-                   );
-    $runnable->{'parent_gene_ids'} = $parent_gene_ids;
-    $self->runnable($runnable);
+#    my $output_biotype = ${$clustered_genes}[0]->biotype();
+#    my $runnable = Bio::EnsEMBL::Analysis::Runnable::GeneBuilder->new(
+#                     -query => $slice,
+#                     -analysis => $self->analysis(),
+#                     -genes => $clustered_genes,
+#                     -output_biotype => $output_biotype,
+#                     -max_transcripts_per_cluster => 1000,
+#                     -min_short_intron_len => 0,
+#                     -max_short_intron_len => 15,
+#                     -blessed_biotypes => {},
+#                     -skip_readthrough_check => 1,
+#                     -max_exon_length => 50000,
+#                     -coding_only => 1,
+#                   );
+#    $runnable->{'parent_gene_ids'} = $parent_gene_ids;
+#    $self->runnable($runnable);
   }
 
   # For unclustered stuff there's no need to build a runnable, so just put directly on the output
@@ -202,7 +209,7 @@ sub make_runnables {
 }
 
 
-sub layer_cluster_genes {
+sub simple_layer_genes {
   my ($self,$genes) = @_;
 
   # This is just a simple filtering to make sure we don't make any weird complex clusters like sncRNA transcripts with protein coding ones
@@ -215,22 +222,84 @@ sub layer_cluster_genes {
                                    'undefined'  => 0,
                                  };
 
-  my $filtered_genes = [];
+  my $selected_gene;
+  my $selected_transcript;
   my $max_priority = 0;
   foreach my $gene (@$genes) {
+    my $transcript = ${$gene->get_all_Transcripts()}[0];
     my $biotype_group = $gene->get_Biotype->biotype_group();
     my $priority = $biotype_group_priorities->{$biotype_group};
-    if($priority > $max_priority) {
+
+    if(!($selected_gene)) {
+      $selected_gene = $gene;
+      $selected_transcript = $transcript;
       $max_priority = $priority;
-      $filtered_genes = [];
-      push(@$filtered_genes,$gene);
-    } elsif($priority == $max_priority) {
-      push(@$filtered_genes,$gene);
+    } elsif($priority > $max_priority) {
+      $selected_gene = $gene;
+      $selected_transcript = $transcript;
+      $max_priority = $priority;
+    } elsif($priority == $max_priority and $transcript->length() > $selected_transcript->length()) {
+      $selected_gene = $gene;
+      $selected_transcript = $transcript;
+      $max_priority = $priority;
     }
   }
 
-  return($filtered_genes)
+  if($selected_gene->get_Biotype->biotype_group() eq 'coding') {
+    $selected_gene->biotype('protein_coding');
+    $selected_transcript->biotype('protein_coding');
+  } elsif($selected_gene->get_Biotype->biotype_group() eq 'lnoncoding') {
+    $selected_gene->biotype('lncRNA');
+    $selected_transcript->biotype('lncRNA');
+  } else {
+    $selected_transcript->biotype($selected_gene->biotype());
+  }
+
+  my $description = $selected_gene->description();
+  $description =~ /;parent_gene=([^;]+);/;
+  my $parent_stable_id = $1;
+  my $parent_attribute = Bio::EnsEMBL::Attribute->new(-CODE => 'proj_parent_g',-VALUE => $parent_stable_id);
+  $selected_gene->add_Attributes($parent_attribute);
+
+  $description = $selected_transcript->description();
+  $description =~ /;parent_transcript=([^;]+);/;
+  $parent_stable_id = $1;
+  $parent_attribute = Bio::EnsEMBL::Attribute->new(-CODE => 'proj_parent_t',-VALUE => $parent_stable_id);
+  $selected_transcript->add_Attributes($parent_attribute);
+
+  return([$selected_gene]);
 }
+
+
+#sub layer_cluster_genes {
+#  my ($self,$genes) = @_;
+
+  # This is just a simple filtering to make sure we don't make any weird complex clusters like sncRNA transcripts with protein coding ones
+  # It does mean that some potentially interesting edge cases might be missed, but this is fine for now
+#  my $biotype_group_priorities = { 'coding'     => 5,
+#                                   'lnoncoding' => 4,
+#                                   'pseudogene' => 3,
+#                                   'snoncoding' => 2,
+#                                   'mnoncoding' => 1,
+#                                   'undefined'  => 0,
+#                                 };
+
+#  my $filtered_genes = [];
+#  my $max_priority = 0;
+#  foreach my $gene (@$genes) {
+#    my $biotype_group = $gene->get_Biotype->biotype_group();
+#    my $priority = $biotype_group_priorities->{$biotype_group};
+#    if($priority > $max_priority) {
+#      $max_priority = $priority;
+#      $filtered_genes = [];
+#      push(@$filtered_genes,$gene);
+#    } elsif($priority == $max_priority) {
+#      push(@$filtered_genes,$gene);
+#    }
+#  }
+
+#  return($filtered_genes)
+#}
 
 
 sub generate_biotypes_hash {
