dde105f88c02f73734cd366e4f73b44d85f9133f
Thibaut Hourlier <thibaut@ebi.ac.uk>
2022-06-29 16:19:02 +0100

    Use fasta for dna. Use slices

--- scripts/genebuild/replace_stops_with_introns.pl
+++ scripts/genebuild/replace_stops_with_introns.pl
@@ -23,13 +23,12 @@
 use warnings;
 use strict;
 use feature 'say';
-use Digest::MD5 qw(md5);
 
-use Bio::EnsEMBL::Translation;
 use Bio::EnsEMBL::DBSQL::DBAdaptor;
 use Getopt::Long qw(:config no_ignore_case);
-use Bio::EnsEMBL::Analysis::Tools::GeneBuildUtils::GeneUtils qw(clone_Gene);
+use Bio::EnsEMBL::Analysis::Tools::GeneBuildUtils::GeneUtils qw(clone_Gene empty_Gene);
 use Bio::EnsEMBL::Analysis::Tools::GeneBuildUtils::TranscriptUtils qw(replace_stops_with_introns);
+use Bio::EnsEMBL::Analysis::Tools::Utilities qw(setup_fasta_db);
 
 my $dbname = '';
 my $dbuser   = '';
@@ -37,16 +36,23 @@ my $dbhost = '';
 my $dbport;
 my $dbpass;
 my $output_dir;
+my $fasta_file;
+my $write = 0;
 
 my $options = GetOptions ("dbuser=s"     => \$dbuser,
                           "dbhost=s"     => \$dbhost,
                           "dbport=i"     => \$dbport,
                           "dbname=s"     => \$dbname,
                           "dbpass=s"     => \$dbpass,
+                          "fasta_file=s" => \$fasta_file,
+                          "write!"       => \$write,
                           "output_dir=s" => \$output_dir);
 
+if ($fasta_file) {
+  setup_fasta_db;
+}
 my $log_file = $output_dir."/replace_stops_with_introns_".$dbname.".log";
-open(LOG,">".$log_file);
+open(LOG,">".$log_file) or die("could not open '$log_file' for writing");
 
 my $db = new Bio::EnsEMBL::DBSQL::DBAdaptor(
   -port    => $dbport,
@@ -55,57 +61,79 @@ my $db = new Bio::EnsEMBL::DBSQL::DBAdaptor(
   -host    => $dbhost,
   -dbname  => $dbname);
 
-my $gene_adaptor = $db->get_GeneAdaptor();
-
-foreach my $old_gene (@{$gene_adaptor->fetch_all()}) {
-  my $max_stops = 999999;
-  my $num_transcripts = 0;
-  my $num_transcripts_changed = 0;
-  my $transcripts = $old_gene->get_all_Transcripts();
-  my $gene = clone_Gene($old_gene);
-  say LOG "Gene: ".$old_gene->dbID()." ".$old_gene->stable_id();
-  $gene->flush_Transcripts();
-  TRANSCRIPT: foreach my $transcript (@$transcripts) {
-    if ($transcript->biotype() ne 'polymorphic_pseudogene' and
-        $transcript->translation() and
-        $transcript->translation()->seq() =~ /\*/) {
-      # we replace the stop codons with introns
-      say LOG "old translation: ".$transcript->translation()->seq();
-      $transcript = replace_stops_with_introns($transcript,$max_stops);
-      say LOG "new translation: ".$transcript->translation()->seq();
-      if ($transcript and $transcript->translate()->seq() !~ /\*/) {
-        $gene->add_Transcript($transcript);
-	$num_transcripts++;
-	$num_transcripts_changed++;
-        say LOG "The transcript ".$transcript->dbID()." ".$transcript->stable_id()." has been changed";
-      } elsif ($transcript and !$transcript->translate()) {
-        say LOG "Transcript ".$transcript->dbID()." ".$transcript->stable_id()." (seq_region_start,seq_region_end,seq_region_strand,seq_region_name) (".$transcript->seq_region_start().",".$transcript->seq_region_end().",".$transcript->seq_region_strand().",".$transcript->seq_region_name().") does not translate after replacing a maximum of $max_stops stops. Discarded.";
-      } elsif ($transcript) {
-        say LOG "Transcript ".$transcript->dbID()." ".$transcript->stable_id()." (seq_region_start,seq_region_end,seq_region_strand,seq_region_name) (".$transcript->seq_region_start().",".$transcript->seq_region_end().",".$transcript->seq_region_strand().",".$transcript->seq_region_name().") does not translate after replacing a maximum of $max_stops stops. Discarded.";
+if ($fasta_file) {
+  $db->get_SequenceAdaptor->fasta($fasta_file);
+}
+my $slice_adaptor = $db->get_SliceAdaptor;
+my @genes_to_process;
+my $count = 0;
+foreach my $slice (@{$slice_adaptor->fetch_all('toplevel', undef, undef, 1)}) {
+  next unless ($slice->seq_region_name eq "3");
+  foreach my $old_gene (@{$slice->get_all_Genes(undef, undef, 1)}) {
+    ++$count;
+    my $max_stops = 999999;
+    my $num_transcripts = 0;
+    my $num_transcripts_changed = 0;
+    my $transcripts = $old_gene->get_all_Transcripts();
+    my $gene = clone_Gene($old_gene, 1);
+    say LOG "Gene: ".$old_gene->dbID()." ".$old_gene->stable_id();
+    $gene->flush_Transcripts();
+    TRANSCRIPT: foreach my $transcript (@$transcripts) {
+      my $translation = $transcript->translation;
+      if ($translation and $transcript->biotype() ne 'polymorphic_pseudogene' and index($translation->seq(),'*') >= 0) {
+        # we replace the stop codons with introns
+        say LOG "old translation: ".$translation->seq();
+        my $new_transcript = replace_stops_with_introns($transcript,$max_stops);
+        say LOG "new translation: ".$new_transcript->translation()->seq();
+        if ($new_transcript and index($new_transcript->translate()->seq(), '*') == -1) {
+          $gene->add_Transcript($new_transcript);
+          if ($old_gene->canonical_transcript == $transcript) {
+            $gene->canonical_transcript($new_transcript);
+          }
+          $num_transcripts++;
+          $num_transcripts_changed++;
+          say LOG "The transcript ", $new_transcript->dbID(), " ", $new_transcript->stable_id(), " has been changed";
+        } elsif ($new_transcript and !$new_transcript->translate()) {
+          say LOG "Transcript ", $new_transcript->dbID(), " ", $new_transcript->stable_id(), " (seq_region_start,seq_region_end,seq_region_strand,seq_region_name) (", $new_transcript->seq_region_start(), ",", $new_transcript->seq_region_end(), ",", $new_transcript->seq_region_strand(), ",", $new_transcript->seq_region_name(), ") does not translate after replacing a maximum of $max_stops stops. Discarded.";
+        } elsif ($new_transcript) {
+          say LOG "Transcript ", $new_transcript->dbID(), " ", $new_transcript->stable_id(), " (seq_region_start,seq_region_end,seq_region_strand,seq_region_name) (", $new_transcript->seq_region_start(), ",", $new_transcript->seq_region_end(), ",", $new_transcript->seq_region_strand(), ",", $new_transcript->seq_region_name(), ") does not translate after replacing a maximum of $max_stops stops. Discarded!";
+        } else {
+          say LOG "No transcript defined after replacing stops.";
+        }
       } else {
-        say LOG "No transcript defined after replacing stops.";
+        # we skip the transcript by adding it to the gene as it is
+        say LOG "Transcript ", $transcript->dbID(), " ", $transcript->stable_id(), " does not contain any stop or it is a polymorphic_pseudogene. Kept as it is.";
+        $gene->add_Transcript($transcript);
+        $num_transcripts++;
       }
-    } else {
-      # we skip the transcript by adding it to the gene as it is
-      say LOG "Transcript ".$transcript->dbID()." ".$transcript->stable_id()." does not contain any stop or it is a polymorphic_pseudogene. Kept as it is.";
-      $gene->add_Transcript($transcript);
-      $num_transcripts++;
     }
-  }
-  
-  if ($num_transcripts and $num_transcripts_changed) {
-    say LOG "Removing old gene: ".$old_gene->dbID()." ".$old_gene->stable_id();
-    $gene_adaptor->remove($old_gene);
-    $gene->dbID(undef);
-    $gene_adaptor->store($gene);
-    say LOG "Stored new gene: ".$gene->dbID()." ".$gene->stable_id()."\n";
-  } elsif ($num_transcripts) {
-    say LOG "Gene kept: ".$gene->dbID()." ".$gene->stable_id()."\n";
-  } else {
-    say LOG "Gene does not have any transcript. Not storing.\n";
+    
+    if ($num_transcripts and $num_transcripts_changed) {
+      say LOG "Updating gene: ", $old_gene->dbID(), " ", $old_gene->stable_id();
+      empty_Gene($gene);
+      push(@genes_to_process, $old_gene, $gene);
+    } elsif ($num_transcripts) {
+      say LOG "Gene kept: ", $gene->dbID(), " ", $gene->stable_id();
+    } else {
+      say LOG "Gene does not have any transcript. Not storing.";
+    }
+    if ($count%1000 == 0) {
+      say "Processed $count genes at ".localtime;
+    }
   }
 }
 
-close LOG;
+my $gene_adaptor = $db->get_GeneAdaptor;
+foreach my $gene (@genes_to_process) {
+  if ($gene->dbID) {
+    say LOG "Removing old gene: ", $gene->dbID(), " ", $gene->stable_id();
+    $gene_adaptor->remove($gene);
+  }
+  else {
+    $gene_adaptor->store($gene);
+    say LOG "Stored new gene: ", $gene->dbID(), " ", $gene->stable_id();
+  }
+}
+close LOG or die("could not close '$log_file'");
 
 1;
