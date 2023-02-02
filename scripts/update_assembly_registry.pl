bc0f67068e5159b05c05400dc55be0f24437cfc8
Denye Ogeh <do1@ebi.ac.uk>
2023-02-02 06:37:17 +0000

    Update update_assembly_registry.pl

--- scripts/update_assembly_registry.pl
+++ scripts/update_assembly_registry.pl
@@ -65,17 +65,19 @@ my $registry_dba = new Bio::EnsEMBL::Analysis::Hive::DBSQL::AssemblyRegistryAdap
   -driver  => $driver,);
 
 my $registry_assembly_id = $registry_dba->fetch_assembly_id_by_gca($assembly_accession);
-my $sth = $registry_dba->dbc->prepare("UPDATE genebuild_status set progress_status =? where assembly_id=?");
+my $sth = $registry_dba->dbc->prepare("UPDATE genebuild_status set progress_status =? where assembly_id=? and is_current=?");
 $sth->bind_param(1,'completed');
 $sth->bind_param(2,$registry_assembly_id);
+$sth->bind_param(3,1);
 if ($sth->execute){
 }
 else{
  $self->throw("Could not update genebuild status with annotation progress");
 }
-$sth = $registry_dba->dbc->prepare("UPDATE genebuild_status set date_completed =? where assembly_id=?");
+$sth = $registry_dba->dbc->prepare("UPDATE genebuild_status set date_completed =? where assembly_id=? and is_current=?");
 $sth->bind_param(1,$date);
 $sth->bind_param(2,$registry_assembly_id);
+$sth->bind_param(3,1);
 if ($sth->execute){
 }   
 else{
