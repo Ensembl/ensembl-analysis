efc55bef2b581c05cea15a10c21e83b0c000d5ae
Leanne Haggerty <leanne@ebi.ac.uk>
2024-02-02 12:26:19 +0000

    update download and layers

--- modules/Bio/EnsEMBL/Analysis/Hive/Config/UniProtCladeDownloadStatic.pm
+++ modules/Bio/EnsEMBL/Analysis/Hive/Config/UniProtCladeDownloadStatic.pm
@@ -280,19 +280,11 @@ sub _master_config {
 	      compress  => 0,
 	      pe_level  => [1,2],
 	  },
-	  
-          fish_pe3 => {
-	      file_name => 'fish_pe12.fasta',
-	      taxon_id  => $taxon_ids->{'fish_taxon_id'},
-	      exclude_id => ['#taxon_id#'],
-	      dest_dir  => '#output_path#',
-	      compress  => 0,
-	      pe_level  => [1,2],
-	  },
-	  
+	  	  
 	  vert_pe12 => {
 	      file_name => 'vert_pe12.fasta',
 	      taxon_id  => $taxon_ids->{'vert_taxon_id'},
+	      exclude_id => [$taxon_ids->{'human_taxon_id'}, $taxon_ids->{'fish_taxon_id'}],
 	      dest_dir  => '#output_path#',
 	      compress  => 0,
 	      pe_level  => [1,2],
