1dfa8e87cd27115b861c2afeae06808f39202aee
Leanne Haggerty <leanne@ebi.ac.uk>
2024-02-16 14:34:45 +0000

    tweaking layers

--- modules/Bio/EnsEMBL/Analysis/Hive/Config/UniProtCladeDownloadStatic.pm
+++ modules/Bio/EnsEMBL/Analysis/Hive/Config/UniProtCladeDownloadStatic.pm
@@ -244,14 +244,6 @@ sub _master_config {
                              pe_level  => [1,2],
                            },
 
-                           #fish_pe3 => {
-                           #  file_name => 'fish_pe3.fasta',
-                           #  taxon_id  => $taxon_ids->{'fish_taxon_id'},
-                           #  exclude_id => ['#taxon_id#'],
-                           #  dest_dir  => '#output_path#',
-                           #  compress  => 0,
-                           #  pe_level  => [3],
-                           #},
              mammals_pe12 => {
                                  file_name  => 'mammals_pe12.fasta',
                                  taxon_id   => $taxon_ids->{'mammals_taxon_id'},
@@ -264,17 +256,11 @@ sub _master_config {
                vert_pe12 => {
                               file_name  => 'vert_pe12.fasta',
                               taxon_id   => $taxon_ids->{'vert_taxon_id'},
-                              exclude_id => [$taxon_ids->{'human_taxon_id'},$taxon_ids->{'mammals_taxon_id'}, $taxon_ids->{fish_taxon_id}],
+                              exclude_id => [$taxon_ids->{'human_taxon_id'},$taxon_ids->{'mammals_taxon_id'}, $taxon_ids->{'fish_taxon_id'}],
                               dest_dir   => '#output_path#',
                               compress   => 0,
                               pe_level   => [1,2],
                             },
-
-
-                         
-                         
-                         
-                         
                          },
 
       aves_basic => {
