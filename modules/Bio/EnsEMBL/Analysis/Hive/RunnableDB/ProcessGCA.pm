67a4384ec2d7c1f8e3f3cfe99e6a7fd44dffd771
Jack Tierney <jackcurragh@gmail.com>
2025-06-25 08:28:47 +0100

    tmp change to allow testing on mammalia for pre-release. Will Be Walked Back

--- modules/Bio/EnsEMBL/Analysis/Hive/RunnableDB/ProcessGCA.pm
+++ modules/Bio/EnsEMBL/Analysis/Hive/RunnableDB/ProcessGCA.pm
@@ -517,6 +517,12 @@ sub get_clade_params {
       $clade_params->{'busco_protein_file'}   = '/nfs/production/flicek/ensembl/genebuild/genebuild_virtual_user/protein_sets/actinopterygii_orthodb_proteins.fa',
       $clade_params->{'rfam_accessions_file'} = '/hps/nobackup/flicek/ensembl/genebuild/blastdb/ncrna/Rfam_14.1/clade_accessions/rfam_teleost_ids.txt',
       $clade_params->{'species_division'}     = 'EnsemblVertebrates',;
+  } elsif ( $clade eq 'mammalia' ) {
+      # Just temp stuff for testing:
+      $clade_params->{'protein_file'} = '/nfs/production/flicek/ensembl/genebuild/genebuild_virtual_user/protein_sets/actinopterygii_uniprot_proteins.fa',
+      $clade_params->{'busco_protein_file'}   = '/nfs/production/flicek/ensembl/genebuild/genebuild_virtual_user/protein_sets/actinopterygii_orthodb_proteins.fa',
+      $clade_params->{'rfam_accessions_file'} = '/hps/nobackup/flicek/ensembl/genebuild/blastdb/ncrna/Rfam_14.1/clade_accessions/rfam_teleost_ids.txt',
+      $clade_params->{'species_division'}     = 'EnsemblVertebrates',;
   } elsif ( $clade eq 'hymenoptera' ) {
       $clade_params->{'protein_file'} = '/nfs/production/flicek/ensembl/genebuild/genebuild_virtual_user/protein_sets/hymenoptera_uniprot_proteins.fa',
       $clade_params->{'busco_protein_file'}   = '/nfs/production/flicek/ensembl/genebuild/genebuild_virtual_user/protein_sets/hymenoptera_orthodb_proteins.fa',
