3a537d7d7986b4467f97f984cf134694f3cae4e6
Anna Lazar <lazar@ebi.ac.uk>
2025-07-01 13:31:33 +0100

    Update to work with new python script

--- modules/Bio/EnsEMBL/Analysis/Hive/Config/EnsemblAnnoBraker_conf.pm
+++ modules/Bio/EnsEMBL/Analysis/Hive/Config/EnsemblAnnoBraker_conf.pm
@@ -206,12 +206,7 @@ sub default_options {
     'dna_db_port'                  => $ENV{GBP6},                                                                                                        # port for dna db host
     'registry_db_server'           => $ENV{GBS1},                                                                                                        # host for registry db
     'registry_db_port'             => $ENV{GBP1},                                                                                                        # port for registry db
-    'registry_db_name'             => 'gb_assembly_registry', 
-
-    otherfeatures_db_host => $self->o('dna_db_server'),
-    otherfeatures_db_port => $self->o('dna_db_port'),
-    otherfeatures_db_name => $self->o('dbowner') . '_' . $self->o('production_name') . '_otherfeatures_' . $self->o('release_number'),
-
+    'registry_db_name'             => 'gb_assembly_registry',
 
 
     'core_db' => {
@@ -255,14 +250,6 @@ sub default_options {
       -dbname => $self->o('pipe_db_name'),
       -host   => $self->o('pipe_db_server'),
       -port   => $self->o('pipe_db_port'),
-      -user   => $self->o('user'),
-      -pass   => $self->o('password'),
-      -driver => $self->o('hive_driver'),
-    },
-    'otherfeatures_db' => {
-      -dbname => $self->o('otherfeatures_db_name'),
-      -host   => $self->o('otherfeatures_db_host'),
-      -port   => $self->o('otherfeatures_db_port'),
       -user   => $self->o('user'),
       -pass   => $self->o('password'),
       -driver => $self->o('hive_driver'),
@@ -414,7 +401,6 @@ sub pipeline_analyses {
         'num_threads'                 => $self->o('num_threads'),
         'dbowner'                     => $self->o('dbowner'),
         'core_db'                     => $self->o('core_db'),
-        'otherfeatures_db'            => $self->o('otherfeatures_db'),
         'ensembl_release'             => $self->o('ensembl_release'),
         'base_output_dir'             => $self->o('base_output_dir'),
         'registry_db'                 => $self->o('registry_db'),
